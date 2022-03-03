35e21532fc51042a723a02c184c383df269ce6e9
Robert Clay <robclay20@hotmail.co.uk>
2022-03-03 13:38:56 +0000

    tidying up

--- minos/data_generation/fake_council_tax.py
+++ minos/data_generation/fake_council_tax.py
@@ -3,12 +3,12 @@
 
 import pandas as pd
 import numpy as np
-import math
 
 import US_utils
 from string import ascii_uppercase as alphabet
 
 def format_bands(df):
+    # Tax band data oddly formatted. Put it into floats for math to work.
     df = df.apply(lambda x: x.str.rstrip()) # remove right whitespace.
     df = df.applymap(lambda x: str(x).replace('£', '')) # remove pounds.
     df = df.applymap(lambda x: str(x).replace(',', '')) # remove commas.
@@ -16,45 +16,49 @@ def format_bands(df):
     return df
 
 def lower_bound(x):
+    # look up tax band lower bound by region.
     region = x["region"]
     band = x["council_tax"]
+    # if both na return na.
     if region != region or band != band:
         return np.nan
     else:
         return g.loc[region, band]
 
 def upper_bound(x):
+    # lookup tax upper bound by region.
     band = x["council_tax"]
     region = x["region"]
 
+    # if both na return na.
     if region != region or band != band:
         return np.nan
 
+    # which integer tax band are we in (0-9).
     where = alphabet.find(band[-1])
     # if at the largest tax band set the upper limit to infinity.
     if region == "Wales": # wales has one more tax band to consider.
         if where == 8:
             return np.nan
-    else:
-        if where == 7:
+    elif where == 7:
             return np.nan
     # upper bound is where the next bound begins. E.g. band A's upper limit is the state of band B.
-    # Find the band above.
+    # Find the band above and lookup upper bound.
     new_band = "Band " + alphabet[where+1]
     return g.loc[x["region"], new_band]
 
 def random_draw(x):
+    # assume uniform distributed tax between upper and lower bounds.
     lb = x["council_tax_lower"]
     ub = x["council_tax_upper"]
 
-    if lb!= lb and ub != ub:
+    # if both na return na.
+    if lb != lb and ub != ub:
         return np.nan
 
     if ub != ub:
         ub = lb + 1000 # if no upper limit just add on a thousand. #TODO can do better than this. geometric dist?
         # TODO can use pretty much any distribution you want here so long as it draws between these bounds.
-    if lb > ub:
-        print('aaaa')
     return np.random.uniform(lb, ub)
 
 if __name__ == '__main__':
@@ -80,11 +84,6 @@ if __name__ == '__main__':
     data["council_tax"] = data["council_tax"].replace([-1,-2,-7,-8,-9], np.nan)
     data["region"] = data["region"].replace(['-1','-2','-7','-8','-9'], np.nan)
 
-    data["council_tax"] = data["council_tax"].map(dict(zip(range(1, 10), band_columns)))
-
-    # lookup minimum bound by region and tax band
-
-
-    data["council_tax_lower"] = data.apply(lower_bound, axis=1)
+    data["council_tax_lower"] = data.apply(lower_bound, axis=1) # establish individual lower and upper bounds.
     data["council_tax_upper"] = data.apply(upper_bound, axis=1)
-    data["council_tax_draw"] = data.apply(random_draw, axis=1)
+    data["council_tax_draw"] = data.apply(random_draw, axis=1) # draw randomly between these bounds.
