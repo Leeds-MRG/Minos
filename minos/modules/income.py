1159fa0be64517fec49d28fe6c0f3c5dfbc048ce
ld-archer <l.archer@leeds.ac.uk>
2022-04-06 16:07:55 +0100

    The r_utils predict method is working pretty well and returning an updated population, but updating the population view is not going as planned...

--- minos/modules/income.py
+++ minos/modules/income.py
@@ -6,8 +6,7 @@ Possible extension to interaction with employment/education and any spatial/inte
 
 import pandas as pd
 from pathlib import Path
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
+import minos.modules.r_utils as r_utils
 
 
 class Income:
@@ -55,6 +54,12 @@ class Income:
                 E.g. rate tables.
         """
 
+        # Load in transition model
+        transition_model = r_utils.load_transitions('income')
+
+        simulation._data.write('income_transition',
+                               transition_model)
+
         return simulation
 
 
@@ -76,21 +81,22 @@ class Income:
         """
 
         # Load in inputs from pre-setup.
+        self.transition_model = builder.data.load("income_transition")
 
         # Build vivarium objects for calculating transition probabilities.
         # Typically this is registering rate/lookup tables. See vivarium docs/other modules for examples.
-        #self.transition_coefficients = builder.lookup.build_table()
+        #self.transition_coefficients = builder.
 
         # Assign randomness streams if necessary.
-        self.randomness = builder.randomness.get_stream("income")
+        self.random = builder.randomness.get_stream("income")
 
         # Determine which subset of the main population is used in this module.
         # columns_created is the columns created by this module.
         # view_columns is the columns from the main population used in this module.
-        view_columns = ['age', 'sex', 'ethnicity', 'region']
-        self.population_view = builder.population.get_view(
-            ["alive"], query="alive == 'alive'"
-        )
+        # In this case, view_columns are taken straight from the transition model
+        view_columns = ['pidp']
+        view_columns += self.transition_model.rx2('model').names
+        self.population_view = builder.population.get_view(columns=view_columns)
 
         # Population initialiser. When new individuals are added to the microsimulation a constructer is called for each
         # module. Declare what constructer is used. usually on_initialize_simulants method is called. Inidividuals are
@@ -98,7 +104,7 @@ class Income:
 
         # Declare events in the module. At what times do individuals transition states from this module. E.g. when does
         # individual graduate in an education module.
-        #builder.event.register_listener("time_step", self.on_time_step())
+        builder.event.register_listener("time_step", self.on_time_step)
 
 
     def on_initialize_simulants(self, pop_data):
@@ -125,14 +131,21 @@ class Income:
         event : vivarium.population.PopulationEvent
             The event time_step that called this function.
         """
+        # Get living people to update their income
+        pop = self.population_view.get(event.index, query="alive =='alive' and sex != 'nan'")
+
+        pIndex = pop.index # save index for re-indexing after prediction
+
         ## Predict next income value
-        # For predicting next income state, we will use the R predict functions
+        newWaveIncome = self.calculate_income(pop)
+        newWaveIncome = pd.DataFrame(newWaveIncome, index=pIndex)
 
         # Draw individuals next states randomly from this distribution.
         # Update population with new income
+        self.population_view.update(newWaveIncome['hh_income'])
 
 
-    def calculate_income(self, index):
+    def calculate_income(self, pop):
         """Calculate income transition distribution based on provided people/indices
 
         Parameters
@@ -142,4 +155,7 @@ class Income:
         Returns
         -------
         """
+        # The calculation relies on the R predict method and the model that has already been specified
+        nextWaveIncome = r_utils.predict_next_timestep(self.transition_model, pop, independant='hh_income')
 
+        return nextWaveIncome
--- minos/modules/r_utils.py
+++ minos/modules/r_utils.py
@@ -2,13 +2,15 @@
 R utility functions. These are currently all related to the use of transition models.
 """
 
-import rpy2.robjects as robjects
+import rpy2.robjects as ro
+from rpy2.robjects import IntVector, StrVector, pandas2ri
 from rpy2.robjects.packages import importr
+from rpy2.robjects.conversion import localconverter
 
 
-
-def load_transitions(component, path = '../data/transitions/'):
+def load_transitions(component, path = 'data/transitions/'):
     """
+    This function will load transition models that have been generated in R and saved as .rds files.
 
     Parameters
     ----------
@@ -21,12 +23,54 @@ def load_transitions(component, path = '../data/transitions/'):
     -------
     An RDS object containing a fitted model for prediction.
     """
+    # import base R package
     base = importr('base')
 
-    #filename = path + component + '.rds'
+    # generate filename from arguments and load model
     filename = f"{path}{component}.rds"
-
-
     model = base.readRDS(filename)
 
     return model
+
+
+def predict_next_timestep(model, current, independant):
+    """
+    This function will take the transition model loaded in load_transitions() and use it to predict the next timestep
+    for a module.
+
+    Parameters
+    ----------
+    Model : R rds object
+        Fitted model loaded in from .rds file
+    current : vivarium.framework.population.PopulationView
+        View including columns that are required for prediction
+
+    Returns:
+    -------
+    A prediction of the information for next timestep
+    """
+    # import R packages
+    base = importr('base')
+    stats = importr('stats')
+
+    # drop the independant variable as we don't need this to predict
+    #current = current.drop(labels=independant, axis=1, inplace=True)
+
+    # TODO: Generalise the vector formation. Check type of python object and use to influence IntVector/Str.../Float etc.
+    # TODO: Use converter instead for this
+    x_pidp = IntVector(current.pidp)
+    x_age = IntVector(current.age)
+    x_sex = StrVector(current.sex).factor()
+
+    rdf_dict = {'pidp' : x_pidp, 'age' : x_age, 'sex' : x_sex}
+    dataf = ro.DataFrame(rdf_dict)
+
+    # R predict method returns a Vector of predicted values, so need to be bound to original df and converter to Pandas
+    prediction = stats.predict(model, dataf)
+
+    newRPopDF = base.cbind(dataf, hh_income = prediction)
+
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        newPandasPopDF = ro.conversion.rpy2py(newRPopDF)
+
+    return newPandasPopDF
