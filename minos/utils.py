9cef8c3b4745d0fc00b7ba6fd4b252a547e43081
Robert Clay <gyrc@leeds.ac.uk>
2022-07-08 14:58:01 +0100

    Update utils.py
    adding in mock artifact because vivarium needs it for god knows what reason.

--- minos/utils.py
+++ minos/utils.py
@@ -9,6 +9,7 @@ import numpy as np
 import os
 import pandas as pd
 import datetime
+from itertools import product
 
 #import humanleague as hl
 from scipy.sparse import coo_matrix
@@ -235,3 +236,24 @@ def to_years(time: pd.Timedelta) -> float:
 
 def get_time():
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+def make_uniform_pop_data(age_bin_midpoint=False):
+    age_bins = [(n, n + 5) for n in range(0, 100, 5)]
+    sexes = ('Male', 'Female')
+    years = zip(range(1990, 2018), range(1991, 2019))
+    locations = (1, 2)
+
+    age_bins, sexes, years, locations = zip(*product(age_bins, sexes, years, locations))
+    mins, maxes = zip(*age_bins)
+    year_starts, year_ends = zip(*years)
+
+    pop = pd.DataFrame({'age_start': mins,
+                        'age_end': maxes,
+                        'sex': sexes,
+                        'year_start': year_starts,
+                        'year_end': year_ends,
+                        'location': locations,
+                        'value': [100] * len(mins)})
+    if age_bin_midpoint:  # used for population tests
+        pop['age'] = pop.apply(lambda row: (row['age_start'] + row['age_end']) / 2, axis=1)
+    return pop
