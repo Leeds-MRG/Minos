6cbdf789f7c6e26be317a4246f95e4387f5134d7
ld-archer <l.archer@leeds.ac.uk>
2022-07-29 11:02:35 +0100

    Added some extra required R packages to the install.R script

--- install.R
+++ install.R
@@ -1,4 +1,7 @@
 # install any packages here that are needed.
 install.packages("ordinal")
 install.packages('nnet')
+install.packages('tidyverse')
+install.packages('stringr')
+install.packages('readr')
 #
