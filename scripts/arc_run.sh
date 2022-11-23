00f1b42cedcb0082fbcc96c4e17ce418e395352d
ld-archer <l.archer@leeds.ac.uk>
2022-11-23 21:50:53 +0000

    Fixed check for number of args to account for the $SGE_RUN_ID when submitting on arc, and changed arc emails to only email on abort

--- scripts/arc_run.sh
+++ scripts/arc_run.sh
@@ -9,7 +9,7 @@
 ## Request hours of runtime
 #$ -l h_rt=48:00:00
 ## Email if a run aborts
-#$ -m abe
+#$ -m a
 ## Select memory
 #$ -l h_vmem=15G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
@@ -22,10 +22,10 @@
 ## Tell computer this is an array job with tasks from 1 to N
 #$ -t 1-2
 
-if [ "$#" -eq 4 ]; then
+if [ "$#" -eq 5 ]; then
   echo "Running baseline MINOS simulation"
   python3 scripts/run.py -c $2 -o $4 -r $SGE_TASK_ID
-elif [ "$#" -eq 6 ]; then
+elif [ "$#" -eq 7 ]; then
   echo "Running MINOS simulation with $6"
   python3 scripts/run.py -c $2 -o $4 -i $6 -r $SGE_TASK_ID
 fi
