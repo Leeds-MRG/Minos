a036731e0e020fdd2d00764920e74a51dee41bc6
Robert Clay <gyrc@leeds.ac.uk>
2023-01-17 22:35:03 +0000

    adding more subset functions to output

--- minos/validation/aggregate_subset_functions.py
+++ minos/validation/aggregate_subset_functions.py
@@ -4,20 +4,20 @@ import numpy as np
 import pandas as pd
 
 def find_subset_function(function_string):
-    if function_string == "none":
-        subset_function = None
-    elif function_string == "who_alive":
-        subset_function = who_alive
-    elif function_string == "who_boosted":
-        subset_function = who_boosted
-    elif function_string == "who_below_living_wage":
-        subset_function = who_below_living_wage
-    elif function_string == "who_kids":
-        subset_function = who_kids
-    elif function_string == "who_below_poverty_line_and_kids":
-        subset_function = who_below_poverty_line_and_kids
-    elif function_string == "who_bottom_income_quintile":
-        subset_function = who_bottom_income_quintile
+
+    subset_functions = {
+        "none": None,
+        "who_alive": who_alive,
+        "who_boosted": who_boosted,
+        "who_below_living_wage": who_below_living_wage,
+        "who_kids": who_kids,
+        "who_below_poverty_line_and_kids": who_below_poverty_line_and_kids,
+        "who_bottom_income_quintile": who_bottom_income_quintile
+        "who_scottish": ,
+
+    }
+    if function_string in subset_functions.keys():
+        subset_function = subset_functions[function_string]
     else:
         print("no subset_function defined. Defaulting to anyone alive.")
         subset_function = who_alive
@@ -77,3 +77,34 @@ def who_bottom_income_quintile(df, k=1):
     df = who_alive(df)
     split = pd.qcut(df['hh_income'], q=5, labels=[1, 2, 3, 4, 5])
     return df.loc[split == k, ]
+
+def who_scotland(df):
+    return df.loc[df["region"]=="Scotland", ]
+
+def who_disabled(df):
+    return df.loc[df["labour_state"]=="Sick/Disabled", ]
+
+def who_ethnic_minority(df):
+    return df.loc[df["ethnicity"] != "WBI", ]
+
+def who_three_kids(df):
+    return df.loc[df["nkids"] >= 3, ]
+def who_young_mother(df):
+    who_kids = df["nkids"] >= 1
+    who_mother = df["sex"] == "female"
+    who_under_twenty_five = df["age"] <= 25
+    who_eligible = who_kids * who_mother * who_under_twenty_five
+    return df.loc[who_eligible, ]
+
+def who_young_adults(df):
+    return df.loc[df["age"] <= 25, ]
+
+def who_unemployed_adults(df):
+    return df.loc[df["labour_state"] == "Unemployed", ]
+
+def who_no_formal_education(df):
+    return df.loc[df["education_state"] == 0, ]
+
+
+# TODO lone parent families, people with complex needs, babies under 1, income related benefits., no recourse to public funds.
+# TODO interventions suggested here. briding payment to £50 per week per child, fuel insecurity fund to £20m, flexible LA funding, moratorium on rent, debt releif for financially vulnerable, island household fund.
