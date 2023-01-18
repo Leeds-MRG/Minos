7a0a8a3f49f0fc9a5cdc77f6fc18e4c2d732dd5d
Robert Clay <gyrc@leeds.ac.uk>
2023-01-18 21:11:27 +0000

    update aggregate lineplot subset functions to have scots

--- minos/validation/aggregate_subset_functions.py
+++ minos/validation/aggregate_subset_functions.py
@@ -10,10 +10,18 @@ def find_subset_function(function_string):
         "who_alive": who_alive,
         "who_boosted": who_boosted,
         "who_below_living_wage": who_below_living_wage,
+<<<<<<< Updated upstream
         "who_kids": who_kids,
         "who_below_poverty_line_and_kids": who_below_poverty_line_and_kids,
         "who_bottom_income_quintile": who_bottom_income_quintile
         "who_scottish": who_scotland,
+=======
+        "who_scottish_below_living_wage": who_scottish_below_living_wage,
+        "who_kids": who_kids,
+        "who_below_poverty_line_and_kids": who_below_poverty_line_and_kids,
+        "who_bottom_income_quintile": who_bottom_income_quintile,
+        "who_scottish": who_scottish,
+>>>>>>> Stashed changes
         "who_disabled": who_disabled,
         "who_ethnic_minority": who_ethnic_minority,
         "who_three_kids": who_three_kids,
@@ -21,6 +29,10 @@ def find_subset_function(function_string):
         "who_young_adults": who_young_adults,
         "who_unemployed_adults": who_unemployed_adults,
         "who_no_formal_education": who_no_formal_education,
+<<<<<<< Updated upstream
+=======
+        "who_scottish_boosted": who_scottish_boosted,
+>>>>>>> Stashed changes
         "who_lone_parent_families": None, # NOT IMPLEMENTED BELOW HERE YET.
         "who_complex_needs": None,
         "who_babies_under_one": None,
@@ -28,12 +40,16 @@ def find_subset_function(function_string):
         "who_no_public_funds_recourse": None,
 
     }
+<<<<<<< Updated upstream
     if function_string in subset_functions.keys():
         subset_function = subset_functions[function_string]
     else:
+=======
+    if function_string not in subset_functions.keys():
+>>>>>>> Stashed changes
         print("no subset_function defined. Defaulting to anyone alive.")
-        subset_function = who_alive
-    return subset_function
+        return who_alive
+    return subset_functions[function_string]
 
 def who_alive(df):
     """ Get who is alive.
@@ -47,6 +63,10 @@ def who_alive(df):
 
     """
     return df.loc[df['alive'] == 'alive', ]
+
+def who_scottish_boosted(df):
+    df = who_scottish(df)
+    return who_boosted(df)
 
 def who_boosted(df):
     """ Get who is alive and who has received boost/intervention
@@ -84,12 +104,21 @@ def who_below_living_wage(df):
     who_uplifted_notLondon *= df['hourly_wage'] < 10.90
     return df.loc[who_uplifted_notLondon | who_uplifted_London, ] # get anyone from either group.
 
+def who_scottish_below_living_wage(df):
+    df = who_alive(df)
+    df = who_scottish(df)
+    "who earns below the living wage?"
+    who_uplifted = df['hourly_wage'] > 0
+    who_uplifted *= df['hourly_wage'] < 10.90
+    return df.loc[who_uplifted, ] # get anyone from either group.
+
 
 def who_bottom_income_quintile(df, k=1):
     df = who_alive(df)
     split = pd.qcut(df['hh_income'], q=5, labels=[1, 2, 3, 4, 5])
     return df.loc[split == k, ]
 
+<<<<<<< Updated upstream
 def who_scotland(df):
     return df.loc[df["region"]=="Scotland", ]
 
@@ -97,10 +126,23 @@ def who_disabled(df):
     return df.loc[df["labour_state"]=="Sick/Disabled", ]
 
 def who_ethnic_minority(df):
+=======
+def who_scottish(df):
+    df = who_alive(df)
+    return df.loc[df["region"]=="Scotland", ]
+
+def who_disabled(df):
+    df = who_alive(df)
+    return df.loc[df["labour_state"]=="Sick/Disabled", ]
+
+def who_ethnic_minority(df):
+    df = who_alive(df)
+>>>>>>> Stashed changes
     return df.loc[df["ethnicity"] != "WBI", ]
 
 def who_three_kids(df):
     return df.loc[df["nkids"] >= 3, ]
+<<<<<<< Updated upstream
 def who_young_mother(df):
     who_kids = df["nkids"] >= 1
     who_mother = df["sex"] == "female"
@@ -116,3 +158,25 @@ def who_unemployed_adults(df):
 
 def who_no_formal_education(df):
     return df.loc[df["education_state"] == 0, ]
+=======
+
+def who_young_mother(df):
+    df = who_alive(df)
+    df = who_young_adults(df)
+    who_kids = df["nkids"] >= 1
+    who_mother = df["sex"] == "female"
+    who_eligible = who_kids * who_mother
+    return df.loc[who_eligible, ]
+
+def who_young_adults(df):
+    df = who_alive(df)
+    return df.loc[df["age"] <= 25, ]
+
+def who_unemployed_adults(df):
+    df = who_alive(df)
+    return df.loc[df["labour_state"] == "Unemployed", ]
+
+def who_no_formal_education(df):
+    df = who_alive(df)
+    return df.loc[df["education_state"] == 0, ]
+>>>>>>> Stashed changes
