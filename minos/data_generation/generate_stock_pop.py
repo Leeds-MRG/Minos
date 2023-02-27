060b08b294d018f56d43058d34b99f3c5068bba8
Luke Archer <l.archer@leeds.ac.uk>
2023-02-27 12:39:24 +0000

    Added function to copy nutrition_quality information from wave 11 onto wave 12 so we have starting values. Also imputed missing values with yearly mean

--- minos/data_generation/generate_stock_pop.py
+++ minos/data_generation/generate_stock_pop.py
@@ -67,6 +67,51 @@ def reweight_stock(data, projections):
     return reweighted_data
 
 
+def wave_data_copy(data):
+    """
+    Unfortunately due to some of the variables we rely on not being available in all waves, we have to take a copy of
+    some information and paste it onto another year. Due to the current aim (24/02/23) being to include wave 12 of data
+    and use wave 12 (2020 in our timescale) as the kick off point, we need to copy nutrition_quality onto this wave.
+    Nutrition_quality is available every second wave, so we will copy wave 11 data onto wave 12. I also don't want to
+    lose any of the new respondents in wave 12 that don't have a nutrition_quality value, so I might impute those people
+    with the mean of the population.
+    Parameters
+    ----------
+    data
+
+    Returns
+    -------
+
+    """
+    print("Copying wave 11 nutrition_quality onto wave 12 sample...")
+    #tmp = data['nutrition_quality'][data['time'] == 2019]
+
+    ## get temp vector of pidp, time, and nutrition_quality from 2019
+    tmp = data[['pidp', 'time', 'nutrition_quality']][data['time'] == 2019]
+    # change time to 2018 for tmp
+    tmp['time'] = 2020
+
+    # replace -9 values in 2020 with Nonetype
+    data['nutrition_quality'][data['time'] == 2020] = None
+
+    # now merge and combine the two separate nutrition_quality columns (now with suffix') into one col
+    data_merged = data.merge(right=tmp,
+                             how='left',
+                             on=['pidp', 'time'])
+    data_merged['nutrition_quality'] = -9
+    data_merged['nutrition_quality'][data_merged['time'] != 2020] = data_merged['nutrition_quality_x']
+    data_merged['nutrition_quality'][data_merged['time'] == 2020] = data_merged['nutrition_quality_y']
+    # drop intermediate columns
+    data_merged.drop(labels=['nutrition_quality_x', 'nutrition_quality_y'], axis=1, inplace=True)
+
+    # last step is to impute the still missing with the mean value. Without this we would have to drop all the
+    # missing values, meaning anybody not in wave 11 would be removed. This is dodgy because we don't know who should be
+    # missing, but I don't know what else to do
+    data_merged['nutrition_quality'][(data_merged['time'] == 2020) & (data_merged['nutrition_quality'].isna())] = round(data_merged['nutrition_quality'][data_merged['time'] == 2020].mean())
+
+    return data_merged
+
+
 def generate_stock(projections):
     maxyr = US_utils.get_data_maxyr()
 
@@ -83,6 +128,9 @@ def generate_stock(projections):
     # Needs a max_educ column despite it not being important for the majority of people
     # Will be used in the future for the 16-25 year olds at the beginning of the simulation
     data['max_educ'] = data['education_state']
+
+    # copy wave 11 nutrition_quality onto wave 12
+    data = wave_data_copy(data)
 
     # Set loneliness and ncigs as int
     data['loneliness'] = data['loneliness'].astype('int64')
