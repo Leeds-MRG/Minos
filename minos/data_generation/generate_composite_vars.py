a62d192b4c8e87acdfe315d6b866fe52f866b356
Luke Archer <l.archer@leeds.ac.uk>
2023-04-28 15:45:39 +0100

    Calculated equivalent income in generate_composite_vars so we have a value in the input populations

--- minos/data_generation/generate_composite_vars.py
+++ minos/data_generation/generate_composite_vars.py
@@ -745,6 +745,7 @@ def generate_physical_health_score(data):
     return data
 
 
+#TODO: Complete this function. Collect information on parents education (where available) and use to predict educ
 def generate_parents_education(data):
     """
     For predicting the highest education a 16 will attain in their life, we can use information on the parents
@@ -765,6 +766,88 @@ def generate_parents_education(data):
     # Groupby hid then filter to make sure there is a 16 year old in the house
     grouped_dat = data.groupby(['hidp'], axis=1).filter(lambda d: (d.age != 16.0), axis=0)
     # filtered_dat = grouped_dat.filter(lambda d: (d['age'] != 16))
+
+    return data
+
+
+def calculate_equivalent_income(data):
+    """Calculate equivalent income based on provided SIPHER 7 variables
+
+    Parameters
+    ----------
+        pop: PopulationView
+            Population from MINOS to calculate next income for.
+    Returns
+    -------
+    nextWaveIncome: pd.Series
+        Vector of new household incomes from OLS prediction.
+    """
+    # This is a deterministic calculation based on the values from each of the SIPHER7 variables
+    # Each level of each variable is assigned a weight, which are then used to modify the value for disposable
+    # income to generate a value that is based on both the income and characteristics of a persons life
+
+    # This was first done by one-hot encoding each of the S7 variables, but this would be clunky and inefficient
+    # I'm going to try and do it slightly more efficiently
+
+    # The goal here is to calculate an exponent term for modifying income to equivalent income:
+    #   EquivalentIncome = Income*EXP(X)
+    # Each weighting applies to the level
+
+    # First set up dictionaries for each variable to hold its factor weights
+    phys_health_dict = {
+        5: 0,
+        4: -0.116/1.282,
+        3: -0.135/1.282,
+        2: -0.479/1.282,
+        1: -0.837/1.282
+    }
+    men_health_dict = {
+        5: 0,
+        4: -0.14/1.282,
+        3: -0.215/1.282,
+        2: -0.656/1.282,
+        1: -0.877/1.282
+    }
+    loneliness_dict = {
+        1: 0,
+        2: -0.186/1.282,
+        3: -0.591/1.282
+    }
+    employment_dict = {
+        'FT Employed': 0,
+        'PT Employed': 0.033/1.282,
+        'Job Seeking': -0.283/1.282,
+        'FT Education': -0.184/1.282,
+        'Family Care': -0.755/1.282,
+        'Not Working': -0.221/1.282
+    }
+    housing_dict = {
+        'Yes to all': 0,
+        'Yes to some': -0.235/1.282,
+        'No to all': -0.696/1.282
+    }
+    nh_safety_dict = {
+        'Hardly ever': 0,
+        'Some of the time': -0.291/1.282,
+        'Often': -0.599/1.282
+    }
+
+    # Now we add together weights for each factor level to generate the exponent term
+    data['EI_exp_term'] = 0
+    #pop['EI_exp_term'] = pop['EI_exp_term'] + phys_health_dict.get(pop['S7_physical_health'])
+    data['EI_exp_term'] = data['EI_exp_term'] + data.apply(lambda x: phys_health_dict[x['S7_physical_health']], axis=1)
+    data['EI_exp_term'] = data['EI_exp_term'] + data.apply(lambda x: men_health_dict[x['S7_mental_health']], axis=1)
+    data['EI_exp_term'] = data['EI_exp_term'] + data.apply(lambda x: loneliness_dict[x['loneliness']], axis=1)
+    data['EI_exp_term'] = data['EI_exp_term'] + data.apply(lambda x: employment_dict[x['S7_labour_state']], axis=1)
+    data['EI_exp_term'] = data['EI_exp_term'] + data.apply(lambda x: housing_dict[x['S7_housing_quality']], axis=1)
+    data['EI_exp_term'] = data['EI_exp_term'] + data.apply(lambda x: nh_safety_dict[x['S7_neighbourhood_safety']], axis=1)
+
+    # finally do the calculation for equivalent income (EI = income^EI_exp_term)
+    data['equivalent_income'] = data['hh_income'] * np.exp(data['EI_exp_term'])
+
+    data.drop(labels=['EI_exp_term'],
+              axis=1,
+              inplace=True)
 
     return data
 
@@ -791,6 +874,7 @@ def main():
     data = generate_hh_structure(data)  # household structure
     data = generate_marital_status(data)  # marital status
     data = generate_physical_health_score(data)  # physical health score
+    data = calculate_equivalent_income(data)  # equivalent income
 
     print('Finished composite generation. Saving data...')
     US_utils.save_multiple_files(data, years, "data/composite_US/", "")
