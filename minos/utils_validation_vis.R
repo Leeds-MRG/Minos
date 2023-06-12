d6f9bdf53ed53d6ea95632e618296fdef291225d
Luke Archer <l.archer@leeds.ac.uk>
2023-06-12 11:51:01 +0100

    Added a separate handovers script for SIPHER 7 vars, and modified the cv ordinal plots function to allow for handovers as they are useful in combination with other handovers

--- minos/utils_validation_vis.R
+++ minos/utils_validation_vis.R
@@ -3,6 +3,38 @@
 miss.values <- c(-10, -9, -8, -7, -3, -2, -1,
                  -10., -9., -8., -7., -3., -2., -1.)
 
+############ PREP ############
+
+validation_prep_ordinal <- function(raw.dat, base.dat, var) {
+  raw <- raw.dat %>%
+    dplyr::select(pidp, time, all_of(var)) %>%
+    group_by(time, .data[[var]]) %>%
+    count() %>%
+    mutate(source = 'US')
+  
+  base <- base.dat %>%
+    dplyr::select(pidp, time, all_of(var)) %>%
+    group_by(time, .data[[var]]) %>%
+    count() %>%
+    mutate(source = 'baseline_output')
+  
+  combined <- rbind(raw, base)
+  combined[[var]] <- as.factor(combined[[var]])
+  
+  var.norm <- combined %>%
+    group_by(time) %>%
+    filter(.data[[var]] != -9) %>%
+    mutate(total = sum(n)) %>%
+    mutate(prct = (n / total))
+  
+  prepped <- list(var = combined,
+                  norm = var.norm)
+  
+  return(prepped)
+}
+
+############ PLOTTING ############
+
 spaghetti_plot <- function(data, v, save=FALSE, save.path=NULL, filename.tag=NULL)
 {
   # spaghetti plot displaying trajectories over time for continuous variable v
