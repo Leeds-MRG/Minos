db240f55c73f956ff08e931d091562ec5282d632
Robert Clay <gyrc@leeds.ac.uk>
2023-06-15 13:53:15 +0100

    fixing lineplots notebook with relative imports and removing scrap ideas.

--- minos/outcomes/epcg_ebss_lineplot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MODE="glasgow_scaled"
-SOURCES="baseline,EPCG,EBSS"
-TAGS="Baseline,Energy Price Cap Guarantee,Energy Bill Support Scheme"
-SUBSETS="who_alive,who_boosted,who_boosted"
-SAVE_PREFIX="all_child_uplift"
-# custom baseline for living wage only.
-bash minos/outcomes/make_lineplot.sh "$MODE" "$SOURCES" "$TAGS" "$SUBSETS" "$SAVE_PREFIX"
