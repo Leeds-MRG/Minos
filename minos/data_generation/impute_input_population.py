83e2639f8a4699b3fd46d553d49d0ba2abdbd441
Luke Archer <l.archer@leeds.ac.uk>
2023-07-14 14:03:26 +0100

    Started imputation script using sklearn.impute functions

---
+++ minos/data_generation/impute_input_population.py
@@ -0,0 +1,32 @@
+"""
+Script for imputing missing values in the input populations, to ensure we have a full dataset with no missing values.
+"""
+
+import numpy as np
+from sklearn.impute import SimpleImputer
+
+
+def impute(data):
+    """
+
+    Parameters
+    ----------
+    data
+
+    Returns
+    -------
+
+    """
+    return data
+
+
+def main():
+    # load in data
+    # run through imputation
+    # one var at a time? Too slow, all at once if possible
+    # save data
+    # done
+
+
+if __name__ == '__main__'():
+    main()
