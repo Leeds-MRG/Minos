fix income variance

minos/modules/income.py
@@ -676,7 +676,7 @@ class lmmYJIncome(Base):
         newWaveIncome['hh_income'] = np.clip(newWaveIncome['hh_income'], -2500, 17000)
         newWaveIncome['hh_income_diff'] = newWaveIncome['hh_income'] - pop['hh_income']
         income_mean = np.mean(newWaveIncome["hh_income"])
-        std_ratio = (np.std(pop['hh_income'])/np.std(newWaveIncome["hh_income"]))
+        std_ratio = (np.std(pop['hh_income'])/np.std(newWaveIncome["hh_income"])) * 0.95
         newWaveIncome["hh_income"] *= std_ratio
         newWaveIncome["hh_income"] -= ((std_ratio-1)*income_mean)
         #newWaveIncome["hh_income"] -= 75
@@ -706,7 +706,7 @@ class lmmYJIncome(Base):
                                                                        dependent='hh_income_new',
                                                                        yeo_johnson = True,
                                                                        reflect=False,
-                                                                       noise_std= 0.13)#0.45 for yj. 100? for non yj.
+                                                                       noise_std= 0.08)#0.45 for yj. 100? for non yj.
         # get new hh income diffs and update them into history_data.
         #self.update_history_dataframe(pop, self.year-1)
         #new_history_data = self.history_data.loc[self.history_data['time']==self.year].index # who in current_year

minos/modules/r_utils.py
@@ -432,7 +432,7 @@ def predict_next_timestep_yj_gamma_glmm(model, rpy2_modules, current, dependent,
         #VGAM = rpy2_modules["VGAM"]
         #prediction = prediction.ro + VGAM.rlaplace(current.shape[0], 0, noise_std) # add gaussian noise.
         prediction = prediction.ro + stats.rnorm(current.shape[0], 0, noise_std) # add gaussian noise.
-        noise = np.clip(stats.rcauchy(current.shape[0], 0, 0.0015), -3, 4) #0.075
+        noise = np.clip(stats.rcauchy(current.shape[0], 0, 0.0015), -5, 5) #0.075
         with localconverter(ro.default_converter + numpy2ri.converter):
             Rnoise = ro.conversion.py2rpy(noise)
         prediction = prediction.ro + Rnoise # add gaussian noise.