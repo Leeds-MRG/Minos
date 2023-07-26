Fixed and reinstated the fake_council_tax.py script so we are randomly generating council tax values within bands. Also improved hh_income calculation doing council tax properly and more missing values where key information is missing

minos/data_generation/fake_council_tax.py
@@ -7,6 +7,7 @@ import numpy as np
 import US_utils
 from string import ascii_uppercase as alphabet
 
+
 def format_bands(df):
     # Tax band data oddly formatted. Put it into floats for math to work.
     df = df.apply(lambda x: x.str.rstrip()) # remove right whitespace.
@@ -15,7 +16,8 @@ def format_bands(df):
     df = df.astype(float)  # to floats.
     return df
 
-def lower_bound(x):
+
+def lower_bound(x, bands):
     # look up tax band lower bound by region.
     region = x["region"]
     band = x["council_tax"]
@@ -23,9 +25,10 @@ def lower_bound(x):
     if region != region or band != band:
         return np.nan
     else:
-        return g.loc[region, band]
+        return bands.loc[region, band]
 
-def upper_bound(x):
+
+def upper_bound(x, bands):
     # lookup tax upper bound by region.
     band = x["council_tax"]
     region = x["region"]
@@ -37,15 +40,16 @@ def upper_bound(x):
     # which integer tax band are we in (0-9).
     where = alphabet.find(band[-1])
     # if at the largest tax band set the upper limit to infinity.
-    if region == "Wales": # wales has one more tax band to consider.
+    if region == "Wales":  # wales has one more tax band to consider.
         if where == 8:
             return np.nan
     elif where == 7:
-            return np.nan
+        return np.nan
     # upper bound is where the next bound begins. E.g. band A's upper limit is the state of band B.
     # Find the band above and lookup upper bound.
-    new_band = "Band " + alphabet[where+1]
-    return g.loc[x["region"], new_band]
+    new_band = "Band " + alphabet[where]
+    return bands.loc[x["region"], new_band]
+
 
 def random_draw(x):
     # assume uniform distributed tax between upper and lower bounds.
@@ -57,12 +61,14 @@ def random_draw(x):
         return np.nan
 
     if ub != ub:
-        ub = lb + 1000 # if no upper limit just add on a thousand. #TODO can do better than this. geometric dist?
+        ub = lb + 1000  # if no upper limit just add on a thousand. #TODO can do better than this. geometric dist?
         # TODO can use pretty much any distribution you want here so long as it draws between these bounds.
     return np.random.uniform(lb, ub)
 
+
 if __name__ == '__main__':
-    years = [2010]
+    maxyr = US_utils.get_data_maxyr()
+    years = np.arange(1991, maxyr)
     file_names = [f"data/raw_US/{item}_US_cohort.csv" for item in years]
     data = US_utils.load_multiple_data(file_names)
 
@@ -71,19 +77,29 @@ if __name__ == '__main__':
     lad_to_region = US_utils.load_json("persistent_data/JSON/", "LAD_to_region_name.json")
 
     band_columns = ['Band A', 'Band B', 'Band C', 'Band D', 'Band E',
-       'Band F', 'Band G', 'Band H', 'Band I']
+                    'Band F', 'Band G', 'Band H', 'Band I']
     # band columns messy. formatting..
     lad_to_band[band_columns] = format_bands(lad_to_band[band_columns])
     # add regional data to band columns
     lad_to_band["region"] = lad_to_band["Area"].map(lad_to_region)
 
     # group bands by region.
-    g = lad_to_band.groupby(by="region").mean()
+    ct_bands = lad_to_band.groupby(by="region").mean()
 
     # convert tax bands to lower/upper numeric bounds.
-    data["council_tax"] = data["council_tax"].replace([-1,-2,-7,-8,-9], np.nan)
-    data["region"] = data["region"].replace(['-1','-2','-7','-8','-9'], np.nan)
+    data["council_tax"] = data["council_tax"].replace([-1, -2, -7, -8, -9], np.nan)
+    data["region"] = data["region"].replace(['-1', '-2', '-7', '-8', '-9'], np.nan)
 
-    data["council_tax_lower"] = data.apply(lower_bound, axis=1) # establish individual lower and upper bounds.
-    data["council_tax_upper"] = data.apply(upper_bound, axis=1)
-    data["council_tax_draw"] = data.apply(random_draw, axis=1) # draw randomly between these bounds.
+    band_dict = dict(zip(range(1, 10), band_columns))
+
+    data['council_tax'] = data['council_tax'].map(band_dict)
+
+    data["council_tax_lower"] = data.apply(lambda x: lower_bound(x, ct_bands), axis=1)  # establish individual lower and upper bounds.
+    data["council_tax_upper"] = data.apply(lambda x: upper_bound(x, ct_bands), axis=1)
+    data["council_tax_draw"] = data.apply(random_draw, axis=1)  # draw randomly between these bounds.
+
+    # Handle single case where someone in London gave has wrong council tax band (Band I which only exists in Wales)
+    data['council_tax_draw'][data['council_tax_draw'].isna()] = -9
+
+    print('Finished composite generation. Saving data...')
+    US_utils.save_multiple_files(data, years, "data/adj_raw_US/", "")