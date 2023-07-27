3e64676a834b76091725f945a8f210532b621679
Robert Clay <gyrc@leeds.ac.uk>
2023-07-27 14:30:12 +0100

    add universal credit variable and subset function

--- minos/data_generation/US_format_raw.py
+++ minos/data_generation/US_format_raw.py
@@ -295,6 +295,7 @@ def format_ukhls_columns(year):
                       'hhsize': 'hhsize', # number of people in household
                       'tenure_dv': 'housing_tenure', # housing tenure type (owned, rented etc.)
                       'urban_dv': 'urban', # urban or rural household.
+                      'benbase4': 'universal_credit',
                       }
 
     # Some variables change names halfway through UKHLS.
--- minos/outcomes/aggregate_subset_functions.py
+++ minos/outcomes/aggregate_subset_functions.py
@@ -3,41 +3,43 @@
 import numpy as np
 import pandas as pd
 
-def dynamic_subset_function(data, subset_chain_string=None, mode = 'default_config'):
 
+def dynamic_subset_function(data, subset_chain_string=None, mode='default_config'):
     if subset_chain_string == None:
         function_string = "who_alive"
         print("No subset defined. Defaulting to who_alive..")
 
     subset_chains = {"who": [],
                      # Original intervention subgroups.
-                    "who_alive": [who_alive],
-                    "who_boosted": [who_alive, who_boosted],
-                    "who_below_living_wage": [who_alive, who_below_living_wage],
-                    "who_kids": [who_alive, who_kids],
-                    "who_below_poverty_line_and_kids": [who_alive, who_kids, who_below_poverty_line],
-                    "who_scottish": [who_alive, who_scottish],
-                    #"who_bottom_income_quintile": who_bottom_income_quintile,
-                    # Scottish gov sgugested vulnerable subgroups.
-                    "who_disabled": [who_alive, who_kids, who_disabled],
-                    "who_ethnic_minority": [who_alive, who_kids, who_ethnic_minority],
-                    "who_three_kids": [who_alive, who_three_kids],
-                    "who_young_mother": [who_alive, who_young_adults, who_female, who_kids],
-                    "who_young_adults": [who_alive, who_kids, who_young_adults],
-                    "who_unemployed_adults": [who_alive, who_adult, who_kids, who_unemployed],
-                    "who_no_formal_education": [who_alive, who_kids, who_no_formal_education],
+                     "who_alive": [who_alive],
+                     "who_boosted": [who_alive, who_boosted],
+                     "who_below_living_wage": [who_alive, who_below_living_wage],
+                     "who_kids": [who_alive, who_kids],
+                     "who_below_poverty_line_and_kids": [who_alive, who_kids, who_below_poverty_line],
+                     "who_universal_credit_poverty_line_kids": [who_alive, who_kids, who_below_poverty_line,
+                                                           who_universal_credit], #
+                     "who_scottish": [who_alive, who_scottish],
+                     # "who_bottom_income_quintile": who_bottom_income_quintile,
+                     # Scottish gov sgugested vulnerable subgroups.
+                     "who_disabled": [who_alive, who_kids, who_disabled],
+                     "who_ethnic_minority": [who_alive, who_kids, who_ethnic_minority],
+                     "who_three_kids": [who_alive, who_three_kids],
+                     "who_young_mother": [who_alive, who_young_adults, who_female, who_kids],
+                     "who_young_adults": [who_alive, who_kids, who_young_adults],
+                     "who_unemployed_adults": [who_alive, who_adult, who_kids, who_unemployed],
+                     "who_no_formal_education": [who_alive, who_kids, who_no_formal_education],
                      # Not sure about this one? single parent may not be primrary caregiver. e.g. divorced dad without custody.
-                    "who_lone_parent_families": [who_alive, who_single, who_kids],
-                    # NOT IMPLEMENTED BELOW HERE YET. NO SUFFICIENT DATA IN UNDERSTANDING SOCIETY.
-                    #"who_complex_needs": None,
-                    #"who_babies_under_one": None,
-                    #"who_income_benefits": None,
-                    #"who_no_public_funds_recourse": None,
-                    }
+                     "who_lone_parent_families": [who_alive, who_single, who_kids],
+                     # NOT IMPLEMENTED BELOW HERE YET. NO SUFFICIENT DATA IN UNDERSTANDING SOCIETY.
+                     # "who_complex_needs": None,
+                     # "who_babies_under_one": None,
+                     # "who_income_benefits": None,
+                     # "who_no_public_funds_recourse": None,
+                     }
 
     subset_chain = subset_chains[subset_chain_string]
 
-    if mode == 'scotland_mode': # if in scotland mode add it to the .
+    if mode == 'scotland_mode':  # if in scotland mode add it to the .
         subset_chain.append(who_scottish)
 
     for subset_function in subset_chain:
@@ -54,7 +56,7 @@ def who_alive(df):
     Returns
     -------
     """
-    return df.loc[df['alive'] == 'alive', ]
+    return df.loc[df['alive'] == 'alive',]
 
 
 def who_adult(df):
@@ -69,10 +71,10 @@ def who_below_living_wage(df):
     who_uplifted_notLondon = df['hourly_wage'] > 0
     who_uplifted_notLondon *= df['region'] != 'London'
     who_uplifted_notLondon *= df['hourly_wage'] < 10.90
-    return df.loc[who_uplifted_notLondon | who_uplifted_London, ] # get anyone from either group.
+    return df.loc[who_uplifted_notLondon | who_uplifted_London,]  # get anyone from either group.
 
 
-#def who_bottom_income_quintile(df, k=1):
+# def who_bottom_income_quintile(df, k=1):
 #    df = who_alive(df)
 #    split = pd.qcut(df['hh_income'], q=5, labels=[1, 2, 3, 4, 5])
 #    return df.loc[split == k, ]
@@ -80,7 +82,7 @@ def who_below_living_wage(df):
 
 def who_below_poverty_line(df):
     "who below poverty line?. Defined as 60% of national median hh income."
-    return df.loc[df['hh_income'] <= (np.nanmedian(df['hh_income']) * 0.6), ]
+    return df.loc[df['hh_income'] <= (np.nanmedian(df['hh_income']) * 0.6),]
 
 
 def who_boosted(df):
@@ -123,15 +125,21 @@ def who_single(df):
 
 def who_three_kids(df):
     # Who has three or more children.
-    return df.loc[df["nkids"] >= 3, ]
+    return df.loc[df["nkids"] >= 3,]
 
 
 def who_unemployed(df):
     # who unemployed
-    return df.loc[df["labour_state"] == "Unemployed", ]
+    return df.loc[df["labour_state"] == "Unemployed",]
 
 
 def who_young_adults(df):
     # who aged between 16 and 25.
-    df =  df.loc[df["age"] <= 25, ]
-    return df.loc[df["age"] >= 16, ]
+    df = df.loc[df["age"] <= 25,]
+    return df.loc[df["age"] >= 16,]
+
+
+def who_universal_credit(df):
+    # whos on universal credit.
+    # TODO extend to other legacy benefits at some point?
+    return df.loc[df['universal_credit'] == 1]
