be59252fd0b968d39d8b0284826a3f1bb4da8529
paddy-r <h.p.rice@leeds.ac.uk>
2023-08-01 10:55:17 +0100

    Updating RunPipeline and priorities with income components

--- minos/minosPipeline/RunPipeline.py
+++ minos/minosPipeline/RunPipeline.py
@@ -122,8 +122,8 @@ def get_priorities():
     component_priorities.update({el:0 for el in replenishment_components_map})
     component_priorities.update({el:1 for el in ["FertilityAgeSpecificRates()",
                                                  "nkidsFertilityAgeSpecificRates()"]})
-    component_priorities.update({el: 2 for el in ["Mortality()"]})
-    # component_priorities.update){el:3 for el in ['']} # New income-based components to be added here
+    component_priorities.update({el:2 for el in ["Mortality()"]})
+    component_priorities.update({el:3 for el in ['Income', 'geeIncome', 'geeYJIncome', 'lmmDiffIncome', 'lmmYJIncome']}) # New income-based components to be added here
     component_priorities.update({el:4 for el in intervention_components_map})
     everything_else = [el for el in list(components_map)+list(SIPHER7_components_map) if el not in list(component_priorities)]
     component_priorities.update({el:5 for el in everything_else})
