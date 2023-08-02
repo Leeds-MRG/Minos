22ac521c7a211db9bcf8b5522cbb6c619bfaf79f
Robert Clay <gyrc@leeds.ac.uk>
2023-08-02 16:48:10 +0100

    low memory warning fix

--- minos/outcomes/append_spatial_data.py
+++ minos/outcomes/append_spatial_data.py
@@ -9,7 +9,7 @@ from multiprocessing import Pool
 from itertools import repeat, chain
 
 def append_spatial_attribute(file_path, spatial_dict, key_variable, attribute_name):
-    data = pd.read_csv(file_path, low_memory=True, engine='c')
+    data = pd.read_csv(file_path, low_memory=False, engine='c')
     #if attribute_name not in data.columns: # only do this if the variable isn't already added..
     data[attribute_name] = data[key_variable].map(spatial_dict)
     data.to_csv(file_path)
