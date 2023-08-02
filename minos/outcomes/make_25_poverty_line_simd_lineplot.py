a03a4437b6e01c68bec5f4cb5acb305b5996d7b8
Robert Clay <gyrc@leeds.ac.uk>
2023-08-02 21:50:11 +0100

    subgraphs for 1,5,10 deciles and fixing subsets

--- minos/outcomes/make_25_poverty_line_simd_lineplot.py
+++ minos/outcomes/make_25_poverty_line_simd_lineplot.py
@@ -1,10 +1,30 @@
 from minos.outcomes.make_lineplots import main as lineplot_main
 
 if __name__ == '__main__':
-    directories = ("povertyLineChildUplift," * 11)[:-1] # repeat 11 times and cut off last comma.
+    directories = "baseline," + ("povertyLineChildUplift," * 10)[:-1] # repeat 11 times and cut off last comma.
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
-    subset_function_strings = """who_below_poverty_line_and_kids,who_poverty_kids_first_simd_decile,who_poverty_kids_second_simd_decile,who_poverty_kids_third_simd_decile,who_poverty_kids_fourth_simd_decile,who_poverty_kids_fifth_simd_decile,who_poverty_kids_sixth_simd_decile,who_poverty_kids_seventh_simd_decile,who_poverty_kids_eighth_simd_decile,who_poverty_kids_ninth_simd_decile,who_poverty_kids_tenth_simd_decile"""
+    subset_function_strings = """who_kids,who_kids_first_simd_decile,who_kids_second_simd_decile,who_kids_third_simd_decile,who_kids_fourth_simd_decile,who_kids_fifth_simd_decile,who_kids_sixth_simd_decile,who_kids_seventh_simd_decile,who_kids_eighth_simd_decile,who_kids_ninth_simd_decile,who_kids_tenth_simd_decile"""
     prefix="25_poverty_simd_deciles"
+    config_mode = "glasgow_scaled"
+    ref = "National Average"
+    v = "SF_12"
+    method='nanmean'
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
+
+    directories = "baseline,povertyLineChildUplift"
+    tags = "National Average,Tenth"
+    subset_function_strings = """who_kids_tenth_simd_decile,who_boosted_tenth_simd_decile"""
+    prefix="25_poverty_tenth_simd_decile"
+    config_mode = "glasgow_scaled"
+    ref = "National Average"
+    v = "SF_12"
+    method='nanmean'
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
+
+    directories = "baseline,povertyLineChildUplift"
+    tags = "National Average,Fifth"
+    subset_function_strings = """who_kids_fifth_simd_decile,who_boosted_fifth_simd_decile"""
+    prefix="25_poverty_fifth_simd_decile"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
