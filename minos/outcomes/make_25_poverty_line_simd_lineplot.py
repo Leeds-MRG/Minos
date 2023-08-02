c05ff40900e2ca495539bd059a0c804efba2045e
Robert Clay <gyrc@leeds.ac.uk>
2023-08-02 21:14:59 +0100

    fixing_subset_string

--- minos/outcomes/make_25_poverty_line_simd_lineplot.py
+++ minos/outcomes/make_25_poverty_line_simd_lineplot.py
@@ -4,17 +4,7 @@ if __name__ == '__main__':
     directories = ("povertyLineChildUplift," * 11)[:-1] # repeat 11 times and cut off last comma.
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
     subset_function_strings = """
-        who_below_poverty_line_and_kids,
-        who_poverty_kids_first_simd_decile,
-        who_poverty_kids_second_simd_decile,
-        who_poverty_kids_third_simd_decile,
-        who_poverty_kids_fourth_simd_decile,
-        who_poverty_kids_fifth_simd_decile,
-        who_poverty_kids_sixth_simd_decile,
-        who_poverty_kids_seventh_simd_decile,
-        who_poverty_kids_eighth_simd_decile,
-        who_poverty_kids_ninth_simd_decile,
-        who_poverty_kids_tenth_simd_decile"""
+        who_below_poverty_line_and_kids,who_poverty_kids_first_simd_decile,who_poverty_kids_second_simd_decile,who_poverty_kids_third_simd_decile,who_poverty_kids_fourth_simd_decile,who_poverty_kids_fifth_simd_decile,who_poverty_kids_sixth_simd_decile,who_poverty_kids_seventh_simd_decile,who_poverty_kids_eighth_simd_decile,who_poverty_kids_ninth_simd_decile,who_poverty_kids_tenth_simd_decile"""
     prefix="25_poverty_simd_deciles"
     config_mode = "glasgow_scaled"
     ref = "National Average"
