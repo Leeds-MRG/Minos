d1060e8d919a0cb2076d6ba4aac3c6ab96a6b39d
Robert Clay <gyrc@leeds.ac.uk>
2023-08-02 17:58:31 +0100

    updating prefix for ebss plot

--- minos/outcomes/make_baseline_simd_lineplot.py
+++ minos/outcomes/make_baseline_simd_lineplot.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     directories = "baseline,baseline,baseline,baseline,baseline,baseline,baseline,baseline,baseline,baseline,baseline,baseline"
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
     subset_function_strings = "who_alive,who_first_simd_decile,who_second_simd_decile,who_third_simd_decile,who_fourth_simd_decile,who_fifth_simd_decile,who_sixth_simd_decile,who_seventh_simd_decile,who_eighth_simd_decile,who_ninth_simd_decile,who_tenth_simd_decile"
-    prefix="simd_deciles"
+    prefix="baseline_simd_deciles"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
