ecf7158b03d602e90209f2998d614138cd66a61c
Luke Archer <l.archer@leeds.ac.uk>
2023-08-31 15:12:02 +0100

    Additions to testing scripts for equivalent income interventions, and expansion of the local batch logic

--- scripts/local_batch.sh
+++ scripts/local_batch.sh
@@ -1,6 +1,6 @@
 
 NUMRUNS=10
-declare -a intlist=(livingWageIntervention energyDownlift)
+declare -a intlist=(livingWageIntervention energyDownlift hhIncomeChildUplift hhIncomePovertyLineChildUplift)
 
 TIME=$(date +%Y_%m_%d_%H_%M_%S)
 
