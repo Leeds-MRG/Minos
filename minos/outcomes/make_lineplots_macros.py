88fccfc15594791f518b77e64a2184c8ad484fe5
Robert Clay <gyrc@leeds.ac.uk>
2023-09-07 15:31:14 +0100

    typo in args

--- minos/outcomes/make_lineplots_macros.py
+++ minos/outcomes/make_lineplots_macros.py
@@ -8,44 +8,50 @@ def all_child_lineplot(*args):
     directories = "baseline,hhIncomeChildUplift"
     tags = "Baseline,All Child Uplift"
     subset_function_strings = "who_kids,who_boosted"
-    prefix="baseline_all_child_uplift"
+    prefix = "baseline_all_child_uplift"
     config_mode = "default_config"
-    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12", method='nanmean')
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12",
+                  method='nanmean')
 
 
 def poverty_line_child_lineplot(*args):
     directories = "baseline,hhIncomePovertyLineChildUplift"
     tags = "Baseline,Poverty Line Child Uplift"
     subset_function_strings = "who_below_poverty_line_and_kids,who_boosted"
-    prefix="baseline_poverty_child_uplift"
+    prefix = "baseline_poverty_child_uplift"
     config_mode = "default_config"
-    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12", method='nanmean')
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12",
+                  method='nanmean')
 
 
 def living_wage_lineplot(*args):
     directories = "baseline,livingWageIntervention"
     tags = "Baseline,Living Wage Intervention"
     subset_function_strings = "who_below_living_wage,who_boosted"
-    prefix="baseline_living_wage"
+    prefix = "baseline_living_wage"
     config_mode = "default_config"
-    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12", method='nanmean')
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12",
+                  method='nanmean')
+
 
 def ebss_lineplot(*args):
     directories = "baseline,energyDownlift"
     tags = "Baseline,Energy Downlift"
     subset_function_strings = "who_uses_energy,who_boosted"
-    prefix="baseline_energy_downlift"
+    prefix = "baseline_energy_downlift"
     config_mode = "default_config"
-    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12", method='nanmean')
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12",
+                  method='nanmean')
 
 
 def all_five_lineplots(*args):
     directories = "baseline,hhIncomeChildUplift,hhIncomePovertyLineChildUplift,livingWageIntervention,energyDownlift"
     tags = "Baseline,All Child Uplift,Poverty Line Child Uplift,Living Wage Intervention,Energy Downlift"
     subset_function_strings = "who_alive,who_boosted,who_boosted,who_boosted,who_boosted"
-    prefix="all_five_combined"
+    prefix = "all_five_combined"
     config_mode = "default_config"
-    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12", method='nanmean')
+    lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref="Baseline", v="SF_12",
+                  method='nanmean')
 
 
 ########################################
@@ -53,27 +59,27 @@ def all_five_lineplots(*args):
 ########################################
 
 def glasgow_deciles_lineplot(config_mode, source, subset_function):
-    #directories = f"baseline," + (f"{source}," * 10)[:-1] # repeat 11 times and cut off last comma.
-    directories = f"{source}," + (f"{source}," * 10)[:-1] # repeat 11 times and cut off last comma.
+    # directories = f"baseline," + (f"{source}," * 10)[:-1] # repeat 11 times and cut off last comma.
+    directories = f"{source}," + (f"{source}," * 10)[:-1]  # repeat 11 times and cut off last comma.
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
-    #subset_function_strings = f"""who_kids,who_kids_first_simd_decile,who_kids_second_simd_decile,who_kids_third_simd_decile,who_kids_fourth_simd_decile,who_kids_fifth_simd_decile,who_kids_sixth_simd_decile,who_kids_seventh_simd_decile,who_kids_eighth_simd_decile,who_kids_ninth_simd_decile,who_kids_tenth_simd_decile"""
+    # subset_function_strings = f"""who_kids,who_kids_first_simd_decile,who_kids_second_simd_decile,who_kids_third_simd_decile,who_kids_fourth_simd_decile,who_kids_fifth_simd_decile,who_kids_sixth_simd_decile,who_kids_seventh_simd_decile,who_kids_eighth_simd_decile,who_kids_ninth_simd_decile,who_kids_tenth_simd_decile"""
     subset_function_strings = "who_alive,who_first_simd_decile,who_second_simd_decile,who_third_simd_decile,who_fourth_simd_decile,who_fifth_simd_decile,who_sixth_simd_decile,who_seventh_simd_decile,who_eighth_simd_decile,who_ninth_simd_decile,who_tenth_simd_decile"
-    prefix=f"25_{source}_simd_deciles"
+    prefix = f"25_{source}_simd_deciles"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
 def poverty_line_simd_deciles_lineplot(*args):
-    directories = "baseline," + ("hhIncomePovertyLineChildUplift," * 10)[:-1] # repeat 11 times and cut off last comma.
+    directories = "baseline," + ("hhIncomePovertyLineChildUplift," * 10)[:-1]  # repeat 11 times and cut off last comma.
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
     subset_function_strings = """who_kids,who_kids_first_simd_decile,who_kids_second_simd_decile,who_kids_third_simd_decile,who_kids_fourth_simd_decile,who_kids_fifth_simd_decile,who_kids_sixth_simd_decile,who_kids_seventh_simd_decile,who_kids_eighth_simd_decile,who_kids_ninth_simd_decile,who_kids_tenth_simd_decile"""
-    prefix="25_poverty_simd_deciles"
+    prefix = "25_poverty_simd_deciles"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
@@ -81,11 +87,11 @@ def poverty_line_first_decile_lineplot(*args):
     directories = "baseline,hhIncomePovertyLineChildUplift"
     tags = "National Average,First"
     subset_function_strings = """who_poverty_kids_first_simd_decile,who_boosted_first_simd_decile"""
-    prefix="25_poverty_first_simd_decile"
+    prefix = "25_poverty_first_simd_decile"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
@@ -93,52 +99,53 @@ def poverty_line_tenth_decile_lineplot(*args):
     directories = "baseline,hhIncomePovertyLineChildUplift"
     tags = "National Average,Tenth"
     subset_function_strings = """who_poverty_kids_tenth_simd_decile,who_boosted_tenth_simd_decile"""
-    prefix="25_poverty_tenth_simd_decile"
+    prefix = "25_poverty_tenth_simd_decile"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
-    #lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
+    method = 'nanmean'
+    # lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
 def poverty_line_fifth_decile_lineplot(*args):
     directories = "baseline,hhIncomePovertyLineChildUplift"
     tags = "National Average,Fifth"
     subset_function_strings = """who_poverty_kids_fifth_simd_decile,who_boosted_fifth_simd_decile"""
-    prefix="25_poverty_fifth_simd_decile"
+    prefix = "25_poverty_fifth_simd_decile"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
+
 def simd_decile_baseline_lineplot(*args):
-    directories = ("baseline," * 11)[:-1] # repeat 11 times and cut off last comma.
+    directories = ("baseline," * 11)[:-1]  # repeat 11 times and cut off last comma.
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
     subset_function_strings = "who_alive,who_first_simd_decile,who_second_simd_decile,who_third_simd_decile,who_fourth_simd_decile,who_fifth_simd_decile,who_sixth_simd_decile,who_seventh_simd_decile,who_eighth_simd_decile,who_ninth_simd_decile,who_tenth_simd_decile"
-    prefix="baseline_simd_deciles"
+    prefix = "baseline_simd_deciles"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
 def epcg_simd_deciles_lineplot(*args):
-    directories = ("EBSS," * 11)[:-1] # repeat 11 times and cut off last comma.
+    directories = ("EBSS," * 11)[:-1]  # repeat 11 times and cut off last comma.
     tags = "National Average,First,Second,Third,Fourth,Fifth,Sixth,Seventh,Eighth,Ninth,Tenth"
     subset_function_strings = "who_alive,who_first_simd_decile,who_second_simd_decile,who_third_simd_decile,who_fourth_simd_decile,who_fifth_simd_decile,who_sixth_simd_decile,who_seventh_simd_decile,who_eighth_simd_decile,who_ninth_simd_decile,who_tenth_simd_decile"
-    prefix="ebss_simd_deciles"
+    prefix = "ebss_simd_deciles"
     config_mode = "glasgow_scaled"
     ref = "National Average"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
+
 
 ######################################################
 # Space for child uplifts split by amount and subset #
 ######################################################
-
 
 
 def all_child(config_mode, boost_amount):
@@ -149,7 +156,7 @@ def all_child(config_mode, boost_amount):
     prefix = f"{boost_amount}_all"
     ref = "Baseline"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
@@ -161,8 +168,9 @@ def relative_poverty(config_mode, boost_amount):
     prefix = f"{boost_amount}_relative_poverty"
     ref = "Baseline"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
+
 
 def priority_only(config_mode, boost_amount):
     "universal credit (the actual intervention) only lineplot"
@@ -172,7 +180,7 @@ def priority_only(config_mode, boost_amount):
     prefix = f"{boost_amount}_priority"
     ref = "Baseline"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
@@ -181,26 +189,26 @@ def UC_priority(config_mode, boost_amount):
     directories = f"baseline,{boost_amount}UniversalCredit,{boost_amount}Priority"
     tags = f"Baseline,£{boost_amount} Universal Credit,£{boost_amount} Priority Groups"
     subset_function_strings = "who_universal_credit_and_kids,who_boosted,who_boosted"
-    prefix= f"{boost_amount}_UC_and_priority"
+    prefix = f"{boost_amount}_UC_and_priority"
     ref = "Baseline"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
+
 
 def incremental_25_to_100(config_mode, intervention_name, intervention_tag, subset_function):
     "The same intervention in increments from £25 to £100"
     directories = f"baseline,25{intervention_name},50{intervention_name},75{intervention_name},100{intervention_name}"
     tags = f"Baseline,£25 {intervention_tag},£50 {intervention_tag},£75 {intervention_tag},£100 {intervention_tag}"
     subset_function_strings = f"{subset_function},who_boosted,who_boosted,who_boosted,who_boosted"
-    prefix=f"25_100_incremental_{intervention_name}_uplift"
+    prefix = f"25_100_incremental_{intervention_name}_uplift"
     ref = "Baseline"
     v = "SF_12"
-    method='nanmean'
+    method = 'nanmean'
     lineplot_main(directories, tags, subset_function_strings, prefix, mode=config_mode, ref=ref, v=v, method=method)
 
 
-
-############### #
+#################
 # main function #
 #################
 
@@ -223,7 +231,7 @@ string_to_lineplot_function = {
     "glasgow_poverty_tenth": poverty_line_tenth_decile_lineplot,
     "epcg_simd_lineplot": epcg_simd_deciles_lineplot,
 
-    #incremental boost amounts for UC and Priority populations
+    # incremental boost amounts for UC and Priority populations
     "25_UC_priority": UC_priority,
     "50_UC_priority": UC_priority,
     "75_UC_priority": UC_priority,
@@ -249,14 +257,13 @@ string_to_lineplot_function = {
 
 }
 
-string_to_lineplot_function_args={
+string_to_lineplot_function_args = {
 
     # glasgow synthpop lineplots
     "glasgow_baseline_all_deciles": ["baseline", "who_kids"],
     "glasgow_poverty_all_deciles": ["25RelativePoverty", "who_below_poverty_line_and_kids"],
     "glasgow_universal_credit_all_deciles": ["25UniversalCredit", "who_universal_credit_and_kids"],
     "glasgow_priority_groups_all_deciles": ["25Priority", "who_priority_subgroups"],
-
 
     "25_UC_priority": [25],
     "50_UC_priority": [50],
@@ -279,9 +286,9 @@ string_to_lineplot_function_args={
     "100_relative_poverty": [100],
 
     "incremental_universal_credit": ["UniversalCredit", "Universal Credit", "who_universal_credit_and_kids"],
-    "incremental_priority_groups": {"Priority", "Priority Groups", "who_priority_subgroups"},
-    "incremental_all_child": {"All", "Nationwide", "who_kids"},
-    "incremental_relative_poverty": {"RelativePoverty", "Relative Poverty", "who_below_poverty_line_and_kids"},
+    "incremental_priority_groups": ["Priority", "Priority Groups", "who_priority_subgroups"],
+    "incremental_all_child": ["All", "Nationwide", "who_kids"],
+    "incremental_relative_poverty": ["RelativePoverty", "Relative Poverty", "who_below_poverty_line_and_kids"],
 }
 
 if __name__ == '__main__':
