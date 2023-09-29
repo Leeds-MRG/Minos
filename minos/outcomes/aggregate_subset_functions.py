b9f4ec85976f03ac25b0c1e59e54f4372d50a240
Robert Clay <gyrc@leeds.ac.uk>
2023-09-29 11:17:01 +0100

    slight error in subset function?

--- minos/outcomes/aggregate_subset_functions.py
+++ minos/outcomes/aggregate_subset_functions.py
@@ -307,8 +307,7 @@ def who_young_adults(df):
 def who_universal_credit(df):
     # whos on universal credit.
     # TODO extend to other legacy benefits at some point?
-    who_credit = np.unique(df.loc[df['universal_credit'] == 1]['hidp'])
-    return df.loc[df['hidp'].isin(who_credit), ]
+    return df.loc[df['universal_credit'] == 1, ]
 
 def who_uses_energy(df):
     return df.loc[df['yearly_energy'] > 0]
