56f1010795ad67a6d9c580a441f47ed022fc66c2
Luke Archer <l.archer@leeds.ac.uk>
2023-10-24 15:01:01 +0100

    Updated mapping scripts and make targets to enable mapping with synthetic data

--- minos/outcomes/sf12_single_map.R
+++ minos/outcomes/sf12_single_map.R
@@ -4,24 +4,39 @@ source("minos/outcomes/minos_SF12_maps.R")
 main <- function(){
   
   parser <- ArgumentParser(description="Plot a map of some MINOS geojson.")
-  parser$add_argument("-f", "--geojson_file_name", dest='file', help="What geojson to use.")
+  parser$add_argument('-m', '--mode', help='Which experiment are we mapping? 
+                      This mainly relates to the output subdirectory we will use 
+                      to find files for aggregation and mapping.')
+  parser$add_argument('-b', '--baseline', help='Which run are we mapping? 
+                      For single maps this will most likely be the baseline run 
+                      i.e. no intervention')
+  parser$add_argument('-r', '--region', help='Which region are we mapping? 
+                      Currently available are Glasgow, Sheffield, Manchester, 
+                      and Scotland.')
+  parser$add_argument('-y', '--year', help='What year to map data for.')
   parser$add_argument("-d", "--plot_destination", dest='destination', help="Where to save a plot.")
-  parser$add_argument("-m", "--mode", dest='mode', help="What spatial region to use. sheffield, manchester, scotland(not yet)")
   parser$add_argument("-v", "--v", dest='var', help="What variable to map.")
   
   args <- parser$parse_args()
-  geojson_file_name <- args$file
+  output_subdir <- args$mode
+  run <- args$baseline
+  region <- args$region
+  year <- args$year
   plot_destination <- args$destination
-  mode <- args$mode
   v <- args$var
 
   ## handle runtime subdirectory
+  # first construct geojson file path and name from arguments
+  geojson_file_path <- paste0('output/', output_subdir, '/', run, '/')
+  geojson_file_name <- paste0(region, '_nanmean_', v, '_', year, '.geojson')
+  
   # first select only the path (not filename)
-  out.files <- list.files(dirname(geojson_file_name))
-
+  out.files <- list.files(geojson_file_path)
+  
+  # if length of out.files is 1, we know that is the directory to look in
   if(length(out.files) == 1) {
-    geojson_file_name = paste0(dirname(geojson_file_name), '/', out.files[1], '/', basename(geojson_file_name))
-  }
+    geojson_file_name = paste0(geojson_file_path, '/', out.files[1], '/', geojson_file_name)
+  } # if out.files length is more than 1, we have multiple directories in the folder. Pick the latest one
   else if(length(out.files) > 1) {
     out.files.date <- as.POSIXlt(out.files, format='%Y_%m_%d_%H_%M_%S')
     max.date <- max(out.files.date)
@@ -41,10 +56,10 @@ main <- function(){
                        min, '_',
                        sec)
 
-    geojson_file_name <- paste0(dirname(geojson_file_name), '/', str.date, '/', basename(geojson_file_name))
+    geojson_file_name <- paste0(geojson_file_path, '/', str.date, '/', geojson_file_name)
   }
 
-  main.single(geojson_file_name, plot_destination, mode, v)
+  main.single(geojson_file_name, plot_destination, v)
 }
 
 main()
