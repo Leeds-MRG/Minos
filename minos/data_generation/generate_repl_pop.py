75a29ed77877a65a74da442c9683d75fddc9ab8b
Luke Archer <l.archer@leeds.ac.uk>
2023-10-25 10:34:01 +0100

    Fixed the type problem with S7 mental and physical health vars, and fixed makefile dependency for S7 transitions so it doesnt re-run every time

--- minos/data_generation/generate_repl_pop.py
+++ minos/data_generation/generate_repl_pop.py
@@ -214,9 +214,12 @@ def generate_replenishing(projections, scotland_mode, cross_validation, inflated
     # finally, predict the highest level of educ
     final_repl = predict_education(reweighted_repl, transition_dir)
 
+    # Have to unfortunately do these type checks as vivarium throws a wobbler when types change
     final_repl['ncigs'] = final_repl['ncigs'].astype(int)
     final_repl['nutrition_quality'] = final_repl['nutrition_quality'].astype(int)
     final_repl['loneliness'] = final_repl['loneliness'].astype(int)
+    final_repl['S7_mental_health'] = final_repl['S7_mental_health'].astype(int)
+    final_repl['S7_physical_health'] = final_repl['S7_physical_health'].astype(int)
 
     US_utils.check_output_dir(output_dir)
     final_repl.to_csv(f'{output_dir}/replenishing_pop_2019-2070.csv', index=False)
--- minos/minosPipeline/RunPipeline.py
+++ minos/minosPipeline/RunPipeline.py
@@ -201,6 +201,28 @@ def validate_components(config_components, intervention):
     return component_list
 
 
+def type_check(data):
+    """
+    We have an unfortunate problem with some variables where the type changes when being read in by Vivarium, which the
+    framework cannot handle and so throws a paddy. This is particularly annoying with the difference between int and
+    float, where the vast majority of int variables are read in as float and so struggle with being updated each wave
+    when new values are assigned int. This function is an attempt to fix this once and for all.
+
+    Parameters
+    ----------
+    data
+
+    Returns
+    -------
+
+    """
+
+    data['S7_mental_health'] = data['S7_mental_health'].astype(int)
+    data['S7_physical_health'] = data['S7_physical_health'].astype(int)
+
+    return data
+
+
 def RunPipeline(config, intervention=None):
     """ Run the daedalus Microsimulation pipeline
 
@@ -270,6 +292,10 @@ def RunPipeline(config, intervention=None):
     # Save population BEFORE start of the simulation. This is for comparisons and change from baseline
     pop = simulation.get_population()
     pop = utils.get_age_bucket(pop)
+
+    # Force type casting for certain problem variables
+    pop = type_check(pop)
+
     # File name and save
     output_data_filename = get_output_data_filename(config)
     output_file_path = os.path.join(config.run_output_dir, output_data_filename)
