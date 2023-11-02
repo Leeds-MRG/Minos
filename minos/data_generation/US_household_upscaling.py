322861471ebc3c732219c257c77cd86650a881da
Robert Clay <gyrc@leeds.ac.uk>
2023-11-02 15:48:12 +0000

    adding seperate bootstrapping file and default household upscaling without it

--- minos/data_generation/US_household_upscaling.py
+++ minos/data_generation/US_household_upscaling.py
@@ -41,14 +41,23 @@ def merge_with_synthpop_households(synthpop, msim_data, merge_column="hidp"):
     #merged_data[f"new_{merge_column}"] = merged_data[f'new_{merge_column}']
     return merged_data
 
-def main(bootstrapping, n):
+def main(bootstrapping=False, n=100_000):
     """
     1. Grab individual synthetic spatial population for UK.
     2. Take subset of spatial population in a specific subregion.
         Glasgow City region / GMCA/ Sheffield City Region / Scotland
     3. Merge synthetic population on real Understanding Society data to get populated individual rows with a spatial component.
     4.
+
+    Parameters
+    ----------
+
+    bootstrapping : bool
+        Do bootstrapping on top of synthetic sample to induce uncertainty?
+    n : int
+        number of boostrapping samples to take.
     """
+    # get synthetic data.
     synthpop_file_path = "persistent_data/spatial_data/HH2011PopEst2020S_population.csv"
     try:
         synthpop_data = pd.read_csv(synthpop_file_path)  # this is individual population weighted data.
@@ -58,23 +67,27 @@ def main(bootstrapping, n):
         raise
 
 
-    # get glasgow data zones, get
+    # get glasgow data zones, get Understanding Society data.
     glasgow_data_zones = pd.read_csv("persistent_data/spatial_data/glasgow_data_zones.csv")["lsoa11cd"]  # glasgow data zone IDs.
     US_data = pd.read_csv("data/final_US/2020_US_cohort.csv")  # only expanding on one year of US data for 2020.
     subsetted_synthpop_data = subset_zone_ids(synthpop_data, glasgow_data_zones)
 
+    # if bootstrapping sample from subsetted synthetic data with replacement.
     if bootstrapping:
         subsetted_synthpop_data = subsetted_synthpop_data.sample(n, replace=True)
 
+    # merge synthetic and US data together.
     subsetted_synthpop_data['hidp'] = subsetted_synthpop_data['hhid']
     merged_data = merge_with_synthpop_households(subsetted_synthpop_data, US_data)
     merged_data = merged_data.dropna(axis=0, subset=["time"])  # remove rows that are missing in spatial data and aren't merged properly.
     print(f"{sum(merged_data['time'].value_counts())} rows out of {merged_data.shape[0]} successfully merged.")
 
+    # scramble new hidp and pidp.
     merged_data['hidp'] = merged_data['new_hidp']  # replace old pidp.
     merged_data.drop(['new_hidp', 'hhid'], axis=1, inplace=True)  # removing old hidp columns
     merged_data['pidp'] = merged_data.index  # creating new pidps.
 
+    # take subset of sample if desired. defaults to 100% for now.
     if bootstrapping:
         percent = 1.0
     percent = 1.0 # = 0.1
@@ -85,7 +98,8 @@ def main(bootstrapping, n):
     # get simd_deciles
     sampled_data = merge_with_spatial_attributes(sampled_data, get_spatial_attribute_data(), "ZoneID")
 
-    sampled_data['weight'] = 1  # force sample weights to 1.
+    sampled_data['weight'] = 1  # force sample weights to 1. as this data is expanded weights no longer representative
+    # but still updating weights helps with weighted aggregates later.
 
     US_utils.check_output_dir("data/scaled_glasgow_US/")  # check save directory exists or create it.
     US_utils.save_file(sampled_data, "data/scaled_glasgow_US/", '', 2020)
