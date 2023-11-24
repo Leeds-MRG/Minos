a5a6da5ef7f3ef9c52bd318602672c15a14fbaa4
Robert Clay <gyrc@leeds.ac.uk>
2023-11-24 16:14:04 +0000

    debugging_maps

--- minos/outcomes/sf12_difference_map.R
+++ minos/outcomes/sf12_difference_map.R
@@ -45,15 +45,16 @@ main <- function(){
   # first select only the path (not filename)
   out.files1 <- list.files(geojson_file_path_base)
   out.files2 <- list.files(geojson_file_path_int)
-
+  
   ## Check how many subdirectories, if more than 1 then pick most recent
   # First output dir
   if(length(out.files1) == 1) {
     geojson_file_name1 = paste0(geojson_file_path_base, '/', out.files1[1], '/', geojson_file_name_base)
   }
-  else if(length(out.files) > 1) {
-    out.files.date <- as.POSIXlt(out.files, format='%Y_%m_%d_%H_%M_%S')
-    max.date <- max(out.files.date)
+  else if(length(out.files1) > 1) {
+    out.files1.date <- as.POSIXlt(out.files1, format='%Y_%m_%d_%H_%M_%S')
+    
+    max.date <- max(out.files1.date)
     
     # Collecting these objects here as they have to be formatted
     yr <- max.date$year + 1900 # year is years since 1900
@@ -62,25 +63,53 @@ main <- function(){
     hour <- formatC(max.date$hour, width=2, flag='0')
     min <- formatC(max.date$min, width=2, flag='0')
     sec <- formatC(max.date$sec, width=2, flag='0')
-    # generate the string runtime directory so we can add to the path
-    str.date <- paste0(yr, '_',
-                       month, '_',
-                       day, '_',
-                       hour, '_',
-                       min, '_',
-                       sec)
+    
+    str.date1 <- paste0(yr, '_',
+                        month, '_',
+                        day, '_',
+                        hour, '_',
+                        min, '_',
+                        sec)
     geojson_file_name1 <- paste0(geojson_file_path_base, '/', str.date1, '/', geojson_file_name_base)
   }
-  
   # Second output dir
   if(length(out.files2) == 1) {
     geojson_file_name2 = paste0(geojson_file_path_int, '/', out.files2[1], '/', geojson_file_name_int)
   }
   else if(length(out.files2) > 1) {
     out.files2.date <- as.POSIXlt(out.files2, format='%Y_%m_%d_%H_%M_%S')
+    
+    max.date <- max(out.files2.date)
+    
+    # Collecting these objects here as they have to be formatted
+    yr <- max.date$year + 1900 # year is years since 1900
+    month <- formatC(max.date$mon + 1, width=2, flag='0') # months are zero indexed (WHY??)
+    day <- formatC(max.date$mday, width=2, flag='0')
+    hour <- formatC(max.date$hour, width=2, flag='0')
+    min <- formatC(max.date$min, width=2, flag='0')
+    sec <- formatC(max.date$sec, width=2, flag='0')
+    
+    str.date2 <- paste0(yr, '_',
+                        month, '_',
+                        day, '_',
+                        hour, '_',
+                        min, '_',
+                        sec)
+    geojson_file_name2 <- paste0(geojson_file_path_int, '/', str.date2, '/', geojson_file_name_int)
   }
+  
+  # Generate output filename
+  #plot_destination <- paste(v, region, run, intervention, year, 'diffmap.pdf', sep='_')
+  
+  if (tolower(synth) == 'true') {
+    plot_destination <- paste(v, region, 'synthetic', run, intervention, year, 'diffmap.pdf', sep='_')
+  }
+  else {
+    plot_destination <- paste(v, region, run, intervention, year, 'diffmap.pdf', sep='_')
+  }
+  
   plot_destination <- paste('plots', plot_destination, sep='/')
-
+  
   main.diff(geojson_file_name1, geojson_file_name2, plot_destination, v)
 }
 
