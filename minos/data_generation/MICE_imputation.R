a90c0f2f7cc4b60da7966b365c815c1f8d3eb3d6
Robert Clay <gyrc@leeds.ac.uk>
2023-12-11 16:32:38 +0000

    mice imputation bugs

--- minos/data_generation/MICE_imputation.R
+++ minos/data_generation/MICE_imputation.R
@@ -3,75 +3,77 @@ library(here)
 source("minos/utils_datain.R")
 source("minos/transitions/utils.R")
 main <- function(){
-  do_mice <- T
-  # load in all post LOCFcorrected data? maybe composite?
-  # MICE impuation from notebook
-  # save to individual waves.
-  # get composite/complete case this data instead. I.E. slot into current pipeline and makes. 
-  start.data <- read_raw_data_out(here::here("data/"), "composite_US") 
-  if (do_mice) {
-    
-
-    
-    # set up MICE and cache data
-    # plots of missingness pre and post raw US
-    # figures of convergence and difference in distribution.
-    imp_columns <- c("SF_12", # continuous variables.
-                     "age", 
-                     "hh_income",
-                     "yearly_energy",
-                     # discrete variables,
-                     'sex',
-                     "job_sec",
-                     "education_state",
-                     "S7_labour_state", 
-                     "region", 
-                     "ethnicity", 
-                     "housing_quality",
-                     "housing_tenure",
-                     "job_sector",
-                     "financial_situation",
-                     "nkids_ind",
-                     'marital_status')
-    
-    method <-   c('pmm',
-                  'pmm',#'polr',
-                  'pmm',
-                  'pmm',
-                  'pmm', #'polr',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm',
-                  'pmm')
-    
-    other.data <- start.data[, !names(start.data) %in% imp_columns]
-    mice.data <- start.data[, c(imp_columns)]
-    mice.data <- replace.missing(mice.data)
-    data <- cbind(mice.data, other.data)
-    
   
-    #mice_set <- mice(data = data[,imp_columns], method=method,
-    #                 m = 1, maxit = 1,
-    #                 #m = 2, maxit = 2,
-    #                 remove.collinear=T)
-    #final.mice.data <- complete(mice_set)
-    final.mice.data <- start.data
-    # add weights, time and pidps back in. 
-    #final.mice.data <- cbind(final.mice.data, other.data)
+  do_mice <- T
+  if (do_mice) {
     cached <- TRUE
     if (cached) {
-      mice.set <- readRDS("data/MICE_set2.rds")
+      mice.set <- readRDS("data/transitions/MICE_set2.rds")
       final.mice.data <- complete(mice.set, 1)
     }
+    else {
+      # load in all post LOCFcorrected data? maybe composite?
+      # MICE impuation from notebook
+      # save to individual waves.
+      # get composite/complete case this data instead. I.E. slot into current pipeline and makes. 
+      start.data <- read_raw_data_out(here::here("data/"), "composite_US") 
+      
+      
+      # set up MICE and cache data
+      # plots of missingness pre and post raw US
+      # figures of convergence and difference in distribution.
+      imp_columns <- c("SF_12", # continuous variables.
+                       "age", 
+                       "hh_income",
+                       "yearly_energy",
+                       # discrete variables,
+                       'sex',
+                       "job_sec",
+                       "education_state",
+                       "S7_labour_state", 
+                       "region", 
+                       "ethnicity", 
+                       "housing_quality",
+                       "housing_tenure",
+                       "job_sector",
+                       "financial_situation",
+                       "nkids_ind",
+                       'marital_status')
+      
+      method <-   c('pmm',
+                    'pmm',#'polr',
+                    'pmm',
+                    'pmm',
+                    'pmm', #'polr',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm',
+                    'pmm')
+      
+      other.data <- start.data[, !names(start.data) %in% imp_columns]
+      mice.data <- start.data[, c(imp_columns)]
+      mice.data <- replace.missing(mice.data)
+      data <- cbind(mice.data, other.data)
+      
+      
+      mice_set <- futuremice(data = data[,imp_columns], method=method,
+                       m = 1, max_iter = 10,
+                       remove.collinear=T)
+      #final.mice.data <- complete(mice_set)
+      final.mice.data <- start.data
+      # add weights, time and pidps back in. 
+      #final.mice.data <- cbind(final.mice.data, other.data)
+    }
   } else {
-    final.mice.data <- start.data
+    final.mice.data <- read_raw_data_out(here::here("data/"), "composite_US") 
+    
   }
   create.if.not.exists("data/mice_US")
   save_raw_data_in(final.mice.data, "data/mice_US/")
