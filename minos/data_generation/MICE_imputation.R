af75b5f8243d875657898c87ed4ae0a2402e5ce5
Robert Clay <gyrc@leeds.ac.uk>
2023-12-11 16:36:00 +0000

    mice imputation bugs

--- minos/data_generation/MICE_imputation.R
+++ minos/data_generation/MICE_imputation.R
@@ -7,70 +7,58 @@ main <- function(){
   do_mice <- T
   if (do_mice) {
     cached <- TRUE
+    imp_columns <- c("SF_12", # continuous variables.
+                     "age", 
+                     "hh_income",
+                     "yearly_energy",
+                     # discrete variables,
+                     'sex',
+                     "job_sec",
+                     "education_state",
+                     "S7_labour_state", 
+                     "region", 
+                     "ethnicity", 
+                     "housing_quality",
+                     "housing_tenure",
+                     "job_sector",
+                     "financial_situation",
+                     "nkids_ind",
+                     'marital_status')
+    
+    method <-   c('pmm',
+                  'pmm',#'polr',
+                  'pmm',
+                  'pmm',
+                  'pmm', #'polr',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm',
+                  'pmm')
+    
+    other.data <- start.data[, !names(start.data) %in% imp_columns]
+    mice.data <- start.data[, c(imp_columns)]
+    mice.data <- replace.missing(mice.data)
+    
     if (cached) {
       mice.set <- readRDS("data/transitions/MICE_set2.rds")
-      final.mice.data <- complete(mice.set, 1)
     }
     else {
-      # load in all post LOCFcorrected data? maybe composite?
-      # MICE impuation from notebook
-      # save to individual waves.
-      # get composite/complete case this data instead. I.E. slot into current pipeline and makes. 
-      start.data <- read_raw_data_out(here::here("data/"), "composite_US") 
-      
-      
-      # set up MICE and cache data
-      # plots of missingness pre and post raw US
-      # figures of convergence and difference in distribution.
-      imp_columns <- c("SF_12", # continuous variables.
-                       "age", 
-                       "hh_income",
-                       "yearly_energy",
-                       # discrete variables,
-                       'sex',
-                       "job_sec",
-                       "education_state",
-                       "S7_labour_state", 
-                       "region", 
-                       "ethnicity", 
-                       "housing_quality",
-                       "housing_tenure",
-                       "job_sector",
-                       "financial_situation",
-                       "nkids_ind",
-                       'marital_status')
-      
-      method <-   c('pmm',
-                    'pmm',#'polr',
-                    'pmm',
-                    'pmm',
-                    'pmm', #'polr',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm',
-                    'pmm')
-      
-      other.data <- start.data[, !names(start.data) %in% imp_columns]
-      mice.data <- start.data[, c(imp_columns)]
-      mice.data <- replace.missing(mice.data)
       data <- cbind(mice.data, other.data)
-      
-      
       mice_set <- futuremice(data = data[,imp_columns], method=method,
                        m = 1, max_iter = 10,
                        remove.collinear=T)
-      #final.mice.data <- complete(mice_set)
-      final.mice.data <- start.data
       # add weights, time and pidps back in. 
       #final.mice.data <- cbind(final.mice.data, other.data)
     }
+    final.mice.data <- complete(mice_set, 1)
+    
   } else {
     final.mice.data <- read_raw_data_out(here::here("data/"), "composite_US") 
     
