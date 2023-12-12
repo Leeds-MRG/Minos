dd79b10f35f474cd33bc87416ae8ae58affefe3c
Robert Clay <gyrc@leeds.ac.uk>
2023-12-12 22:45:35 +0000

    lifting some make restrictions

--- minos/outcomes/make_lineplots.py
+++ minos/outcomes/make_lineplots.py
@@ -170,9 +170,10 @@ def aggregate_variables_by_year(source, tag, years, subset_func_string, v="SF_12
                         aggregated_data = pd.concat([aggregated_data, single_year_aggregate])
                 elif method == aggregate_boosted_counts_and_cumulative_score:
                     for i, single_year_aggregate in enumerate(aggregated_means):
-                        if i == 99 and tag == "No Support" and year == 2035:
+                        if i == 99 and tag == "No Support":
                             print(years)
                             print(single_year_aggregate)
+                            print(aggregated_means[99])
                         if year == 2036:
                             print("good year")
                         if type(single_year_aggregate) != pd.DataFrame: # if no data available create a dummy frame to preserve data frame structure.
