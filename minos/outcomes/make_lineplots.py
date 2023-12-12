db167d3855c75d0416620f5fdba6eebdab992aee
Robert Clay <gyrc@leeds.ac.uk>
2023-12-12 21:09:34 +0000

    trying different conversion to icer

--- minos/outcomes/make_lineplots.py
+++ minos/outcomes/make_lineplots.py
@@ -151,7 +151,7 @@ def aggregate_variables_by_year(source, tag, years, subset_func_string, v="SF_12
                 aggregated_means = pool.starmap(aggregate_csv,
                                                     zip(files, repeat(subset_func_string), repeat(v), repeat(method),
                                                         repeat(mode), repeat(region)))
-                if aggregated_means == []:  # if no datasets found for given year supply a dummy row.
+                if aggregated_means == [] or aggregated_means == None:  # if no datasets found for given year supply a dummy row.
                     print(
                         f"warning no datasets found for intervention {tag} and year {year}. This will result in a blank datapoint in the final lineplot.")
                     aggregated_means = [None]
