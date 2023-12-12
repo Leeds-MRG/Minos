d61f9c7b9b7ff9fccb4a72a53d915cd644c1d822
Robert Clay <gyrc@leeds.ac.uk>
2023-12-12 22:54:45 +0000

    lifting some make restrictions

--- minos/outcomes/make_lineplots.py
+++ minos/outcomes/make_lineplots.py
@@ -151,7 +151,8 @@ def aggregate_variables_by_year(source, tag, years, subset_func_string, v="SF_12
                 aggregated_means = pool.starmap(aggregate_csv,
                                                     zip(files, repeat(subset_func_string), repeat(v), repeat(method),
                                                         repeat(mode), repeat(region)))
-
+                if i == 99 and tag == "No Support" and year == 2035:
+                    print(aggregated_means)
                 if aggregated_means == []:  # if no datasets found for given year supply a dummy row.
                     print(
                         f"warning no datasets found for intervention {tag} and year {year}. This will result in a blank datapoint in the final lineplot.")
