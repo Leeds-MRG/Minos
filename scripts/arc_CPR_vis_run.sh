32a812d36eda4009ed65bbd1a61523458e8163e9
Luke Archer <l.archer@leeds.ac.uk>
2024-01-19 21:52:09 +0000

    Added some variables that were missing from the batch run, and upped the vmem assigned to the full 192GB. Fingers crossed...

--- scripts/arc_CPR_vis_run.sh
+++ scripts/arc_CPR_vis_run.sh
@@ -7,19 +7,23 @@
 ## Use current environment variables and modules
 #$ -V
 ## Request hours of runtime
-#$ -l h_rt=4:00:00
+#$ -l h_rt=12:00:00
 ## Email if a run aborts
 #$ -m a
 ## Select memory
-#$ -l h_vmem=50G # was 15 for big runs
+#$ -l h_vmem=192G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
 #$ -pe smp 1
 ## Set logs directories
-#$ -o ./logs/log
-#$ -e ./logs/errors
+#$ -o ./logs/SCP/log
+#$ -e ./logs/SCP/errors
 
 ############## SET NUMBER OF RUNS HERE ##############
 ## Tell computer this is an array job with tasks from 1 to N
 #$ -t 1-1
+
+mkdir -p logs/SCP
+mkdir -p logs/SCP/log
+mkdir -p logs/SCP/errors
 
 Rscript -e "require(rmarkdown); render('minos/outcomes/CPR_vis.Rmd')"
