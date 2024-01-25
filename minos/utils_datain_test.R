a55da890c0f6d54380258903920ae8849abf18f6
ld-archer <l.archer@leeds.ac.uk>
2024-01-25 12:48:19 +0000

    Separate out the summary generation commands to run in parallel on arc. Also created make target for producing these summaries

--- minos/utils_datain_test.R
+++ minos/utils_datain_test.R
@@ -7,6 +7,41 @@ require(parallelly)
 require(future)
 require(future.apply)
 
+###################### HELPER FUNCTIONS  ######################
+
+get_latest_runtime_subdirectory <- function(path) {
+  # first select only the path (not filenames)
+  out.folders <- list.files(path)
+
+  # if more than 1, take the most recent
+  if(length(out.folders) == 1) {
+    path = paste0(path, '/', out.folders[1], '/')
+  }
+  else if(length(out.folders) > 1) {
+    out.folders.date <- as.POSIXlt(out.folders, format='%Y_%m_%d_%H_%M_%S')
+
+    max.date <- max(out.folders.date)
+
+    # Collecting these objects here as they have to be formatted
+    yr <- max.date$year + 1900 # year is years since 1900
+    month <- formatC(max.date$mon + 1, width=2, flag='0') # months are zero indexed (WHY??)
+    day <- formatC(max.date$mday, width=2, flag='0')
+    hour <- formatC(max.date$hour, width=2, flag='0')
+    min <- formatC(max.date$min, width=2, flag='0')
+    sec <- formatC(max.date$sec, width=2, flag='0')
+
+    str.date <- paste0(yr, '_',
+                       month, '_',
+                       day, '_',
+                       hour, '_',
+                       min, '_',
+                       sec)
+
+    path <- paste0(path, '/', str.date, '/')
+  }
+  return(path)
+}
+
 
 
 # extract run_id from filepath string, so it can be attached to each output file
@@ -23,6 +58,8 @@ drop_dead <- function(data) {
     select(-alive)
   return(data)
 }
+
+###################### SUMMARISE FUNCTIONS  ######################
 
 whole_pop_summarise <- function(data) {
   if ('boost_amount' %in% names(data)) {
@@ -116,127 +153,188 @@ group_summarise <- function(data, group.var) {
   return(data)
 }
 
-# parallel_read_summarise <- function(file_paths, drop.dead = TRUE) {
-#   no_cores <- detectCores() - 1  # Reserve one core for the system
-#   #print("Number available cores:")
-#   #print(no_cores)
-#   cl <- makeCluster(no_cores)
-#   on.exit(stopCluster(cl))  # Ensure the cluster is stopped when the function exits
-#   
-#   # Use parLapply directly with file_paths and fread
-#   #print('Reading files...')
-#   loaded.file.list <- parLapply(cl, file_paths, fread, stringsAsFactors=TRUE)
-#   
-#   # Optionally convert data.tables to data.frames
-#   loaded.file.list <- lapply(loaded.file.list, as.data.frame)
-#   
-#   if (drop.dead) {
-#     loaded.file.list <- lapply(loaded.file.list, drop_dead)
-#   }
-#   
-#   #print('Adding run_id column...')
-#   # Add run_id to each dataframe
-#   for (i in seq_along(loaded.file.list)) {
-#     run_id <- extract_run_id(file_paths[i])
-#     loaded.file.list[[i]]$run_id <- as.numeric(str_remove(run_id, "^0+"))
-#   }
-#   
-#   
-#   # create list for summarised outputs
-#   summary.out.list <- list()
-#   
-#   # Whole pop SF12 summary stats
-#   #print('Generating whole pop summary...')
-#   whole.pop.summary <- lapply(loaded.file.list, whole_pop_summarise)
-#   #whole.pop.summary <- do.call(rbind, do.call(rbind, whole.pop.summary))
-#   whole.pop.summary <- do.call(rbind, whole.pop.summary)
-#   #return(whole.pop.summary)
-#   #summary.out.list <- list(summary.out.list, whole.pop.summary)
-#   summary.out.list[['whole_pop']] <- whole.pop.summary
-#   
-#   #return(whole.pop.summary)
-#   
-#   #print('Households with children...')
-#   families.summary <- lapply(loaded.file.list, families_summarise)
-#   #summary.out.list <- list(summary.out.list, families.summary)
-#   families.summary <- do.call(rbind, families.summary)
-#   summary.out.list[['families']] <- families.summary
-#   
-#   #return(families.summary)
-#   
-#   #print('Treatement on Treated...')
-#   treated.summary <- lapply(loaded.file.list, treated_summarise)
-#   #summary.out.list <- list(summary.out.list, treated.summary)
-#   treated.summary <- do.call(rbind, treated.summary)
-#   summary.out.list[['treated']] <- treated.summary
-#   
-#   #return(treated.summary)
-#   
-#   #print('SIMD Deciles')
-#   SIMD.summary <- lapply(loaded.file.list, group_summarise, 'simd_decile')
-#   # summary.out.list <- list(summary.out.list, SIMD.summary)
-#   SIMD.summary <- do.call(rbind, SIMD.summary)
-#   summary.out.list[['simd_decile']] <- SIMD.summary
-#   
-#   #return(SIMD.summary)
-#   rm(loaded.file.list)
-#   
-#   #print('Finished creating summary outputs')
-#   return(summary.out.list)
-#   
-#   #return(loaded.file.list)
-# }
+simd_generate_quintiles <- function(data) {
+  data[['simd_quintile']] <- round(data[['simd_decile']] / 2)
+  return(data)
+}
+
+###################### PRIORITY SUBGROUPS ######################
+
+priority_summarise_ethnicity <- function(data) {
+  if ('boost_amount' %in% names(data)) {
+    data <- data %>%
+      filter(ethnicity != 'WBI') %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE),
+                total_cost = sum(boost_amount),
+                mean_cost = mean(boost_amount))
+    #TODO: Add number households affected by interventions and other stats
+  } else {
+    data <- data %>%
+      filter(ethnicity != 'WBI') %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE)) %>%
+      mutate(total_cost = 0,
+             mean_cost = 0)
+  }
+  return(data)
+}
+
+priority_summarise_child_under_one <- function(data) {
+  if ('boost_amount' %in% names(data)) {
+    data <- data %>%
+      filter(substr(child_ages, 1, 1) == 0) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE),
+                total_cost = sum(boost_amount),
+                mean_cost = mean(boost_amount))
+    #TODO: Add number households affected by interventions and other stats
+  } else {
+    data <- data %>%
+      filter(substr(child_ages, 1, 1) == 0) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE)) %>%
+      mutate(total_cost = 0,
+             mean_cost = 0)
+  }
+  return(data)
+}
+
+priority_summarise_three_plus_children <- function(data) {
+  if ('boost_amount' %in% names(data)) {
+    data <- data %>%
+      filter(nkids >= 3) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE),
+                total_cost = sum(boost_amount),
+                mean_cost = mean(boost_amount))
+    #TODO: Add number households affected by interventions and other stats
+  } else {
+    data <- data %>%
+      filter(nkids >= 3) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE)) %>%
+      mutate(total_cost = 0,
+             mean_cost = 0)
+  }
+  return(data)
+}
+
+priority_summarise_mother_under_25 <- function(data) {
+  if ('boost_amount' %in% names(data)) {
+    data <- data %>%
+      filter((age < 25) & (nkids_ind > 0)) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE),
+                total_cost = sum(boost_amount),
+                mean_cost = mean(boost_amount))
+    #TODO: Add number households affected by interventions and other stats
+  } else {
+    data <- data %>%
+      filter(nkids >= 3) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE)) %>%
+      mutate(total_cost = 0,
+             mean_cost = 0)
+  }
+  return(data)
+}
+
+priority_summarise_disabled <- function(data) {
+  if ('boost_amount' %in% names(data)) {
+    data <- data %>%
+      group_by(hidp, run_id) %>%
+      mutate(disabled_flag = ifelse(any(S7_labour_state) == 'disabled', TRUE, FALSE)) %>%
+      filter(disabled_flag == TRUE) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE),
+                total_cost = sum(boost_amount),
+                mean_cost = mean(boost_amount))
+    #TODO: Add number households affected by interventions and other stats
+  } else {
+    data <- data %>%
+      group_by(hidp, run_id) %>%
+      mutate(disabled_flag = ifelse(any(S7_labour_state) == 'disabled', TRUE, FALSE)) %>%
+      filter(disabled_flag == TRUE) %>%
+      summarise(count = n(),
+                hh_income = weighted.mean(hh_income, w=weight, na.rm=TRUE),
+                SF_12 = weighted.mean(SF_12, w=weight, na.rm=TRUE)) %>%
+      mutate(total_cost = 0,
+             mean_cost = 0)
+  }
+  return(data)
+}
+
+###################### READ FUNCTIONS  ######################
 
 parallel_read_summarise <- function(file_paths, drop.dead = TRUE) {
   #no_cores <- detectCores() - 1  # Reserve one core for the system
   no_cores <- availableCores(omit=1)
   plan(multisession, workers = no_cores)  # Set up parallel plan
-  
+
   # Use future_lapply with file_paths and fread
   loaded.file.list <- future_lapply(file_paths, fread, stringsAsFactors = TRUE)
-  
+
   # Optionally convert data.tables to data.frames
   loaded.file.list <- lapply(loaded.file.list, as.data.frame)
-  
+
   if (drop.dead) {
     loaded.file.list <- lapply(loaded.file.list, drop_dead)
   }
-  
+
   # Add run_id to each dataframe
   for (i in seq_along(loaded.file.list)) {
     run_id <- extract_run_id(file_paths[i])
     loaded.file.list[[i]]$run_id <- as.numeric(str_remove(run_id, "^0+"))
   }
-  
+
+  # Add SIMD quintiles alongside deciles
+  loaded.file.list <- lapply(loaded.file.list, simd_generate_quintiles)
+
   # Create list for summarised outputs and process each type of summary
   summary.out.list <- list(
     whole_pop = do.call(rbind, lapply(loaded.file.list, whole_pop_summarise)),
     families = do.call(rbind, lapply(loaded.file.list, families_summarise)),
     treated = do.call(rbind, lapply(loaded.file.list, treated_summarise)),
-    simd_decile = do.call(rbind, lapply(loaded.file.list, group_summarise, 'simd_decile'))
+    simd_decile = do.call(rbind, lapply(loaded.file.list, group_summarise, 'simd_decile')),
+    simd_quintile = do.call(rbind, lapply(loaded.file.list, group_summarise, 'simd_quintile')),
+    priority_ethnicity = do.call(rbind, lapply(loaded.file.list, priority_summarise_ethnicity)),
+    priority_child_under_one = do.call(rbind, lapply(loaded.file.list, priority_summarise_child_under_one)),
+    priority_three_plus_children = do.call(rbind, lapply(loaded.file.list, priority_summarise_three_plus_children)),
+    priority_mother_under_25 = do.call(rbind, lapply(loaded.file.list, priority_summarise_mother_under_25)),
+    priority_disabled = do.call(rbind, lapply(loaded.file.list, priority_summarise_disabled))
   )
   rm(loaded.file.list)
-  
+
   return(summary.out.list)
 }
 
 read_and_sumarise_batch_1year <- function(out.path, scenario, year, drop.dead = TRUE, batch.size = 10) {
   scen.path <- here::here(out.path, scenario)
   scen.path <- get_latest_runtime_subdirectory(scen.path)
-  
+
   # Create file strings using year from args
   target.pattern <- paste0('[0-9]*_run_id_', year, '.csv')
   filepath.list <- list.files(path = scen.path,
                               pattern = target.pattern,
                               full.names = TRUE)
-  
+
   #print(paste0("Number of files to process in year ", year, ': ', length(filepath.list)))
-  
+
   # Split filepath.list into batches
   batches <- split(filepath.list, ceiling(seq_along(filepath.list)/batch.size))
-  
+
   output <- lapply(batches, parallel_read_summarise, drop.dead)
-  
+
   # Extract and bind 'whole_pop' tibbles
   whole_pop_combined <- bind_rows(lapply(output, `[[`, "whole_pop"))
   whole_pop_combined$time <- year
@@ -249,49 +347,49 @@ read_and_sumarise_batch_1year <- function(out.path, scenario, year, drop.dead = 
   # Extract and bind 'SIMD_decile' tibbles
   treated_simd_decile <- bind_rows(lapply(output, `[[`, "simd_decile"))
   treated_simd_decile$time <- year
-  
+
   combined_output <- list(whole_pop = whole_pop_combined,
                           families = families_combined,
                           treated = treated_combined,
                           simd_decile = treated_simd_decile)
-  
+
   return(combined_output)
 }
 
 
-read_batch_out_all_years_summarise <- function(out.path, scenario, start.year=2020, end.year=2035, verbose=TRUE, drop.dead = TRUE) {
+read_batch_out_all_years_summarise <- function(out.path, scenario, save.path, start.year=2020, end.year=2035, verbose=TRUE, drop.dead = TRUE) {
   print(paste0("Starting aggregation of output files for ", scenario, '...'))
-  
+
   all.years.list <- list()
   for (i in start.year:end.year) {
     if (verbose) { print(paste0("Aggregating files for year ", i)) }
     one.year.list <- read_and_sumarise_batch_1year(out.path, scenario, year=i, drop.dead)
     all.years.list[[i]] <- one.year.list
   }
-  
+
   # Final step is to collapse all the individual year summary dfs to a combined df over all years
   # Then write to file
   # Extract and bind 'whole_pop' tibbles
   whole_pop_combined <- bind_rows(lapply(all.years.list, `[[`, "whole_pop"))
   whole_pop_combined$scenario <- scenario
   write.csv(x = whole_pop_combined,
-            file = here::here(out.path, 'summary_files', paste0(scenario, '_whole_pop_summary.csv')))
+            file = here::here(save.path, paste0(scenario, '_whole_pop_summary.csv')))
   # Extract and bind 'families' tibbles
   families_combined <- bind_rows(lapply(all.years.list, `[[`, "families"))
   families_combined$scenario <- scenario
   write.csv(x = families_combined,
-            file = here::here(out.path, 'summary_files', paste0(scenario, '_families_summary.csv')))
+            file = here::here(save.path, paste0(scenario, '_families_summary.csv')))
   # Extract and bind 'treated' tibbles
   treated_combined <- bind_rows(lapply(all.years.list, `[[`, "treated"))
   treated_combined$scenario <- scenario
   write.csv(x = treated_combined,
-            file = here::here(out.path, 'summary_files', paste0(scenario, '_treated_summary.csv')))
+            file = here::here(save.path, paste0(scenario, '_treated_summary.csv')))
   # Extract and bind 'SIMD_deciles' tibbles
   simd_decile <- bind_rows(lapply(all.years.list, `[[`, "simd_decile"))
   simd_decile$scenario <- scenario
   write.csv(x = simd_decile,
-            file = here::here(out.path, 'summary_files', paste0(scenario, '_simd_decile_summary.csv')))
-  
+            file = here::here(save.path, paste0(scenario, '_simd_decile_summary.csv')))
+
   print("All output files successfully aggregated and summarised.")
   print("Output csv files saved to: ")
 }
@@ -300,74 +398,49 @@ read_batch_out_all_years_summarise <- function(out.path, scenario, start.year=20
 
 
 
-get_latest_runtime_subdirectory <- function(path) {
-  # first select only the path (not filenames)
-  out.folders <- list.files(path)
-  
-  # if more than 1, take the most recent
-  if(length(out.folders) == 1) {
-    path = paste0(path, '/', out.folders[1], '/')
-  }
-  else if(length(out.folders) > 1) {
-    out.folders.date <- as.POSIXlt(out.folders, format='%Y_%m_%d_%H_%M_%S')
-    
-    max.date <- max(out.folders.date)
-    
-    # Collecting these objects here as they have to be formatted
-    yr <- max.date$year + 1900 # year is years since 1900
-    month <- formatC(max.date$mon + 1, width=2, flag='0') # months are zero indexed (WHY??)
-    day <- formatC(max.date$mday, width=2, flag='0')
-    hour <- formatC(max.date$hour, width=2, flag='0')
-    min <- formatC(max.date$min, width=2, flag='0')
-    sec <- formatC(max.date$sec, width=2, flag='0')
-    
-    str.date <- paste0(yr, '_', 
-                       month, '_',  
-                       day, '_',
-                       hour, '_',
-                       min, '_',
-                       sec)
-    
-    path <- paste0(path, '/', str.date, '/')
-  }
-  return(path)
-}
+
 
 
 ################################# RUNNING SCRIPT #################################
 
-out.path <- here::here('output', 'scaled_scotland')
-save.path <- here::here(out.path, 'summary_files')
+args <- commandArgs(trailingOnly=TRUE)
+
+out.path <- here::here('output', args[1])
+save.path <- here::here(out.path, args[2])
+
+scen <- args[3]
 
 create.if.not.exists <- function(path) {
   if(!file.exists(path)) {
     dir.create(path = path)
   }
 }
-
 create.if.not.exists(save.path)
 
 
+
+
 read_batch_out_all_years_summarise(out.path,
-                                   scenario = 'baseline',
+                                   scenario = scen,
+                                   save.path = save.path,
                                    verbose = TRUE)
-read_batch_out_all_years_summarise(out.path,
-                                   scenario = '25UniversalCredit',
-                                   verbose = TRUE)
-read_batch_out_all_years_summarise(out.path,
-                      scenario = '30UniversalCredit',
-                      verbose = TRUE)
-read_batch_out_all_years_summarise(out.path,
-                      scenario = '35UniversalCredit',
-                      verbose = TRUE)
-read_batch_out_all_years_summarise(out.path,
-                      scenario = '40UniversalCredit',
-                      verbose = TRUE)
-read_batch_out_all_years_summarise(out.path,
-                      scenario = '45UniversalCredit',
-                      verbose = TRUE)
-read_batch_out_all_years_summarise(out.path,
-                                   scenario = '50UniversalCredit',
-                                   verbose = TRUE)
+# read_batch_out_all_years_summarise(out.path,
+#                                    scenario = '25UniversalCredit',
+#                                    verbose = TRUE)
+# read_batch_out_all_years_summarise(out.path,
+#                       scenario = '30UniversalCredit',
+#                       verbose = TRUE)
+# read_batch_out_all_years_summarise(out.path,
+#                       scenario = '35UniversalCredit',
+#                       verbose = TRUE)
+# read_batch_out_all_years_summarise(out.path,
+#                       scenario = '40UniversalCredit',
+#                       verbose = TRUE)
+# read_batch_out_all_years_summarise(out.path,
+#                       scenario = '45UniversalCredit',
+#                       verbose = TRUE)
+# read_batch_out_all_years_summarise(out.path,
+#                                    scenario = '50UniversalCredit',
+#                                    verbose = TRUE)
 
 print('All complete!')
--- scripts/arc_SCP_summarise.sh
+++ scripts/arc_SCP_summarise.sh
@@ -12,15 +12,12 @@
 #$ -m a
 ## Select memory
 #$ -l h_vmem=15G # was 15 for big runs
-## Choose cores. See arc website for more details. 5 high memory cores chosen here.
+## Choose cores. See arc website for more details.
 #$ -pe smp 8
 ## Set logs directories
 #$ -o ./logs/sum/log
 #$ -e ./logs/sum/errors
 
-mkdir -p logs/sum
-mkdir -p logs/sum/log
-mkdir -p logs/sum/errors
 
 #Rscript -e "require(rmarkdown); render('minos/utils_datain_test.Rmd')"
 Rscript minos/utils_datain_test.R
