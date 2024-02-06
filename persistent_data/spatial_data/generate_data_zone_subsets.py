6f25555f2cdef427d59e99de5523a6c52a12cb9c
paddy-r <h.p.rice@leeds.ac.uk>
2024-02-06 12:53:14 +0000

    Tweaks to zone generation to make easier to modify in future

--- persistent_data/spatial_data/generate_data_zone_subsets.py
+++ persistent_data/spatial_data/generate_data_zone_subsets.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import os
+from os.path import dirname as up
 
 
 def main():
 
     # load in full UK wide lookup.
     # get this data here https://geoportal.statistics.gov.uk/datasets/0d11d17118ec44fcbe890fed53df4a9d_0/explore
-    lookup_file_name = "persistent_data/spatial_data/Output_Area_to_LSOA_to_MSOA_to_Local_Authority_District_(December_2017)_Lookup_with_Area_Classifications_in_Great_Britain.csv"
+    spatial_path = up(__file__)
+    lookup_file_name = os.path.join(spatial_path, "Output_Area_to_LSOA_to_MSOA_to_Local_Authority_District_(December_2017)_Lookup_with_Area_Classifications_in_Great_Britain.csv")
     lsoa_lookup = pd.read_csv(lookup_file_name)
 
     # subsetting data from desired regions.
@@ -21,10 +24,11 @@ def main():
     scotland_data_zones = scotland_subset['LSOA11CD']
 
     #saving
-    sheffield_lsoas.to_csv("persistent_data/spatial_data/sheffield_lsoas.csv")
-    manchester_lsoas.to_csv("persistent_data/spatial_data/manchester_lsoas.csv")
-    glasgow_data_zones.to_csv("persistent_data/spatial_data/glasgow_data_zones.csv")
-    scotland_data_zones.to_csv("persistent_data/spatial_data/scotland_data_zones.csv")
+    sheffield_lsoas.to_csv(os.path.join(spatial_path, "sheffield_lsoas.csv"))
+    manchester_lsoas.to_csv(os.path.join(spatial_path, "manchester_lsoas.csv"))
+    glasgow_data_zones.to_csv(os.path.join(spatial_path, "glasgow_data_zones.csv"))
+    scotland_data_zones.to_csv(os.path.join(spatial_path, "scotland_data_zones.csv"))
+
 
 if __name__ == '__main__':
     main()
