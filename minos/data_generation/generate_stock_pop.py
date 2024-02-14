17705a661f010226712627791b55a597bdbcbb56
Luke Archer <l.archer@leeds.ac.uk>
2024-02-14 10:02:46 +0000

    Fixed synthetic pop preparation for uk scaled simulation

--- minos/data_generation/generate_stock_pop.py
+++ minos/data_generation/generate_stock_pop.py
@@ -190,6 +190,11 @@ def generate_stock(projections, cross_validation):
                           copy_year=2020,
                           paste_year=2021,
                           var_type='ordinal')
+    data = wave_data_copy(data,
+                          var='heating',
+                          copy_year=2014,
+                          paste_year=2015,
+                          var_type='ordinal')
     # data = wave_data_copy(data,
     #                       var='matdep',
     #                       copy_year=2020,
