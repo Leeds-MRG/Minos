a3326b2ad378a2301e08b2235388d52971957df7
RobertClay <robclay20@hotmail.co.uk>
2024-04-15 17:14:17 +0100

    simpler yearly energy. why arent you running?

--- scripts/arc_run_longitudinal_transitions.sh
+++ scripts/arc_run_longitudinal_transitions.sh
@@ -10,9 +10,9 @@
 ## Email if a run aborts
 #$ -m a
 ## Select memory
-#$ -l h_vmem=10G # was 15 for big runs
+#$ -l h_vmem=4G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
-#$ -pe smp 10
+#$ -pe smp 8
 ## Set logs directories
 #$ -o ./logs/transitions/
 #$ -e ./logs/transitions/
