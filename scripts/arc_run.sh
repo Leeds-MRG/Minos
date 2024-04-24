d021eeac2702f8f6305c0b0d2485c79928f06583
Luke Archer <l.archer@leeds.ac.uk>
2024-04-24 17:57:35 +0100

    Changed arc run scripts to email on completion of job and increased memory requests

--- scripts/arc_run.sh
+++ scripts/arc_run.sh
@@ -8,10 +8,10 @@
 #$ -V
 ## Request hours of runtime
 #$ -l h_rt=48:00:00
-## Email if a run aborts
-#$ -m a
+## Email if a run aborts or finishes
+#$ -m ae
 ## Select memory
-#$ -l h_vmem=2G # was 15 for big runs
+#$ -l h_vmem=15G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
 #$ -pe smp 1
 ## Set logs directories
