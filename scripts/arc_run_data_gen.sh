26a63b984137a0f8581496670830b670d23172a1
RobertClay <robclay20@hotmail.co.uk>
2024-04-25 10:27:55 +0100

    adding datagen arc script

--- scripts/arc_run_data_gen.sh
+++ scripts/arc_run_data_gen.sh
@@ -6,17 +6,19 @@
 ## Use current environment variables and modules
 #$ -V
 ## Request hours of runtime
-#$ -l h_rt=48:00:00
+#$ -l h_rt=2:00:00
 ## Email if a run aborts
 #$ -m a
 ## Select memory
 #$ -l h_vmem=2G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
-#$ -pe smp 1
+#$ -pe smp 8
 ## Set logs directories
 #$ -o ./logs/transitions/
 #$ -e ./logs/transitions/
 
-mkdir -p ./logs/transitions
-Rscript minos/transitions/estimate_transitions.R
-python3 minos/data_generation/generate_repl_pop.py
+python3 minos/data_generation/US_format_raw.py --source_dir ../UKDA-6614-stata/stata/stata13_se/
+python3 minos/data_generation/US_missing_main.py
+python3 minos/data_generation/generate_composite_vars.py
+python3 minos/data_generation/US_complete_case.py
+python3 minos/data_generation/generate_stock_pop.py
--- scripts/arc_submit_transitions.sh
+++ scripts/arc_submit_transitions.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#qrsh -l h_rt=00:15:00,h_vmem=4G -pe smp 2
-qsub -pe 8 'scripts/arc_run_transitions.sh'
+qsub 'scripts/arc_run_transitions.sh'
