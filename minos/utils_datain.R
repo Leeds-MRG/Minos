430b44b1132f850d0994bf92fff27fd80c2624b0
RobertClay <robclay20@hotmail.co.uk>
2024-04-26 14:48:58 +0100

    other things needed for MICE/manc config

--- minos/utils_datain.R
+++ minos/utils_datain.R
@@ -198,3 +198,79 @@ get_summary_out <- function(scenario_out_path, year, var.list) {
 extract_child_ages <- function(child_ages_string){
   return (as.numeric(unlist(strsplit(child_ages_string, "_"))))
 }
+
+
+# Function to read all output files from a singular (non-batch) local simulation run
+# This will read output files for all years within the latest runtime subdirectory
+# (which is automatically determined)
+# Args: 
+#       out.path - path to top level output directory
+#       scenario - string scenario name of which output files to read
+read_all_UKHLS_waves <- function(out.path, scenario) {
+  ## Start with scenario name
+  # get runtime directory
+  # list files
+  # return do.call(...)
+  
+  scen.path <- paste0(out.path, scenario)
+  files <- list.files(scen.path,
+                      pattern = '[0-9]{4}_US_cohort.csv',
+                      full.names = TRUE)
+  dat <- do.call(rbind, lapply(files, read.csv))
+  return(dat)
+}
+
+
+
+save_raw_data_in <- function(data, data.path) {
+  ## save all data after processing.
+  
+  for (year_time in unique(data$time)) {
+    yearly_file_name <- paste0(data.path, year_time, "_US_cohort.csv")
+    yearly_df <- data[which(data$time == year_time),]
+    write.csv(yearly_df, file=yearly_file_name, row.names=FALSE)
+    print(paste0("Saved file to: ", yearly_file_name, "."))
+  }
+}
+
+
+# Function to find the latest runtime subdirectory (most recent)
+# Reads all directories in a given path and returns the most recent subdir
+# This requires all folders in the given path to be named with a specific 
+# format, which is a datetime from Year to Second. If any directories in the
+# path are not in this format, the function will fail somewhow.
+# Args: 
+#       path - path to a directory containing runtime subdirectories
+get_latest_runtime_subdirectory <- function(path) {
+  # first select only the path (not filenames)
+  out.folders <- list.files(path)
+  
+  # if more than 1, take the most recent
+  if(length(out.folders) == 1) {
+    path = paste0(path, '/', out.folders[1], '/')
+  }
+  else if(length(out.folders) > 1) {
+    out.folders.date <- as.POSIXlt(out.folders, format='%Y_%m_%d_%H_%M_%S')
+    
+    max.date <- max(out.folders.date)
+    
+    # Collecting these objects here as they have to be formatted
+    yr <- max.date$year + 1900 # year is years since 1900
+    month <- formatC(max.date$mon + 1, width=2, flag='0') # months are zero indexed (WHY??)
+    day <- formatC(max.date$mday, width=2, flag='0')
+    hour <- formatC(max.date$hour, width=2, flag='0')
+    min <- formatC(max.date$min, width=2, flag='0')
+    sec <- formatC(max.date$sec, width=2, flag='0')
+    
+    str.date <- paste0(yr, '_', 
+                       month, '_',  
+                       day, '_',
+                       hour, '_',
+                       min, '_',
+                       sec)
+    
+    path <- paste0(path, '/', str.date, '/')
+  }
+  return(path)
+}
+
