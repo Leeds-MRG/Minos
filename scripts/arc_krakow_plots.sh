2eab6231b035641a4afd7f2453b1b938c7181737
RobertClay <robclay20@hotmail.co.uk>
2024-05-20 16:38:47 +0100

    adding krk bash script for arc runs

---
+++ scripts/arc_krakow_plots.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This script must be run with qsub if running on Arc
+
+## Set current working directory
+#$ -cwd
+## Use current environment variables and modules
+#$ -V
+## Request hours of runtime
+#$ -l h_rt=0:30:00
+## Email if a run aborts
+#$ -m a
+## Select memory
+#$ -l h_vmem=10G # was 15 for big runs
+## Choose cores. See arc website for more details. 5 high memory cores chosen here.
+#$ -pe smp 8
+## Set logs directories
+#$ -o ./logs/krakow_plots/
+#$ -e ./logs/krakow_plots/
+
+mkdir -p ./logs/krakow_plots # make dir if not exists
+
+# general lineplots
+
+python3 minos/outcomes/make_lineplots_macros.py "energy_manchester_scaled" "EPCG_GBIS_SF_12" "SF_12"
+python3 minos/outcomes/make_lineplots_macros.py "energy_manchester_scaled" "EPCG_GBIS_intervention_cost" "boost_amount"
+python3 minos/outcomes/make_lineplots_macros.py "energy_manchester_scaled" "EPCG_GBIS_yearly_energy" "yearly_energy"
+
+# quintile plots
+
+python3 minos/outcomes/make_lineplots_macros.py "energy_manchester_scaled" "EPCG_quintiles" "SF_12"
+python3 minos/outcomes/make_lineplots_macros.py "energy_manchester_scaled" "GBIS_quintiles" "SF_12"
+
+# maps
+
+python3 minos/outcomes/format_spatial_output.py -i "baseline" -y 2024 -m "energy_manchester_scaled" -s who_poor_heating -p true -r "manchester" -v "SF_12"
+python3 minos/outcomes/format_spatial_output.py -i "EPCG" -y 2024 -m "energy_manchester_scaled" -s who_boosted -p true -r "manchester"  -v "SF_12"
+RScript minos/outcomes/sf12_difference_map.R -m "energy_manchester_scaled" -b "baseline" -i "EPCG" -r "manchester" -y 2024 -v "SF_12" -s true
+
+python3 minos/outcomes/format_spatial_output.py -i "baseline" -y 2024 -m "energy_manchester_scaled" -s who_poor_heating -p true -r "manchester" -v "SF_12"
+python3 minos/outcomes/format_spatial_output.py -i "GBIS" -y 2024 -m "energy_manchester_scaled" -s who_boosted -p true -r "manchester"  -v "SF_12"
+Rscript minos/outcomes/sf12_difference_map.R -m "energy_manchester_scaled" -b "baseline" -i "GBIS" -r "manchester" -y 2024 -v "SF_12" -s true
+
+
