4aa57056595b21914116c70804f88351238cb372
Luke Archer <l.archer@leeds.ac.uk>
2024-05-24 13:26:09 +0100

    Removed some additional summaries that we dont need for child pov work

--- minos/read_summarise_2.R
+++ minos/read_summarise_2.R
@@ -700,14 +700,14 @@ summary_funcs <- c(whole_pop = whole_pop_summarise,
                    families = families_summarise,
                    families_income_quint = families_income_quint_summarise,
                    treated = treated_summarise,
-                   priority_any = priority_summarise_any,
-                   UC = UC_summarise,
-                   UC_rel_pov = UC_rel_pov_summarise,
-                   UC_kids_rel_pov = UC_kids_rel_pov_summarise,
-                   UC_abs_pov = UC_abs_pov_summarise,
-                   UC_kids_abs_pov = UC_kids_abs_pov_summarise,
-                   UC_gender = UC_gender_summarise
+                   priority_any = priority_summarise_any
                    )
+# UC = UC_summarise,
+# UC_rel_pov = UC_rel_pov_summarise,
+# UC_kids_rel_pov = UC_kids_rel_pov_summarise,
+# UC_abs_pov = UC_abs_pov_summarise,
+# UC_kids_abs_pov = UC_kids_abs_pov_summarise,
+# UC_gender = UC_gender_summarise
 #                   priority_any2 = priority_summarise_any2,
 #                   priority_num = priority_summarise_num
 
