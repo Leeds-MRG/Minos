91ae2a46a0f13fe8ddedaab71cff04076e031b87
RobertClay <robclay20@hotmail.co.uk>
2024-05-31 10:41:13 +0100

    type error for finsit

--- minos/modules/financial_situation.py
+++ minos/modules/financial_situation.py
@@ -97,6 +97,7 @@ class financialSituation(Base):
         #nextWaveFinancialPerception["financial_situation"] = nextWaveFinancialPerception["financial_situation"].astype(int)
         # Draw individuals next states randomly from this distribution.
         # Update population with new income.
+        nextWaveFinancialPerception['financial_situation'] = nextWaveFinancialPerception['financial_situation'].astype(int)
         self.population_view.update(nextWaveFinancialPerception['financial_situation'])
 
     def calculate_financial_situation(self, pop):
