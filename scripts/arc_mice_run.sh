5d98f8d16514224e675caa0f5a9ec0dade925fa7
Luke Archer <l.archer@leeds.ac.uk>
2024-05-31 09:36:38 +0100

    Reduced the memory request of arc_mice_run as the previous request of a whole node has been queued for about 15 hours

--- scripts/arc_mice_run.sh
+++ scripts/arc_mice_run.sh
@@ -11,14 +11,18 @@
 ## Email if a run aborts
 #$ -m a
 ## Select memory
-## just use one node
-#$ -l nodes=1
+#$ -l h_vmem=5G # was 15 for big runs
+## Choose cores. See arc website for more details.
+#$ -pe smp 6
 ## Set logs directories
 #$ -o ./logs/log
 #$ -e ./logs/errors
 
+# Not any more...
+##### just use one node
+##### -l nodes=1
+
 ############## SET NUMBER OF RUNS HERE ##############
 ## Tell computer this is an array job with tasks from 1 to N
-#$ -t 1-1
 
-Rscript minos/data_generation/US_MICE_imputation.R -n 30 -i 10
+Rscript minos/data_generation/US_MICE_imputation.R -n 1 -i 10
