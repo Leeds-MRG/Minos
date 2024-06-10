4e9ffff562d07d5456d29fd6b3e1145841046f3b
Luke Archer <l.archer@leeds.ac.uk>
2024-06-10 14:27:36 +0100

    Fixed typo in the function list for combine_summaries_all_years

--- minos/combine_summaries_all_years.R
+++ minos/combine_summaries_all_years.R
@@ -874,9 +874,11 @@ scen <- args[[3]]
 
 # Create named list of summary functions to go through
 summary_funcs <- c(whole_pop = whole_pop_summarise,
-                   whole_pop_income_quint = whole_pop_income_quintile_summarise,
+                   whole_pop_income_quint = whole_pop_income_quint_summarise,
+                   whole_pop_income_quint2 = whole_pop_income_quint2_summarise,
                    families = families_summarise,
                    families_income_quint = families_income_quint_summarise,
+                   families_income_quint2 = families_income_quint2_summarise,
                    treated_relative = treated_relative_summarise,
                    treated_absolute = treated_absolute_summarise,
                    priority_any = priority_any_summarise,
