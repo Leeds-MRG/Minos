73a32441edcd3493213a531583a04d42e87e75d2
ld-archer <l.archer@leeds.ac.uk>
2024-07-11 15:45:32 +0100

    Testing more noise for MARS income module and added a RandomForest income module

--- minos/modules/income.py
+++ minos/modules/income.py
@@ -1230,15 +1230,6 @@ class marsIncome(Base):
         newWaveIncome['hidp'] = pop['hidp']
         newWaveIncome = newWaveIncome.groupby('hidp').apply(select_random_income).reset_index(drop=True)
 
-        # # calculate household income mean
-        # income_mean = np.mean(newWaveIncome["hh_income"])
-        # # calculate change in standard deviation between waves.
-        # std_ratio = (np.std(pop['hh_income_last'])/np.std(newWaveIncome["hh_income"]))
-        # # rescale income to have new mean but keep old standard deviation.
-        # newWaveIncome["hh_income"] *= std_ratio
-        # newWaveIncome["hh_income"] -= ((std_ratio-1)*income_mean)
-        # #newWaveIncome["hh_income"] -= 75
-
         # difference in hh income
         newWaveIncome['hh_income_diff'] = newWaveIncome['hh_income'] - pop['hh_income_last']
 
@@ -1258,11 +1249,137 @@ class marsIncome(Base):
             Vector of new household incomes from OLS prediction.
         """
 
-        nextWaveIncome = r_utils.predict_next_MARS(self.transition_model,
-                                                   self.rpy2Modules,
+        nextWaveIncome = r_utils.predict_next_MARS(model=self.transition_model,
+                                                   rpy2_modules=self.rpy2Modules,
                                                    current=pop,
                                                    dependent='hh_income',
                                                    seed=self.run_seed,
-                                                   noise_std=0)
+                                                   noise_std=500)
+
+        return nextWaveIncome
+
+
+class RFIncome(Base):
+
+    # Special methods used by vivarium.
+    @property
+    def name(self):
+        return 'rfIncome'
+
+    def __repr__(self):
+        return "RFIncome()"
+
+    def setup(self, builder):
+        """ Initialise the module during simulation.setup().
+
+        Notes
+        -----
+        - Load in data from pre_setup
+        - Register any value producers/modifiers for income
+        - Add required columns to population data frame
+        - Update other required items such as randomness stream.
+
+        Parameters
+        ----------
+        builder : vivarium.engine.Builder
+            Vivarium's control object. Stores all simulation metadata and allows modules to use it.
+
+        """
+
+        # Load in inputs from pre-setup.
+        # self.transition_model = builder.data.load("income_transition")
+        self.rpy2Modules = builder.data.load("rpy2_modules")
+
+        # Build vivarium objects for calculating transition probabilities.
+        # Typically this is registering rate/lookup tables. See vivarium docs/other modules for examples.
+        # self.transition_coefficients = builder.
+
+        # Assign randomness streams if necessary.
+        self.random = builder.randomness.get_stream(self.generate_run_crn_key())
+
+        # Determine which subset of the main population is used in this module.
+        # columns_created is the columns created by this module.
+        # view_columns is the columns from the main population used in this module.
+        # In this case, view_columns are taken straight from the transition model
+        view_columns = ["age",
+                        "sex",
+                        "ethnicity",
+                        "region",
+                        "education_state",
+                        'job_sec',
+                        'SF_12',
+                        'pidp',
+                        'hh_income',
+                        'hh_income_diff',
+                        'S7_labour_state',
+                        'time',
+                        'hidp'
+                        ]
+        #columns_created = ['hh_income_diff']
+        # view_columns += self.transition_model.rx2('model').names
+        self.population_view = builder.population.get_view(columns=view_columns)  # columns_created
+
+        # Population initialiser. When new individuals are added to the microsimulation a constructer is called for each
+        # module. Declare what constructer is used. usually on_initialize_simulants method is called. Inidividuals are
+        # created at the start of a model "setup" or after some deterministic (add cohorts) or random (births) event.
+        #builder.population.initializes_simulants(self.on_initialize_simulants)  # , creates_columns=columns_created
+
+        # Declare events in the module. At what times do individuals transition states from this module. E.g. when does
+        # individual graduate in an education module.
+        # builder.event.register_listener("time_step", self.on_time_step, priority=self.priority)
+        super().setup(builder)
+
+        # just load this once.
+        self.rf_transition_model = r_utils.load_transitions(f"hh_income/rf/hh_income_RF",
+                                                            self.rpy2Modules,
+                                                            path=self.transition_dir)
+
+    def on_time_step(self, event):
+        """ Predicts the hh_income for the next timestep.
+
+        Parameters
+        ----------
+        event : vivarium.population.PopulationEvent
+            The event time_step that called this function.
+        """
+        # Get living people to update their income
+        pop = self.population_view.get(event.index, query="alive =='alive'")
+        self.year = event.time.year
+
+        pop['hh_income_last'] = pop['hh_income']
+
+        #pop['hh_income_diff_last'] = pop['hh_income_diff']
+        ## Predict next income value
+        newWaveIncome = pd.DataFrame(columns=['hh_income'])
+        newWaveIncome['hh_income'] = self.calculate_income(pop)
+        newWaveIncome.index = pop.index
+
+        # Ensure whole household has equal hh_income by taking mean after prediction
+        newWaveIncome['hidp'] = pop['hidp']
+        newWaveIncome = newWaveIncome.groupby('hidp').apply(select_random_income).reset_index(drop=True)
+
+        # difference in hh income
+        newWaveIncome['hh_income_diff'] = newWaveIncome['hh_income'] - pop['hh_income_last']
+
+        self.population_view.update(newWaveIncome[['hh_income', 'hh_income_diff']])
+
+    def calculate_income(self, pop):
+        """Calculate income transition distribution based on provided people/indices
+
+        Parameters
+        ----------
+            pop: PopulationView
+                Population from MINOS to calculate next income for.
+        Returns
+        -------
+        nextWaveIncome: pd.Series
+            Vector of new household incomes from OLS prediction.
+        """
+        nextWaveIncome = r_utils.predict_next_rf(self.rf_transition_model,
+                                                 self.rpy2Modules,
+                                                 pop,
+                                                 dependent='hh_income',
+                                                 seed=self.run_seed,
+                                                 noise=0)
 
         return nextWaveIncome
