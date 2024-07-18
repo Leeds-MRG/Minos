fixing some variables and scaling

minos/modules/ncars.py
@@ -110,7 +110,7 @@ class nCars(Base):
         std_ratio = (np.std(pop.loc[pop['ncars']>0, 'ncars'])/np.std(non_zero_ncars['ncars']))
         #non_zero_ncars["ncars"] *= std_ratio
         #non_zero_ncars["ncars"] -= ((std_ratio-1)*ncars_mean)
-        #non_zero_ncars['ncars'] += 0.175 #0.175
+        non_zero_ncars['ncars'] += 0.105 #0.175
         newWaveNCars.loc[newWaveNCars['ncars']>0, 'ncars'] = non_zero_ncars['ncars']
 
         newWaveNCars['ncars'] = newWaveNCars['ncars'].clip(0, 10)

minos/modules/tobacco.py
@@ -247,17 +247,21 @@ class lmmTobacco(Base):
         newWaveTobacco["ncigs"] = np.clip(newWaveTobacco['ncigs'], 0, 50)
 
         non_zero_ncigs = newWaveTobacco.loc[newWaveTobacco['ncigs']>0, ]
+        non_zero_ncigs['ncigs'] = 5 * (np.ceil((non_zero_ncigs['ncigs'] / 5))) # change from np.round
+
         tobacco_mean = np.mean(non_zero_ncigs["ncigs"])
-        std_ratio = (np.std(pop.loc[pop['ncigs']>0, 'ncigs'])/np.std(non_zero_ncigs['ncigs']))
-        #non_zero_ncigs["ncigs"] *= std_ratio
-        #non_zero_ncigs["ncigs"] -= ((std_ratio-1)*tobacco_mean)
+        std_ratio = (np.std(pop.loc[pop['ncigs']>0, 'ncigs'])/np.std(non_zero_ncigs['ncigs']))*0.75
+        non_zero_ncigs["ncigs"] *= std_ratio
+        non_zero_ncigs["ncigs"] -= ((std_ratio-1)*tobacco_mean)
+
         #non_zero_ncigs["ncigs"] += 0.2
         #non_zero_ncigs['ncigs'] *= 1.05
-        non_zero_ncigs['ncigs'] = 5 * (np.round((non_zero_ncigs['ncigs'] / 5)))
-        non_zero_ncigs.loc[non_zero_ncigs['ncigs'] == 0, 'ncigs'] += 5
+
+        # UNHASH THIS IF SCALING FAILS.
+        #non_zero_ncigs.loc[non_zero_ncigs['ncigs'] == 0, 'ncigs'] += 5
 
         newWaveTobacco.loc[newWaveTobacco['ncigs']>0, 'ncigs'] = non_zero_ncigs['ncigs']
-        #newWaveTobacco['ncigs'] = newWaveTobacco['ncigs'].clip(lower=0)
+        newWaveTobacco['ncigs'] = newWaveTobacco['ncigs'].clip(lower=0)
 
         #newWaveTobacco['ncigs'] *= 1.1
         newWaveTobacco["ncigs"] = np.round(newWaveTobacco["ncigs"]).astype(int)