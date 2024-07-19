96253369a7aa48bda01ab925dd3d6af8d6cd0d5b
RobertClay <robclay20@hotmail.co.uk>
2024-07-19 09:04:57 +0100

    more memory

--- scripts/arc_run.sh
+++ scripts/arc_run.sh
@@ -11,7 +11,7 @@
 ## Email if a run aborts
 #$ -m a
 ## Select memory
-#$ -l h_vmem=25G # was 15 for big runs
+#$ -l h_vmem=30G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
 #$ -pe smp 1
 ## Set logs directories
