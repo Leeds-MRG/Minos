450076e5a2aeabdd161e47dcccce3d93d2406d62
RobertClay <robclay20@hotmail.co.uk>
2024-07-25 11:46:51 +0100

    upping transitions script resources

--- scripts/arc_run_transitions.sh
+++ scripts/arc_run_transitions.sh
@@ -6,11 +6,11 @@
 ## Use current environment variables and modules
 #$ -V
 ## Request hours of runtime
-#$ -l h_rt=00:15:00
+#$ -l h_rt=00:30:00
 ## Email if a run aborts
 #$ -m a
 ## Select memory
-#$ -l h_vmem=2G # was 15 for big runs
+#$ -l h_vmem=20G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
 #$ -pe smp 1
 ## Set logs directories
