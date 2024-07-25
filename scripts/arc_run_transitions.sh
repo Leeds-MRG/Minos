58705f8cf6ef55c1c7ccd206f01c60524c814ca6
RobertClay <robclay20@hotmail.co.uk>
2024-07-25 11:42:55 +0100

    adding more to arc script to run whole manchester synth pipe

--- scripts/arc_run_transitions.sh
+++ scripts/arc_run_transitions.sh
@@ -20,3 +20,5 @@
 mkdir -p ./logs/transitions
 Rscript minos/transitions/estimate_transitions.R
 python3 minos/data_generation/generate_repl_pop.py
+python3 minos/data_generation/US_household_upscaling.py -r 'manchester' -p 5
+python3/generate_repl_pop.py --region 'manchester'
