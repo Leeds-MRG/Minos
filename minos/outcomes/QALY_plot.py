f788994e5ae0573ca28c4f3d5643eb887eafca5c
RobertClay <robclay20@hotmail.co.uk>
2024-08-08 16:37:15 +0100

    change ordering in qaly plot to match everything else

--- minos/outcomes/QALY_plot.py
+++ minos/outcomes/QALY_plot.py
@@ -17,12 +17,12 @@ from scipy.integrate import cumulative_simpson, cumulative_trapezoid
 def QALY_lineplot(data, prefix, destination="plots/"):
 
     #sort by time and run id.
-    data.sort_values(by=['tag', 'run_id', 'year'], inplace=True)
+    data.sort_values(by=['year'], inplace=True)
     # note needs initial 0 in cumulative_simposon at the front to maintain array shape.
     data["QALYs_cumsum"] = data.groupby(by=["tag", 'run_id'])["QALYs"].transform(lambda x: cumulative_simpson(x, dx=1,initial=0))
     data["total_boost_cumsum"] = data.groupby(by=["tag", 'run_id'])["total_boost"].transform(np.cumsum)
     # cum sum qaly.
-    data.reset_index(inplace=True, drop=True)
+    data.reset_index(inplace=True)
     data = data.groupby(by=['tag','run_id', 'year'], as_index=False).agg({"QALYs_cumsum": np.mean, "total_boost_cumsum": np.mean})
     data['QALYs_cumsum_diff'] = data['QALYs_cumsum'] - np.tile(data.loc[data['tag']=="Baseline", "QALYs_cumsum"].values, len(data['tag'].value_counts()))
     #data['QALYs_cumsum_diff'] = data['QALYs_cumsum'] - np.repeat(data.loc[data['tag']=="Baseline", "QALYs_cumsum"].values, len(data['tag'].value_counts()))
@@ -41,7 +41,7 @@ def QALY_lineplot(data, prefix, destination="plots/"):
 def ICER_lineplot(data, prefix, destination="plots/"):
 
     #sort by time and run id.
-    data.sort_values(by=['tag', 'run_id', 'year'], inplace=True)
+    data.sort_values(by=['year'], inplace=True)
 
     # note needs initial 0 in cumulative_simposon at the front to maintain array shape.
     data["QALYs_cumsum"] = data.groupby(by=["tag", 'run_id'])["QALYs"].transform(lambda x: cumulative_simpson(x, dx=1,initial=0))
