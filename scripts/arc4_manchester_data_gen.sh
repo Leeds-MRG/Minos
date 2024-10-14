b5cb3ebf15ec51a9dc4c6b9a4746de54fca9684a
RobertClay <robclay20@hotmail.co.uk>
2024-10-14 13:58:09 +0100

    fixing error in arc4 manchester generation.

--- scripts/arc4_manchester_data_gen.sh
+++ scripts/arc4_manchester_data_gen.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e # throws error if any command fails. otherwise would just keep going.
 
-make data
-make transitions_default
-make manchester_scaled_data
-make synthetic_manchester_repl
+make synthetic_manchester_data
