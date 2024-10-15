5dea57d95dd64281d175e429573e0bbd06ede71b
Luke Archer <l.archer@leeds.ac.uk>
2024-10-15 17:44:06 +0100

    Small changed to arc run scripts, mostly for memory allocation

--- scripts/arc_run.sh
+++ scripts/arc_run.sh
@@ -11,7 +11,7 @@
 ## Email if a run aborts or finishes
 #$ -m ae
 ## Select memory
-#$ -l h_vmem=15G # was 15 for big runs
+#$ -l h_vmem=20G # was 15 for big runs
 ## Choose cores. See arc website for more details. 5 high memory cores chosen here.
 #$ -pe smp 1
 ## Set logs directories
@@ -20,7 +20,7 @@
 
 ############## SET NUMBER OF RUNS HERE ##############
 ## Tell computer this is an array job with tasks from 1 to N
-#$ -t 1-10
+#$ -t 1-50
 
 # create these if they dont exist. Will crash arc4 if you dont do this.
 mkdir -p logs
