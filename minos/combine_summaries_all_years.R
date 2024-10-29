2565e03cffa0efe64bb486efc17f78cc44356ddc
Luke Archer <l.archer@leeds.ac.uk>
2024-10-29 10:54:30 +0000

    Adding latest summary and vis scripts to this reverted branch, plus some Makefile targets for running and summarising the reduced run set

--- minos/combine_summaries_all_years.R
+++ minos/combine_summaries_all_years.R
@@ -880,6 +880,10 @@ scen <- args[[3]]
 
 
 # Create named list of summary functions to go through
+# summary_funcs <- c(whole_pop_income_quint = whole_pop_income_quint_summarise,
+#                    families_income_quint = families_income_quint_summarise
+# )
+
 summary_funcs <- c(whole_pop = whole_pop_summarise,
                    whole_pop_income_quint = whole_pop_income_quint_summarise,
                    families = families_summarise,
