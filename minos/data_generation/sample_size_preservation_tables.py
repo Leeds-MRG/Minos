c45cd29fc3154e412166eb28bcc13005b0a1aab1
RobertClay <robclay20@hotmail.co.uk>
2025-01-14 10:53:02 +0000

    adding short script on statistics for sample size preservatino through data preprocessing.

---
+++ minos/data_generation/sample_size_preservation_tables.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+def main():
+
+    # Load in datasets. Get number of columns by source and by year.
+    # Plot in 2x2 grid by year and by processing step.
+
+    out = pd.DataFrame()
+
+    processing_steps = ["raw_US", "corrected_US", "mice_US", "imputed_complete_US", "imputed_final_US"]
+    years = np.arange(2009, 2021, 1)
+    for file_path in processing_steps:
+        n_columns = []
+        for year in years:
+            try: # mildly stupid but easiest way to get around MICE imputation not doing all years of data.
+                df = pd.read_csv(f"data/{file_path}/{year}_US_cohort.csv")
+                n_columns.append(df.shape[0])
+            except:
+                print(f"No data for source {file_path} for year {year}.")
+                n_columns.append(np.nan)
+        out[file_path] = n_columns
+
+    print(out)
+
+if __name__ == '__main__':
+    main()
