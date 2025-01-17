fd2bdd0d377019349082906c3de7864868e542d6
RobertClay <robclay20@hotmail.co.uk>
2025-01-17 11:13:55 +0000

    migratingto AIRE

---
+++ scripts/aire_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+################
+# Slurm settings
+################
+#SBATCH --job-name=parallel_minos_job      # Job name
+#SBATCH --mail-type=FAIL             # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=gyrc@leeds.ac.uk # Where to send mail
+#SBATCH --nodes=1                    # Run all processes on a single node
+#SBATCH --ntasks=1                   # Run a single task
+#SBATCH --cpus-per-task=2            # Number of CPU cores per task
+#SBATCH --mem=40gb                     # Job memory request
+#SBATCH --time=12:00:00               # Time limit hrs:min:sec
+#SBATCH --output=logs/errors/minos_batch-%A-%a.out   # Standard output log.
+#SBATCH --error=logs/logs/minos_batch-%A-%a.err   # Standard output log.
+
+#../gpu-burn/gpu_burn # GPU testing.. ignore.
+
+mkdir -p logs #make logs directory if it doesn't exist.
+mkdir -p logs/logs #make logs directory if it doesn't exist.
+mkdir -p logs/errors #make logs directory if it doesn't exist.
+
+echo "Running Minos task $SLURM_JOBID on $SLURM_CPUS_ON_NODE CPU cores"
+echo "Running task $SLURM_ARRAY_TASK_ID of $SLURM_ARRAY_TASK_MAX"
+
+if [ "$#" -eq 6 ]; then
+  echo "Running baseline MINOS simulation"
+  python3 scripts/run.py -c $2 -o $4 -t $6 -r $SLURM_ARRAY_TASK_ID
+elif [ "$#" -eq 8 ]; then
+  echo "Running MINOS simulation with $6"
+  python3 scripts/run.py -c $2 -o $4 -i $6 -t $8 -r $SLURM_ARRAY_TASK_ID
+fi
+
+# no errors
+exit 0
