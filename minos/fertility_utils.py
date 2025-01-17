311318692f3a7382017d4afb8cc2b6c69f50d8c8
paddy-r <h.p.rice@leeds.ac.uk>
2025-01-17 09:54:31 +0000

    Additions to ferility utils - plotting etc

--- minos/fertility_utils.py
+++ minos/fertility_utils.py
@@ -6,6 +6,7 @@ from os.path import dirname as up
 import pandas as pd
 import yaml
 import matplotlib.pyplot as plt
+import utils
 
 CURR_DIR = up(__file__)
 PERSISTENT_PATH = os.path.join(up(CURR_DIR), 'persistent_data')
@@ -13,8 +14,26 @@ FERT_REF_PATH = os.path.join(PERSISTENT_PATH, 'fertility_reference')
 OUTPUT_DEFAULT = os.path.join(up(CURR_DIR), 'output')
 METRICS_FILE = 'metrics.csv'
 
-COLUMNS_TO_READ = ['alive', 'region', 'nkids_ind', 'age', 'sex',
-                   'nnewborn', 'nnewborn_hh', 'ethnicity', 'child_ages', 'pidp', 'time', 'nkids', 'nresp']
+# COLUMNS_TO_READ = ['alive', 'ethnicity', 'pidp', 'time', 'age', 'sex',
+#                    'nnewborn', 'nnewborn_hh', 'nkids_ind', 'nkids', 'nresp', 'child_ages',
+#                    'region', 'LSOA11CD',
+#                    ]
+
+ETH_GROUPS = {'White': ['WBI', 'WHO',],
+              'Black': ['BLA', 'BLC', 'OBL',],
+              'Asian': ['IND', 'PAK', 'BAN', 'CHI', 'OAS',],
+              'Mixed': ['MIX',],
+              'Other': ['OTH',],
+              }
+
+
+# HR 18/12/24 To get ethnic supergroup map
+def get_ethnicity_map():
+    _map = {}
+    for k, v in ETH_GROUPS.items():
+        for g in v:
+            _map[g] = k
+    return _map
 
 
 # HR 11/12/24 Get path to latest set of output for specific run configuration
@@ -50,11 +69,16 @@ def get_config_data(file):
     return config_data
 
 
-# HR 11/12/24 Get dictionary of year: data file for simulation output
-def get_latest_data(parity=False,
-                    synthpop=False,
-                    intervention=None,
-                    ):
+# HR 20/12/24 To reformat age bins for plot labelling, etc.; takes Pandas (right closed) interval and returns string
+def format_age_bins(interval):
+    formatted = str(interval).strip('(').strip(']').replace(', ', '_')
+    return formatted
+
+
+# HR 20/12/24 Get range of years in sim output
+def get_sim_info(parity=False,
+                  synthpop=False,
+                  ):
     path = get_latest(parity=parity, synthpop=synthpop)
     config_fullpath = os.path.join(path, 'config_file.yml')
     cd = get_config_data(config_fullpath)
@@ -62,11 +86,38 @@ def get_latest_data(parity=False,
     year_start = cd['time']['start']['year']
     year_end = cd['time']['num_years'] + year_start
     years = range(year_start, year_end+1)
-    print(config_fullpath)
-    print(years)
+
+    return path, years
+
+
+# HR 20/12/24 Get single year of sim data to reduce memory usage
+def get_latest_data_by_year(year,
+                            parity=False,
+                            synthpop=False,
+                            # intervention=None,
+                            ):
+    path, years = get_sim_info(parity=parity, synthpop=synthpop)
+
+    if year not in years:
+        print('Year not in simulation years; returning None')
+        return None
+
+    file = str(year) + '.csv'
+    # data = pd.read_csv(os.path.join(path, file), low_memory=False)[COLUMNS_TO_READ]
+    data = pd.read_csv(os.path.join(path, file), low_memory=False)
+    return data
+
+
+# HR 11/12/24 Get dictionary of year: data file for simulation output
+def get_latest_data(parity=False,
+                    synthpop=False,
+                    # intervention=None,
+                    ):
+    path, years = get_sim_info(parity=parity, synthpop=synthpop)
 
     file_dict = {y: str(y) + '.csv' for y in years}
-    data = {y: pd.read_csv(os.path.join(path, f), low_memory=False)[COLUMNS_TO_READ] for y, f in file_dict.items()}
+    # data = {y: pd.read_csv(os.path.join(path, f), low_memory=False)[COLUMNS_TO_READ] for y, f in file_dict.items()}
+    data = {y: pd.read_csv(os.path.join(path, f), low_memory=False) for y, f in file_dict.items()}
     return data
 
 
@@ -118,12 +169,11 @@ def get_metrics_post(parity=False,
                      synthpop=False,
                      cache=True,
                      overwrite=False,
+                     outfile=METRICS_FILE,
                      ):
 
-    latest = get_latest(parity=parity,
-                        synthpop=synthpop,
-                        )
-    metrics_fullpath = os.path.join(latest, METRICS_FILE)
+    latest = get_latest(parity=parity, synthpop=synthpop)
+    metrics_fullpath = os.path.join(latest, outfile)
 
     if not overwrite:
         try:
@@ -148,6 +198,33 @@ def get_metrics_post(parity=False,
         mdf.to_csv(metrics_fullpath)
 
     return mdf
+
+
+# # HR 20/12/24 Get metrics in disaggregated form, i.e. by age group, ethnicity, parity and area
+# def get_metrics_post_disaggregated(disaggregator=None,
+#                                    parity=False,
+#                                    synthpop=False,
+#                                    cache=True,
+#                                    overwrite=False,
+#                                    outfile=METRICS_FILE,
+#                                    ):
+#     path, years = get_sim_info(parity=parity, synthpop=synthpop)
+#
+#     if disaggregator is None:
+#         gb = {'all', data}
+#     elif disaggregator == 'age':
+#         bins = [16, 18, 20, 25, 30, 35, 40, 50, 120]
+#         gb = data.groupby(pd.cut(data.age, bins))
+#     elif disaggregator == 'region':
+#         gb = data.groupby('region')
+#     elif disaggregator == 'ethnicity':
+#         eth_map = get_ethnicity_map()
+#         data['ethnicity_super'] = data['ethnicity'].map(ethnicity_map)
+#         gb = data.groupby('ethnicity_super')
+#     elif disaggregator == 'parity':
+#         gb = data.groupby('nkids_ind')
+#
+#     return
 
 
 def get_fertility_reference_data():
@@ -218,6 +295,7 @@ def get_fertility_reference_data():
 # HR 17/12/24 To plot metrics over time for different simulation configurations
 def plot_metrics(data,
                  ref_data,
+                 outfile,
                  ):
 
     labels = ['US only w/o parity', 'US only with parity', 'Synthpop (10%) w/o parity', 'Synthpop (10%) with parity']
@@ -232,6 +310,7 @@ def plot_metrics(data,
     for i, ax in enumerate(fig.axes):
         for j, dataset in enumerate(data):
             ax.plot(dataset.index[1:], dataset[_vars[i]][1:], linestyle=line_styles[j], color=line_colours[j])
+            # ax.plot(dataset.index, dataset[_vars[i]], linestyle=line_styles[j], color=line_colours[j])
 
         ax.plot(ref_data.index, ref_data[ref_data.columns[i]], color='black')
 
@@ -240,19 +319,77 @@ def plot_metrics(data,
     fig.legend(labels + ['External data'], loc='right', bbox_to_anchor=(1.07, 0.5))
     # fig.legend(labels[0:2], loc='right', bbox_to_anchor=(1.07, 0.5))
     fig_path = OUTPUT_DEFAULT
-    fig_full = os.path.join(fig_path, 'metrics.png')
+    fig_full = os.path.join(fig_path, outfile)
     fig.savefig(fig_full, bbox_inches='tight')
 
 
 if __name__ == '__main__':
-    m1 = get_metrics_post(parity=False, synthpop=False)
-    m2 = get_metrics_post(parity=True, synthpop=False)
-    m3 = get_metrics_post(parity=False, synthpop=True)
-    m4 = get_metrics_post(parity=True, synthpop=True)
 
-    data = [m1, m2, m3, m4]
-    ref_data = get_fertility_reference_data()
-
-    plot_metrics(data=data,
-                 ref_data=ref_data)
-
+    # ref_data = get_fertility_reference_data()
+    #
+    ''' Plot up mort and fert metrics with and without synthpop and parity '''
+    # m1 = get_metrics_post(parity=False, synthpop=False)
+    # m2 = get_metrics_post(parity=True, synthpop=False)
+    # m3 = get_metrics_post(parity=False, synthpop=True)
+    # m4 = get_metrics_post(parity=True, synthpop=True)
+    #
+    # data = [m1, m2, m3, m4]
+    #
+    # plot_metrics(data=data,
+    #              ref_data=ref_data,
+    #              outfile='metrics_all.jpg')
+    #
+    # ''' Get latest data '''
+    # d1 = get_latest_data(parity=False, synthpop=False)
+    # d2 = get_latest_data(parity=True, synthpop=False)
+    d3 = get_latest_data(parity=False, synthpop=True)
+    # d4 = get_latest_data(parity=True, synthpop=True)
+    #
+    # ''' Adding spatial attributes '''
+    # lsoa_col = 'LSOA11CD'
+    # ward_col = 'WD22CD'
+    # la_col = 'LAD22CD'
+    # region_col = 'RGN22CD'
+    # region_name_col = 'RGN22NM'
+    #
+    # m1 = utils.get_lsoa_to_ward_map()
+    # m2, m3, m4 = utils.get_ward_to_region_map()
+    # # to_add_spatial = [d1, d2, d3, d4]
+    #
+    # ### Workaround until LSOA11 linkage data available
+    # to_add_regional = [d1, d2]
+    # to_add_spatial = [d3, d4]
+    # m4_rev = {v: k for k, v in m4.items()}
+    #
+    # for dataset in to_add_regional:
+    #     for yr, ds in dataset.items():
+    #         ds[region_col] = ds['region'].map(m4_rev)
+    # ###
+    #
+    # for dataset in to_add_spatial:
+    #     for yr, ds in dataset.items():
+    #         ds = utils.add_spatial_attributes(ds)
+    #         ds['region'] = ds['RGN22CD'].map(m4)
+    #
+    # ''' Can now disaggregate and plot by groups '''
+    # # By age groups
+    # # bins = [16, 18, 20, 25, 30, 35, 40, 50, 120]
+    # # by_age_bin = {}
+    # # for i, ds in enumerate([d1, d2, d3, d4]):
+    # #     by_age_bin[i] = {}
+    # #     for y, d in ds.items():
+    # #         for _bin, g in d.groupby(pd.cut(d.age, bins)):
+    # #             by_age_bin[i][_bin] = get_metrics(g, y)
+    # #
+    # # for i, ds in [d1, d2, d3, d4]:
+    # #     for _bin, met in by_age_bin[i].items():
+    # #         label = '_age_' + '' + '.jpg'
+    #
+    #
+    # # By ethnicity
+    #
+    #
+    # # By region
+    #
+    #
+    # # By parity
