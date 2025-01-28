fed24af80ef97b42d12fac5470e6c23d588682da
RobertClay <robclay20@hotmail.co.uk>
2025-01-28 17:17:37 +0000

    data gen script debuggin

--- scripts/aire_data_gen.sh
+++ scripts/aire_data_gen.sh
@@ -7,15 +7,15 @@
 #SBATCH --mail-user=gyrc@leeds.ac.uk                              # Where to send mail
 #SBATCH --ntasks=1                                                # Run a single task
 #SBATCH --cpus-per-task=32                                        # Number of CPU cores per task
-#SBATCH --mem=15G                                                 # Job memory request
+#SBATCH --mem=20G                                                 # Job memory request
 #SBATCH --time=03:00:00                                           # Time limit hrs:min:sec
-#SBATCH --output=logs/logs/minos_single_lineplot-%A-%a.out        # Standard output log.
-#SBATCH --error=logs/errors/minos_single_lineplot-%A-%a.err       # Standard error log.
+#SBATCH --output=logs/data_gen/minos_data_gen-%A-%a.out        # Standard output log.
+#SBATCH --error=logs/data_gen/minos_data_gen.err-%A-%a.err       # Standard error log.
 
 
 # the LOCF annoyingly won't run on arc4 without shedloads of memory so running it in login node and then the rest on a job.
 # new HPC so going to rag this one instead and see if it will work.
-mkdir -p ./logs/data_gen # make dir if not exists
+mkdir -p logs/data_gen # make dir if not exists
 python3 minos/data_generation/US_format_raw.py --source_dir ../UKDA-6614-stata/stata/stata13_se/ # raw data.
 python3 minos/data_generation/US_missing_main.py # LOCF and other deterministic correction.
 qsub 'scripts/arc_run_data_gen.sh'
@@ -26,5 +26,6 @@ python3 minos/data_generation/US_complete_case.py # complete case.
 python3 minos/data_generation/generate_stock_pop.py
 
 python3 minos/data_generation/US_household_upscaling.py -r 'manchester' -p 10
-python3 minos_data_generation/generate_repl_pop.py --region 'manchester'
+python3 minos/data_generation/generate_repl_pop.py --region 'manchester'
+
 
