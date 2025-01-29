baf86a232e27637721aa9b95c9cda816a2c65564
RobertClay <robclay20@hotmail.co.uk>
2025-01-29 10:18:16 +0000

    boosting aire plotting script resources

--- scripts/aire_single_lineplot_script.sh
+++ scripts/aire_single_lineplot_script.sh
@@ -6,8 +6,8 @@
 #SBATCH --mail-type=FAIL                                # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=gyrc@leeds.ac.uk                    # Where to send mail
 #SBATCH --ntasks=1                                      # Run a single task
-#SBATCH --cpus-per-task=16                               # Number of CPU cores per task
-#SBATCH --mem=10G                                       # Job memory request
+#SBATCH --cpus-per-task=64                               # Number of CPU cores per task
+#SBATCH --mem=30G                                       # Job memory request
 #SBATCH --time=01:00:00                                 # Time limit hrs:min:sec
 #SBATCH --output=logs/logs/minos_single_lineplot-%A-%a.out        # Standard output log.
 #SBATCH --error=logs/errors/minos_single_lineplot-%A-%a.err       # Standard error log.
