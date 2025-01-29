b956d87378146386a9219e90625b5cc70c1e115f
RobertClay <robclay20@hotmail.co.uk>
2025-01-29 12:06:57 +0000

    quintiles plot fixing

--- minos/outcomes/QALY_quintiles_plot.py
+++ minos/outcomes/QALY_quintiles_plot.py
@@ -4,13 +4,13 @@ from minos import  utils
 import os
 import pandas as pd
 
-def main(mode, interventions):
+def main(mode, interventions, tag):
     # downlaod three qaly datasets
 
     subset_function_strings = ["who_first_simd_quintile", "who_second_simd_quintile",
                                "who_third_simd_quintile", "who_fourth_simd_quintile", "who_fifth_simd_quintile"]
     qaly_data = pd.DataFrame()
-    tags = ["Baseline", "Good Heating Dummy", "EPCG", "GBIS"]
+    tags = [tag] * 5
     for i, intervention in enumerate(interventions):
         file_dir = os.path.join('output/', mode, intervention)
         runtime_list = os.listdir(os.path.abspath(file_dir))
@@ -27,6 +27,6 @@ def main(mode, interventions):
     ICER_lineplot(qaly_data, f"{mode}_{intervention[0]}_simd_quintiles_energy_combined_ICR_lineplot")
 
 if __name__ == '__main__':
-    main("energy_manchester_scaled", ["GBIS"] * 5)
-    main("energy_manchester_scaled", ["EPCG"] * 5)
-    main("energy_manchester_scaled", ["EPCGandGBIS"] * 5)
+    main("energy_manchester_scaled", ["GBIS"] * 5, "GBIS")
+    main("energy_manchester_scaled", ["EPCG"] * 5, "EPCG")
+    main("energy_manchester_scaled", ["EPCGandGBIS"] * 5, "EPCG and GBIS")
