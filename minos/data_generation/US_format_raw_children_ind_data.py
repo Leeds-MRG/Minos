5139306ec068f8650e1a7774944765c29a0df06d
paddy-r <h.p.rice@leeds.ac.uk>
2025-01-29 15:38:10 +0000

    Individual child ages: new script now working in DG

--- minos/data_generation/US_format_raw_children_ind_data.py
+++ minos/data_generation/US_format_raw_children_ind_data.py
@@ -4,6 +4,7 @@ missing data (if any) there is.
 """
 
 import US_utils
+from US_utils import missing_types as mt
 import pandas as pd
 import numpy as np
 from collections import Counter
@@ -11,6 +12,8 @@ from math import isnan
 
 import os
 from os.path import dirname as up
+
+DATA_PATH = os.path.join(up(up(up(__file__))))
 
 
 def age_64bit_integer_stack(ages):
@@ -53,108 +56,75 @@ def integer_child_ages_to_nkids(ages):
     return nkids
 
 
-def main(input_raw_data, year):
+def main(adult_data, year):
 
-    # # Download children datasets in one at a time
-    # child_name = US_utils.US_file_name(year, "../UKDA-6614-stata/stata/stata13_se/", "child")  # Get hidp, pidp, age
-    # child_data = US_utils.load_file(child_name)
-    #
-    # # Format child data
-    # attribute_columns = US_utils.wave_prefix(['hidp', 'age_dv'], year)
-    # child_data = child_data[attribute_columns]
-    # child_data.columns = ['hidp', 'age']
-    #
-    # child_data.loc[child_data['age'] == 16, 'age'] = 15  # Force max child age to 15 years old
-    # child_data['is_child'] = True
-    # child_data['is_adult'] = False
-    # child_data['time'] = year
-    #
-    # # Assign adults from indresp data as adults, i.e. over 16s
-    # #US_data = pd.read_csv(f"data/composite_US/{year}_US_cohort.csv")
-    # input_raw_data['is_child'] = False
-    # input_raw_data['is_adult'] = True
-    # #collaped_children_US_with_children = collapsed_children_US.merge(child_data, 'inner', on='hidp')
-    # collapsed_children_US_with_children = pd.concat([input_raw_data, child_data])
-    #
-    # # Removing orphans
-    # # Calculating number of adults per hidp; if 0 adults in the house the children are orphans?
-    # #orphans = collapsed_children_US_with_children.groupby(['hidp']).filter(lambda x : sum(x['is_adult']) == 0)
-    # collapsed_children_US_with_children = collapsed_children_US_with_children.groupby('hidp').filter(lambda x : sum(x['is_adult']) > 0)
-    #
-    #
-    # # Sanity check for number of child rows vs declared nkids
-    # actual_children = collapsed_children_US_with_children.groupby(['hidp'])['is_child'].sum()
-    # declared_children = collapsed_children_US_with_children.groupby(['hidp'])['nkids'].max()
-    # # print(sum(np.abs(actual_children - declared_children)))  # Sum absolute error, where lower is better
-    #
-    # # Grab children and join ages into a string separated by "-"
-    # final_US_with_children = collapsed_children_US_with_children.sort_values(by=['hidp', 'age'], ascending =True)
-    # #final_US_with_children['age'] = final_US_with_children['age'].astype(str)
-    # #chained_ages = final_US_with_children.loc[final_US_with_children['is_child'] == True, ].groupby('hidp', as_index=False)['age'].apply('_'.join)
-    # chained_ages = final_US_with_children.loc[final_US_with_children['is_child'] == True, ].groupby('hidp', as_index=True)['age'].apply(age_64bit_integer_stack)
-    #
-    # # Merge chained child ages back onto adults in the dataframe, then tidy up generated child rows and columns needed
-    # #collapsed_children_US = pd.merge(final_US_with_children, chained_ages, how='left', on='hidp')
-    # #collapsed_children_US['child_ages'] = collapsed_children_US['age_y']  # sort out two age columns from the merge
-    #
-    # # Map chained child ages back onto adults in the dataframe
-    # final_US_with_children['child_ages'] = final_US_with_children['hidp'].map(dict(chained_ages))
-    # final_US_with_children.loc[final_US_with_children['child_ages'].isna(), 'child_ages'] = 0
-    # final_US_with_children['child_ages'] = final_US_with_children['child_ages'].astype(int)
+    # Download children datasets in one at a time
+    child_name = US_utils.US_file_name(year, os.path.join(DATA_PATH, "../UKDA-6614-stata/stata/stata13_se/"), "child")  # Get child data
+    child_data = US_utils.load_file(child_name)
 
-    # # Remove children
-    # final_US_with_children = final_US_with_children.loc[final_US_with_children['is_adult'] == 1, ]
+    # Format child data
+    child_vars = ['age_dv',  # Age of child at interview
+                  'pn1pid',  # PIDP of first natural parent
+                  'pn2pid',  # PIDP of second natural parent
+                  'pn1sex',  # Sex of first natural parent
+                  'pn2sex',  # Sex of second natural parent
+                  'pns1pid',  # PIDP of first natural/adoptive/step-parent
+                  'pns2pid',  # PIDP of second natural/adoptive/step-parent
+                  'pns1sex',  # Sex of first natural/adoptive/step-parent
+                  'pns2sex',  # Sex of second natural/adoptive/step-parent
+                  ]
+    attribute_columns = US_utils.wave_prefix(child_vars, year)
+    child_data = child_data[attribute_columns]
+    wave_letter = US_utils.get_wave_letter(year)
+    child_data.columns = [el.split(wave_letter + '_', 1)[1] if el.startswith(wave_letter) else el for el in child_vars]  # Necessary as US_utils.wave_prefix mutates child_vars
+
+    child_data.loc[child_data['age_dv'] == 16, 'age_dv'] = 15  # Force max child age to 15 years old
+
+    # Add columns from parent columns in child data
+    childless_value = 0
+    id_cols = ('pn1pid', 'pn2pid')
+    for parent_id in id_cols:
+        parent_map = child_data.groupby(parent_id)['age_dv'].apply(list)
+        parent_map.index.name = 'pidp'
+        adult_data[parent_id + '_ages'] = adult_data['pidp'].map(parent_map).fillna(childless_value)
+        # Awkward method to create empty lists for no children, but can't pass "[]" directly as Pandas thinks it's a list of values to replace
+        adult_data.loc[adult_data[parent_id + '_ages'] == childless_value, parent_id + '_ages'] = adult_data[parent_id + '_ages'].apply(lambda x: [])
+
+    # Combine children from both IDs
+    child_age_cols = [el + '_ages' for el in id_cols]
+    adult_data['child_ages_list'] = adult_data[child_age_cols].sum(axis=1)  # Add lists across columns
+    adult_data['child_ages_list'] = adult_data['child_ages_list'].apply(lambda x: sorted(list(x)))  # Sort ages
+    # adult_data['nkids_ind_u16'] = adult_data['child_ages_list'].str.len()  # Count U16 children
+    adult_data['child_ages_ind'] = adult_data['child_ages_list'].apply(age_64bit_integer_stack)  # Convert to binary format
+
+    ### BLOCK FOR TESTING
+    # # Count unassigned children, i.e. children with no parents given
+    # all_children = len(child_data)
+    # no_parents_natural = len(child_data.loc[(child_data['pn1pid'].isin(mt)) &
+    #                                         (child_data['pn2pid'].isin(mt))])
+    # no_parents_any = len(child_data.loc[(child_data['pn1pid'].isin(mt)) &
+    #                                     (child_data['pn2pid'].isin(mt)) &
+    #                                     (child_data['pns1pid'].isin(mt)) &
+    #                                     (child_data['pns2pid'].isin(mt))])
     #
-    # final_US_with_children['true_nkids'] = final_US_with_children['child_ages'].apply(integer_child_ages_to_nkids)
-    #
-    #
-    # # People with NA child values either have no children or their children aren't present in the child data for some reason
-    # # Set everyone with no children and NA child ages to custom missing value "childless"
-    # # Some households have positive nkids but their children aren't in the child dataset so can't determine their ages; set these to missing (-9)
-    # # Maybe need to estimate ages from age bins instead for these
-    # # Maybe these are all newborns? There are three in one year though, which may be triplets, but unlikely
-    # final_US_with_children['child_ages'] = final_US_with_children.groupby(['hidp'])['child_ages'].transform('first')
-    # final_US_with_children['nkids'] = final_US_with_children.groupby(['hidp'])['nkids'].transform('max')
-    #
-    # # Remove children
-    # final_US_with_children = final_US_with_children.loc[final_US_with_children['is_adult'] == 1, ]
-    #
-    # # Households with nkids > 0 but no children in the child dataset: assign better error values
-    # # Force children to 0 for these 20 or so weird households with children not in the dataset (newborns?)
-    # # Suspect these are 16 yos in the 15 and under data for some reason, could be human error in US
-    # """Per UKHLS. This is the total number of children aged 15 or under in the household. Count includes children
-    # whose age is unknown if the interview outcome code indicates that the person is a child ineligible
-    # for interview or a child eligible for a youth interview. """
-    # # Given how few of these children there are, just going to ignore them.
-    # # If we add up the child age buckets variables in UKHLS, presumably would see the same gaps
-    # final_US_with_children.loc[(final_US_with_children['nkids'] != 0) & (final_US_with_children['child_ages']==0), 'nkids'] = 0
-    #
-    # # Households with discrepancies between recorded nkids and actual nkids in the children data, assuming as above
-    # # There are a few ineligible children who are added to nkids but ages aren't recorded
-    # # Ignoring them; assign nkids values according to number of recorded child ages per house
-    # discrepant_nkids = final_US_with_children.loc[final_US_with_children['nkids'] - final_US_with_children['true_nkids'] != 0, ['true_nkids']]
-    # final_US_with_children.loc[final_US_with_children['nkids'] - final_US_with_children['true_nkids'] != 0, ['nkids']] = discrepant_nkids
-    #
-    #
-    # who_childless = (final_US_with_children['child_ages'].isna()) & (final_US_with_children['nkids'] == 0.0)
-    #
-    # #collapsed_children_US.loc[who_childless, 'child_ages'] = "childless"  # No children in household
-    #
-    #
-    # final_US_with_children.reset_index(inplace=True, drop=True)
-    # final_US_with_children = final_US_with_children.drop(['is_adult', 'is_child', "true_nkids"], axis=1)
-    #
-    # # Save data
-    # #US_utils.save_file(final_US_with_children, "children_ages_US/", "", year)
-    # return final_US_with_children
-    return input_raw_data
+    # print('\nProcessing individual child ages for {}...'.format(year))
+    # print('Children with natural parents not present: {}/{} ({}%)'.format(no_parents_natural, all_children, 100*no_parents_natural/all_children))
+    # print('Children with any parents not present: {}/{} ({}%)'.format(no_parents_any, all_children, 100*no_parents_any/all_children))
+    ###
+
+    # Drop extraneous columns
+    cols_to_drop = child_age_cols + ['child_ages_list']
+    adult_data.drop(cols_to_drop, axis=1)
+    return adult_data
 
 
 if __name__ == '__main__':
 
-    years = np.arange(1991, 2020)
+    # HR 29/01/25 All below for testing
+    year = 2019
+    years = np.arange(2014, year+1)
     # file_names = [f"data/raw_US/{item}_US_cohort.csv" for item in years]
-    data_path = os.path.join(up(up(up(__file__))))
-    file_names = [os.path.join(data_path, f"data/raw_US/{item}_US_cohort.csv") for item in years]
+    file_names = [os.path.join(DATA_PATH, f"data/raw_US/{item}_US_cohort.csv") for item in years]
     data = US_utils.load_multiple_data(file_names)
-    main(data, 2020)
+    input_data = data.loc[data['time'] == year].copy()
+    child_data, adult_data = main(input_data, year)
