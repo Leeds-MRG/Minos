f822eff281f8956d6c4e3ed12c88bd6d79d49794
RobertClay <robclay20@hotmail.co.uk>
2025-01-30 16:40:28 +0000

    using smaller subset of columns for qaly

--- minos/outcomes/QALY_calculation.py
+++ minos/outcomes/QALY_calculation.py
@@ -35,7 +35,13 @@ def aggregate_csv(filename, intervention, year, start_year, subset_func_string=N
     -------
 
     """
-    df = pd.read_csv(filename, low_memory=False)
+
+    keep_columns = ["SF_12_MCS", "SF_12_PCS", "alive", "weight", "hidp", "pidp", "universal_credit",
+                    "housing_sector", "simd_decile"]
+    if intervention != "baseline":
+        keep_columns += ['intevention_cost', "boost_amount"]
+
+    df = pd.read_csv(filename, usecols= keep_columns, low_memory=False)
     if subset_func_string:
         df = dynamic_subset_function(df, subset_func_string, mode)
         print(f"For substring chain {subset_func_string} there are {df.shape[0]} eligible individuals in the dataset.")
