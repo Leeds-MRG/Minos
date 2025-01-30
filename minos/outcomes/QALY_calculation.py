358b3dd05a89ae19f71bdecbaebcca32932f2c6e
RobertClay <robclay20@hotmail.co.uk>
2025-01-30 16:59:20 +0000

    more typos urgh

--- minos/outcomes/QALY_calculation.py
+++ minos/outcomes/QALY_calculation.py
@@ -38,10 +38,7 @@ def aggregate_csv(filename, intervention, year, start_year, subset_func_string=N
 
     keep_columns = ["SF_12_MCS", "SF_12_PCS", "alive", "weight", "hidp", "pidp", "universal_credit",
                     "housing_sector", "simd_decile"]
-    if intervention == "baseline":
-    elif intervention:
-        keep_columns += ['intervention_cost', "income_boosted"]
-    else:
+    if intervention != "baseline":
         keep_columns += ['intervention_cost', "income_boosted"]
     df = pd.read_csv(filename, usecols= keep_columns, low_memory=False)
     if subset_func_string:
