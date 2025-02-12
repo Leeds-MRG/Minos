c8e29a7ce7b9c6b6df2e1648f3c6c230c0947b44
RobertClay <robclay20@hotmail.co.uk>
2025-02-12 17:17:22 +0000

    Adding in legen labels

--- minos/outcomes/QALY_plot.py
+++ minos/outcomes/QALY_plot.py
@@ -88,26 +88,26 @@ def QALY_quintiles_lineplot(data, prefix, destination="plots/", baseline="Baseli
 
     plot_data= pd.DataFrame()
 
+    data['Quintile'] = data['tag']
+
     for i, level in enumerate(["first", "second", "third", "fourth", "fifth"]):
         subset_data = data.loc[data['subset'] == f"who_{level}_{group}_quintile"]
-        subset_data["QALYs_cumsum"] = subset_data.groupby(by=["tag", 'run_id'])["QALYs"].transform(
+        subset_data["QALYs_cumsum"] = subset_data.groupby(by=["Quintile", 'run_id'])["QALYs"].transform(
             lambda x: cumulative_simpson(x, dx=1, initial=0))
-        subset_data["total_boost_cumsum"] = subset_data.groupby(by=["tag", 'run_id'])["total_boost"].transform(np.cumsum)
+        subset_data["total_boost_cumsum"] = subset_data.groupby(by=["Quintile", 'run_id'])["total_boost"].transform(np.cumsum)
         # cum sum qaly.
         subset_data.reset_index(inplace=True)
-        subset_data = subset_data.groupby(by=['tag', 'run_id', 'year'], as_index=False, sort=False).agg(
+        subset_data = subset_data.groupby(by=['Quintile', 'run_id', 'year'], as_index=False, sort=False).agg(
             {"QALYs_cumsum": np.mean, "total_boost_cumsum": np.mean})
-        subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.tile(subset_data.loc[subset_data['tag'] == baseline, "QALYs_cumsum"].values,
-                                                                   len(subset_data['tag'].value_counts()))
-        # subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.repeat(subset_data.loc[subset_data['tag']=="Baseline", "QALYs_cumsum"].values, len(subset_data['tag'].value_counts()))
+        subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.tile(subset_data.loc[subset_data['Quintile'] == baseline, "QALYs_cumsum"].values,
+                                                                   len(subset_data['Quintile'].value_counts()))
+        # subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.repeat(subset_data.loc[subset_data['Quintile']=="Baseline", "QALYs_cumsum"].values, len(subset_data['Quintile'].value_counts()))
         subset_data['QALYs_cumsum_percentage_diff'] = subset_data['QALYs_cumsum_diff'] / subset_data["QALYs_cumsum"]
-        subset_data['tag'] = f"Quintile {i+1}"
+        subset_data['Quintile'] = f"{i+1}"
         plot_data = pd.concat([plot_data, subset_data])
 
     # plot.
-    plot_data['Quintile'] = plot_data['tag']
     f = plt.figure()
-    # TODO: CHANGE TO TAG
     ax = sns.lineplot(data=plot_data, x='year', y="QALYs_cumsum_diff", hue='Quintile', style='Quintile',
                       markers=True, palette='Set2')
     ax.set(ylabel="QALYs difference (years)")
@@ -123,29 +123,28 @@ def ICER_quintiles_lineplot(data, prefix, destination="plots/", baseline="Baseli
     #data.sort_values(by=['year'], inplace=True)
 
     plot_data= pd.DataFrame()
-
+    data['Quintile'] = data['tag']
     for i, level in enumerate(["first", "second", "third", "fourth", "fifth"]):
         # note needs initial 0 in cumulative_simposon at the front to maintain array shape.
         subset_data = data.loc[data['subset'] == f"who_{level}_{group}_quintile"]
-        subset_data["QALYs_cumsum"] = subset_data.groupby(by=["tag", 'run_id'])["QALYs"].transform(lambda x: cumulative_simpson(x, dx=1,initial=0))
-        subset_data["total_boost_cumsum"] = subset_data.groupby(by=["tag", 'run_id'])["total_boost"].transform(np.cumsum)
+        subset_data["QALYs_cumsum"] = subset_data.groupby(by=["Quintile", 'run_id'])["QALYs"].transform(lambda x: cumulative_simpson(x, dx=1,initial=0))
+        subset_data["total_boost_cumsum"] = subset_data.groupby(by=["Quintile", 'run_id'])["total_boost"].transform(np.cumsum)
         # cum sum qaly.
         subset_data.reset_index(inplace=True)
-        subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.tile(subset_data.loc[subset_data['tag']==baseline, "QALYs_cumsum"].values, len(subset_data['tag'].value_counts()))
-        #subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.repeat(subset_data.loc[subset_data['tag']==baseline, "QALYs_cumsum"].values, len(subset_data['tag'].value_counts()))
+        subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.tile(subset_data.loc[subset_data['Quintile']==baseline, "QALYs_cumsum"].values, len(subset_data['Quintile'].value_counts()))
+        #subset_data['QALYs_cumsum_diff'] = subset_data['QALYs_cumsum'] - np.repeat(subset_data.loc[subset_data['Quintile']==baseline, "QALYs_cumsum"].values, len(subset_data['Quintile'].value_counts()))
         subset_data['QALYs_cumsum_percentage_diff'] = subset_data['QALYs_cumsum_diff']/subset_data["QALYs_cumsum"]
 
-        subset_data = subset_data.loc[subset_data['tag'].isin(["EPCG", "GBIS", "EPCG and GBIS"]), ]
+        subset_data = subset_data.loc[subset_data['Quintile'].isin(["EPCG", "GBIS", "EPCG and GBIS", "WBS", "EPCG and WBS"]), ]
         subset_data.loc[subset_data['QALYs_cumsum_diff']==0, 'QALYs_cumsum_diff'] += 1
         subset_data['ICER'] = subset_data['total_boost_cumsum']/subset_data['QALYs_cumsum_diff']
 
         subset_data.loc[subset_data['ICER'].isna(), "ICER"]=0
         subset_data['ICER'] = np.log10(np.abs(subset_data['ICER'])+1)
-        subset_data['tag'] = f"Quintile {i+1}"
+        subset_data['Quintile'] = f"{i+1}"
         plot_data = pd.concat([plot_data, subset_data])
 
     plot_data = plot_data.loc[plot_data['year'] != 2020, ]
-    plot_data['Quintile'] = data['tag']
 
     print(plot_data['ICER'])
     plot_data.to_csv(f"plots/{group}_icer.csv", index=False)
