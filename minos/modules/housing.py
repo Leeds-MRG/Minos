1aa5cace5e6ab333cd39f32823e3ce53157f2ce2
Luke Archer <l.archer@leeds.ac.uk>
2025-02-13 21:45:30 +0000

    Fixed complete case for custom years to avoid pandas warning and updated some modules. Added random hh_income assignment to income module

--- minos/modules/housing.py
+++ minos/modules/housing.py
@@ -123,10 +123,11 @@ class Housing(Base):
         housing_prob_df.index = pop.index
 
         # convert numeric prediction into string factors (low, medium, high)
-        # NOTE: These strings obviously do not match with the numbers, but when switching to the rfo model
-        housing_factor_dict = {1: 'Medium',
+        # NOTE: These strings obviously do not match with the numbers, but when switching to the rfo model the factor
+        #       levels are being jumbled up for some reason. Don't know why but this fixes it.
+        housing_factor_dict = {1: 'High',
                                2: 'Low',
-                               3: 'High'}
+                               3: 'Medium'}
         housing_prob_df.replace({'housing_quality': housing_factor_dict},
                                 inplace=True)
 
