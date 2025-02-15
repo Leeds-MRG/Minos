a7db6e268bb56730ba9a83bf22924750fbbb50df
paddy-r <h.p.rice@leeds.ac.uk>
2025-02-15 10:12:56 +0000

    GB synthpop repl: all-case fix for ageing type annoyings

--- minos/modules/ageing.py
+++ minos/modules/ageing.py
@@ -52,12 +52,13 @@ class Ageing(Base):
         # do this by getting the oldest ALIVE member of a household and give everyone in the household that age chain.
         population['child_ages'] = population.groupby('hidp')['child_ages'].transform("first")
         # update children age chains.
+        nkids_type = population['nkids'].dtype  # Grab type before modification to avoid Vivarium PopulationError
         population = self.update_binary_child_ages(population)
         population = self.update_binary_child_ages_ind(population)
 
         # update new population.
         logging.info(f"Aged population to year {event.time.year}")
-        population['nkids'] = population['nkids'].astype('int64')  # HR 14/02/25 Grr
+        population['nkids'] = population['nkids'].astype(nkids_type)  # HR 14/02/25 Grr
         self.population_view.update(population[['age', 'time',
                                                 'nkids', 'nkids_ind',
                                                 'child_ages', 'child_ages_ind']])
