e9971b3747e9e14fe30425325d295ed42d60cea1
RobertClay <robclay20@hotmail.co.uk>
2025-02-16 19:44:42 +0000

    poor heating qalys

--- minos/outcomes/QALY_calculation.py
+++ minos/outcomes/QALY_calculation.py
@@ -39,9 +39,9 @@ def aggregate_csv(filename, intervention, year, start_year, subset_func_string=N
     keep_columns = ["SF_12_MCS", "SF_12_PCS", "alive", "weight", "hidp", "pidp", "universal_credit",
                     "housing_sector", "simd_decile", "hh_income", "heating"]
     if "who_boosted" in subset_func_string:
-        keep_columns += ["income_boosted"]
+        keep_columns.append("income_boosted")
     if "GBIS_boosted" in subset_func_string:
-        keep_columns += ["GBIS_income_boosted"]
+        keep_columns.append("GBIS_income_boosted")
 
     if intervention != "baseline":
         keep_columns += ['intervention_cost', "income_boosted"]
