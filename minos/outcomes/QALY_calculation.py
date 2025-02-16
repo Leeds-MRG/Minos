fc4c7522e88be1ec369c64dd82001223f7231429
RobertClay <robclay20@hotmail.co.uk>
2025-02-16 19:50:17 +0000

    poor heating qalys

--- minos/outcomes/QALY_calculation.py
+++ minos/outcomes/QALY_calculation.py
@@ -44,6 +44,8 @@ def aggregate_csv(filename, intervention, year, start_year, subset_func_string=N
     if intervention != "baseline":
         keep_columns += ['intervention_cost', "income_boosted"]
 
+    print(keep_columns)
+    
     df = pd.read_csv(filename, usecols= keep_columns, low_memory=False)
     if subset_func_string:
         df = dynamic_subset_function(df, subset_func_string, mode)
