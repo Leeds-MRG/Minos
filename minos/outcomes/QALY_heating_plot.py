8e5cc0294624cad4c89a4e16c351fce3e36970d7
RobertClay <robclay20@hotmail.co.uk>
2025-02-16 19:32:18 +0000

    poor heating qalys

---
+++ minos/outcomes/QALY_heating_plot.py
@@ -0,0 +1,31 @@
+
+from minos.outcomes.QALY_plot import QALY_lineplot, ICER_lineplot
+from minos import  utils
+import os
+import pandas as pd
+
+
+def main(mode, interventions):
+    # downlaod three qaly datasets
+
+    subset_function_strings = ["who_poor_heating", "who_poor_heating", "who_boosted", "who_GBIS_boosted"]
+    qaly_data = pd.DataFrame()
+    tags = ["Baseline", "EPCG", "WHS", "EPCG and WHS"]#, "Good Heating Dummy"
+
+    for i, intervention in enumerate(interventions):
+        file_dir = os.path.join('output/', mode, intervention)
+        runtime_list = os.listdir(os.path.abspath(file_dir))
+        runtime = utils.get_latest_subdirectory(runtime_list)
+        batch_source= os.path.join(file_dir, runtime)
+        qaly_subset = pd.read_csv(batch_source + "/" + subset_function_strings[i] + '_qalys.csv')
+        qaly_subset['tag'] =tags[i]
+        qaly_data = pd.concat([qaly_data, qaly_subset])
+
+    # plot just qalys
+    QALY_lineplot(qaly_data, f"{mode}_{subset_function_strings[i]}_energy_combined_QALY_lineplot")
+
+    # plot icer ratios.
+    ICER_lineplot(qaly_data, f"{mode}_{subset_function_strings[i]}_energy_combined_ICR_lineplot")
+
+if __name__ == '__main__':
+    main("energy_manchester_scaled",["baseline",  "EPCG", "GBIS", "EPCGandGBIS"])#"goodHeatingDummy",
