47da886336e9783c4ad6a16ec4f4be0c9e9adb0b
paddy-r <h.p.rice@leeds.ac.uk>
2025-02-19 14:58:57 +0000

    Refactor rate tables to allow for scale factor to be applied

--- minos/data_generation/convert_rate_data.py
+++ minos/data_generation/convert_rate_data.py
@@ -320,6 +320,12 @@ def transform_rate_table(df,
     return dfn
 
 
+# HR 19/02/25 Set any data with x > 1 to x = 1
+def correct_rate_data_to_unity(data, _var):
+    data.loc[data[_var] > 1.0, _var] = 1.0
+    return data
+
+
 if __name__ == "__main__":
     # file_source = PERSISTENT_DATA_DIR
     # d1, d2 = main(file_source)
@@ -332,9 +338,5 @@ if __name__ == "__main__":
     # # Final rate table, compatible with Vivarium; for validation
     # mort_correct = os.path.join(PERSISTENT_DATA_DIR, 'mortality_rate_table_2020_2035_1_.csv')
     # df_correct = pd.read_csv(mort_correct)
-    #
-    # # Want to rewrite transform_rate_data to be simpler and use Pandas;
-    # # validation is to verify it is identical to df_correct
-    # # Working copy of df to be acted on
-    # dfn = transform_rate_table(df, 2020, 2035, 0, 100)
+
     pass
