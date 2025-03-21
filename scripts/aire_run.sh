7374a1c30e94641e87373d8aa610f11d035609ee
RobertClay <robclay20@hotmail.co.uk>
2025-03-21 14:37:45 +0000

    tweaking aire submit script

--- scripts/aire_run.sh
+++ scripts/aire_run.sh
@@ -5,10 +5,10 @@
 #SBATCH --job-name=parallel_minos_job                   # Job name
 #SBATCH --mail-type=FAIL                                # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=gyrc@leeds.ac.uk                    # Where to send mail. if this is not you change it or I will forward them all.
-#SBATCH --array=1-30                                    # Run a single task. Chance size as desired.
+#SBATCH --array=1-100                                    # Run a single task. Chance size as desired.
 #SBATCH --cpus-per-task=1                               # Number of CPU cores per array task.
-#SBATCH --mem=50G                                       # Job memory request. Needs a lot for synthpop jobs.
-#SBATCH --time=12:00:00                                 # Time limit hrs:min:sec
+#SBATCH --mem=10G                                       # Job memory request. Needs a lot for synthpop jobs.
+#SBATCH --time=6:00:00                                 # Time limit hrs:min:sec
 #SBATCH --output=logs/logs/minos_batch-%A-%a.out        # Standard output log.
 #SBATCH --error=logs/errors/minos_batch-%A-%a.err       # Standard error log.
 
