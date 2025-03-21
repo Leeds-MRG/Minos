ca05f35f63b74226c3e31dbde315542863ee0420
RobertClay <robclay20@hotmail.co.uk>
2025-03-21 14:30:52 +0000

    nkids variable type check

--- minos/modules/ageing.py
+++ minos/modules/ageing.py
@@ -61,7 +61,8 @@ class Ageing(Base):
         logging.info(f"Aged population to year {event.time.year}")
 
         #if type(population['nkids'][0]) == float:
-        population['nkids'] = population['nkids'].astype(float)
+        #population['nkids'] = population['nkids'].astype(float)
+        #population['nkids'] = population['nkids'].astype(int)
         self.population_view.update(population[['age', 'time', 'child_ages', 'nkids']])
 
 
