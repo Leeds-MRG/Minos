36ec77a3176b51975eb3fb6edcd2f1d13fe59f02
paddy-r <h.p.rice@leeds.ac.uk>
2025-03-21 16:12:22 +0000

    Moving sbatch array param to aire_run to avoid empty SLURM_ARRAY_TASK_ID value - testing

--- scripts/aire_run.sh
+++ scripts/aire_run.sh
@@ -2,25 +2,32 @@
 ################
 # Slurm settings
 ################
-#SBATCH --job-name=parallel_minos_job                 # Job name. Can probably name this better...
+#SBATCH --job-name=parallel_minos_job                 # Job name
 #SBATCH --mail-type=FAIL                              # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-type=END
 #SBATCH --mail-user=h.p.rice@leeds.ac.uk              # Where to send mail
-#SBATCH --ntasks=1                                    # Run a single task # THIS IS THE IMPORTANT NUMBER. IF RUNNING BATCH JOBS NEED TO CHANGE THIS MANUALLY! 100 jobs rquired 100 ntasks.
-#SBATCH --cpus-per-task=1                             # Number of CPU cores per task. Not multithreaded so 1 core works fine. may need minimum of 2 depending on system.
-#SBATCH --mem=10gb                                    # Job memory request. If running synthpop jobs may need to increase mem and time.
+#SBATCH --array=1-30                                  # Number of runs
+###SBATCH --ntasks=1                                    # Number of tasks to run, change as desired - disabled 21/03/24 as distinction with array not clear
+#SBATCH --cpus-per-task=1                             # Number of CPU cores per task. Not multithreaded so 1 core works fine. May need minimum of 2 depending on system
+#SBATCH --mem=10gb                                    # Job memory request. If running synthpop jobs may need to increase mem and time
 #SBATCH --time=01:00:00                               # Time limit hrs:min:sec
-#SBATCH --output=logs/errors/minos_batch-%A-%a.out    # Standard output log.
-#SBATCH --error=logs/logs/minos_batch-%A-%a.err       # Standard output log.
+#SBATCH --output=logs/errors/minos_batch-%A-%a.out    # Standard output log
+#SBATCH --error=logs/logs/minos_batch-%A-%a.err       # Standard output log
 
 echo "Running Minos task $SLURM_JOBID on $SLURM_CPUS_ON_NODE CPU cores"
 echo "Running task $SLURM_ARRAY_TASK_ID of $SLURM_ARRAY_TASK_MAX"
 
+# Create these if they don't exist
+mkdir -p logs
+mkdir -p logs/log
+mkdir -p logs/errors
+
+
 if [ "$#" -eq 6 ]; then
-  echo "Running baseline MINOS simulation"
+  echo "Running baseline MINOS simulation with config $2, mode $4, run ID $SLURM_ARRAY_TASK_ID"
   python3 scripts/run.py -c $2 -o $4 -t $6 -r $SLURM_ARRAY_TASK_ID
 elif [ "$#" -eq 8 ]; then
-  echo "Running MINOS simulation with $6"
+  echo "Running MINOS simulation with config $2, mode $4, intervention $6, run ID $SLURM_ARRAY_TASK_ID"
   python3 scripts/run.py -c $2 -o $4 -i $6 -t $8 -r $SLURM_ARRAY_TASK_ID
 fi
 
--- scripts/aire_submit.sh
+++ scripts/aire_submit.sh
@@ -33,24 +33,24 @@ fi
 # Set current time for directory naming
 TIME=`date +%Y_%m_%d_%H_%M_%S`
 
-# Create these if they dont exist. Will crash Aire if you dont do this.
+# Create these if they don't exist
 mkdir -p logs
 mkdir -p logs/log
 mkdir -p logs/errors
 
 
 ###########################################
-# Determine array size for minos runs here. Move to argument/better default.
-# --array=1-X will run X jobs (X >= 1)..
+# Determine array size for Minos runs here
+# --array=1-X will run X jobs (X >= 1)
 ###########################################
 
 if [ "$#" -eq 4 ]; then
-  echo "Running baseline MINOS simulation"
-#  sbatch --array=1-5  'scripts/aire_run.sh' -c $2 -o $4 -t $TIME
-    sbatch scripts/aire_run.sh -c $2 -o $4 -t $TIME
+  echo "Running baseline MINOS simulation with config $2, mode $4"
+#  sbatch --array=1-5 'scripts/aire_run.sh' -c $2 -o $4 -t $TIME
+  sbatch scripts/aire_run.sh -c $2 -o $4 -t $TIME
 elif [ "$#" -eq 6 ]; then
-  echo "Running MINOS simulation with $6"
-#  sbatch --array=1-5  'scripts/aire_run.sh' -c $2 -o $4 -i $6 -t $TIME
+  echo "Running MINOS simulation with config $2, mode $4, intervention $6"
+#  sbatch --array=1-5 'scripts/aire_run.sh' -c $2 -o $4 -i $6 -t $TIME
   sbatch scripts/aire_run.sh -c $2 -o $4 -i $6 -t $TIME
 fi
 
