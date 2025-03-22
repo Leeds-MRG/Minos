9f6ef9d2d36be43ca4195462c1819a0a1ca34edf
prehr prehr <prehr@login2.aire.lee.alces.network>
2025-03-22 14:41:34 +0000

    More path fixes for HPC fertility_validation - testing

--- minos/validation/fertility_validation.sh
+++ minos/validation/fertility_validation.sh
@@ -9,4 +9,4 @@ echo "Change Slurm settings at the top of this script, validation/fertility_vali
 echo "******************************************************************************************************"
 echo
 
-srun jupyter nbconvert --execute --to html fertility_validation.ipynb
+srun jupyter nbconvert --execute --to html minos/validation/fertility_validation.ipynb
