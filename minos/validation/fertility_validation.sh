cb9265507cf035edee18eb4d41c2501baaa07993
paddy-r <h.p.rice@leeds.ac.uk>
2025-03-22 14:27:19 +0000

    HPC shell script for fertility validation + makefile update - testing

---
+++ minos/validation/fertility_validation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+
+echo
+echo "******************************************************************************************************"
+echo "Running fertility validation notebook, Aire version"
+echo "Change Slurm settings at the top of this script, validation/fertility_validation.sh"
+echo "******************************************************************************************************"
+echo
+
+srun jupyter nbconvert --execute --to html fertility_validation.ipynb
