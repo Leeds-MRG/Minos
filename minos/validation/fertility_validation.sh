4827c135a7cfd4186d1a913bee3016fd00b7acc9
prehr prehr <prehr@login2.aire.lee.alces.network>
2025-03-22 16:24:04 +0000

    Adding Slurm output to .gitignore + more params to validation shell script - testing

--- minos/validation/fertility_validation.sh
+++ minos/validation/fertility_validation.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#SBATCH --job-name=fertility_validation               # Job name
+#SBATCH --mail-type=FAIL                              # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-type=END
+#SBATCH --mail-user=h.p.rice@leeds.ac.uk              # Where to send mail
 #SBATCH --time=01:00:00
 #SBATCH --nodes=1
+#SBATCH --mem=64gb
+#SBATCH --output=minos/validation/fertility-validation-%j.out
 
 echo
 echo "******************************************************************************************************"
