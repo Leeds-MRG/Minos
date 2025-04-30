83183ec936e8f7df9486fb6fab519dc8c11d066b
RobertClay <robclay20@hotmail.co.uk>
2025-04-30 15:00:44 +0100

    typo in plot names

--- minos/outcomes/make_lineplots_macros.py
+++ minos/outcomes/make_lineplots_macros.py
@@ -267,7 +267,7 @@ def persistent_income_quintiles_lineplot(config_mode, source, subset, region=Non
     # tags = "Baseline,Baseline,First,Second"
     # subset_function_strings = """who_first_simd_quintile,who_second_simd_quintile,who_first_simd_quintile,who_second_simd_quintile"""
 
-    prefix = f"{subset}_persistent_income_quintiles"
+    prefix = f"{source}_persistent_income_quintiles"
     ref = "Baseline"
     method = 'nanmean'
     region=None
