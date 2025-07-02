6b49bc6d48c78c0a20ef6fd58c8fe9615aa1d371
Joao A. Duro <75260626+jaduro@users.noreply.github.com>
2025-07-02 09:44:12 +0100

    Format to children script

--- minos/data_generation/US_format_raw_children_data.py
+++ minos/data_generation/US_format_raw_children_data.py
@@ -10,43 +10,51 @@ import numpy as np
 
 def main(input_raw_data, year):
 
-
     # download children datasets in one at a time
     child_name = US_utils.US_file_name(year, "../UKDA-6614-stata/stata/stata13_se/", "child")    # get hidp, pidp, and age.
     child_data = US_utils.load_file(child_name)
+    print('child_data %d' % len(child_data.index))
 
     # format child data.
-    attribute_columns = US_utils.wave_prefix(['hidp', 'age_dv'], year)
+    attribute_columns = US_utils.wave_prefix(['hidp', 'age_dv', 'pidp'], year)
     child_data = child_data[attribute_columns]
-    child_data.columns = ['hidp', 'age']
+    child_data.columns = ['hidp', 'age', 'pidp']
     child_data['is_child'] = True
     child_data['is_adult'] = False
     child_data['time'] = year
 
     # assign adults from indresp data as adults. over 16s.
     #US_data = pd.read_csv(f"data/composite_US/{year}_US_cohort.csv")
-    input_raw_data['is_child'] = False
-    input_raw_data['is_adult'] = True
+    input_raw_data['is_child'] = np.where(input_raw_data['age'] < 16, True, False)
+    input_raw_data['is_adult'] = np.where(input_raw_data['age'] > 15, True, False)
+    # input_raw_data['is_child'] = False
+    # input_raw_data['is_adult'] = True
     #collaped_children_US_with_children = collaped_children_US.merge(child_data, 'inner', on='hidp')
-    collapsed_children_US_with_children = pd.concat([input_raw_data, child_data])
+    collapsed_children_US_with_children = input_raw_data.merge(right=child_data, on=['pidp'], how='left', suffixes=('', '_delme'))
+    collapsed_children_US_with_children = collapsed_children_US_with_children.drop_duplicates(subset=['pidp'])
+    collapsed_children_US_with_children = collapsed_children_US_with_children[[c for c in collapsed_children_US_with_children.columns if not c.endswith("_delme")]]
+    # collapsed_children_US_with_children = pd.concat([input_raw_data, child_data])
+    print('collapsed_children_US_with_children ', len(collapsed_children_US_with_children.index))
 
     # removing orphans.
     # calculating number of adults per hidp.
     # if 0 adults in the house the children are orphans?
     #orphans = collapsed_children_US_with_children.groupby(['hidp']).filter(lambda x : sum(x['is_adult']) == 0)
     collapsed_children_US_with_children = collapsed_children_US_with_children.groupby('hidp').filter(lambda x : sum(x['is_adult']) > 0)
-
+    print('remove orphans ', len(collapsed_children_US_with_children.index))
 
     # sanity check for number of child rows vs declared nkids
     actual_children = collapsed_children_US_with_children.groupby(['hidp'])['is_child'].sum()
     declared_children = collapsed_children_US_with_children.groupby(['hidp'])['nkids'].max()
-    # print(sum(np.abs(actual_children - declared_children))) # Sum absolute error. lower is better.
+    print('SAE ', sum(np.abs(actual_children - declared_children))) # Sum absolute error. lower is better.
 
     # grab children
     # join ages into a string seperated by -
     final_US_with_children = collapsed_children_US_with_children.sort_values(by=['hidp', 'age'], ascending =True)
+    final_US_with_children = final_US_with_children.assign(age=lambda df_: df_['age'].astype('int8'))
     final_US_with_children['age'] = final_US_with_children['age'].astype(str)
     chained_ages = final_US_with_children.loc[final_US_with_children['is_child'] == True, ].groupby('hidp', as_index=False)['age'].apply('_'.join)
+    print('final_US_with_children ', len(final_US_with_children.index))
 
     # merge chained child ages back onto adults in the dataframe. tidy up generated child rows and columns needed.
     collapsed_children_US = pd.merge(final_US_with_children, chained_ages, how='left', on='hidp')
@@ -60,15 +68,16 @@ def main(input_raw_data, year):
     collapsed_children_US['nkids'] = collapsed_children_US.groupby(['hidp'])['nkids'].transform('max')
 
     # households with nkids >0 but no children in the child dataset?? assign better error values.
-    who_children_not_in_data = (collapsed_children_US['child_ages'].isna()) & (collapsed_children_US['nkids']>0.0)
+    who_children_not_in_data = (collapsed_children_US['child_ages'].isna()) & (collapsed_children_US['nkids'] > 0.0)
     collapsed_children_US.loc[who_children_not_in_data, 'nkids'] *= 0# force children to 0 for these 20 or so wierd households with children not in the dataset. (newborns?)
-    who_childless = (collapsed_children_US['child_ages'].isna()) & (collapsed_children_US['nkids']==0.0)
+    who_childless = (collapsed_children_US['child_ages'].isna()) & (collapsed_children_US['nkids'] == 0.0)
     collapsed_children_US.loc[who_childless, 'child_ages'] = "childless"# no children in household.
 
     collapsed_children_US['age'] = collapsed_children_US['age_x']
     collapsed_children_US.reset_index(inplace=True, drop=True)
     collapsed_children_US = collapsed_children_US.loc[collapsed_children_US['is_adult'] == 1, ]
     collapsed_children_US = collapsed_children_US.drop(['age_x', 'age_y', 'is_adult', 'is_child'], axis=1)
+    print('collapsed_children_US ', len(collapsed_children_US.index))
 
     # save data
     #US_utils.save_file(collaped_children_US, "children_ages_US/", "", year)
