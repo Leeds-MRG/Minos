e2eff77eeb22a1473ff03ab00932584bde2a6131
paddy-r <prehr@leeds.ac.uk>
2025-08-15 14:27:08 +0100

    IPF - script to generate IPF births + pop data (empty)

---
+++ minos/data_generation/generate_ipf_fertility_data.py
@@ -0,0 +1,8 @@
+# HR 15/08/25 Produce births and population tables via IPF for use with fertility modules
+year_start, year_end = 2015, 2045
+
+def main():
+    print("Generating births and population data from {} to {}".format(year_start, year_end))
+
+if __name__ == "__main__":
+    main()
