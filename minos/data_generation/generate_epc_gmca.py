559381b31dd209995d5d46fdd5f9d49c0731f955
Joao A. Duro <75260626+jaduro@users.noreply.github.com>
2026-01-09 15:33:22 +0000

    For predicting environmental impact improvement, replaced average across LSOAs, tenure and number of habitable rooms, with a Random Forest Model as a function of number of habitable rooms and EPC (before and after)

--- minos/data_generation/generate_epc_gmca.py
+++ minos/data_generation/generate_epc_gmca.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 from statsmodels.miscmodels.ordinal_model import OrderedModel
+from sklearn.ensemble import RandomForestRegressor
 
 from sipherdb.sipher_database import SipherDatabase
 from sipherdb.sipher_database import SqlDB
@@ -20,18 +21,10 @@ def db_init(sql_db=SqlDB.POSTGRESQL):
     return Queries()
 
 
-def compute_clogit(data_latest_df):
+def compute_clogit(data_latest_df, epc_retrofit_df):
 
-    dbquery = db_init()
-
-    # Get the EPC retrofit data
-    data = dbquery.ca_epc_retrofit_at_lsoa_level(ca_name='Greater Manchester')
-    data_df = pd.DataFrame(data, columns=['lsoa_code', 'imd_rank', 'housing_tenure', 'number_of_habitable_rooms',
-                                          'rating_previous', 'rating_latest', 'environment_impact'])
-    # All number of habitable rooms higher than max_number_of_rooms, make them max_number_of_rooms
-    data_df['number_of_habitable_rooms'] = np.where(data_df['number_of_habitable_rooms'] > max_number_of_rooms,
-                                                    max_number_of_rooms, data_df['number_of_habitable_rooms'])
-    df = data_df[['imd_rank', 'housing_tenure', 'number_of_habitable_rooms', 'rating_previous', 'rating_latest']].copy()
+    df = epc_retrofit_df[['imd_rank', 'housing_tenure', 'number_of_habitable_rooms',
+                          'rating_previous', 'rating_latest']].copy()
 
     # Map EPC ratings (worst->best) G..A to integers 0..6
     rating_map = {'G': 0, 'F': 1, 'E': 2, 'D': 3, 'C': 4, 'B': 5, 'A': 6}
@@ -112,25 +105,47 @@ def main():
 
     dbquery = db_init()  # initialise connection to the database
 
-    # Get EPC latest data
+    # Get EPC latest data and merge with IMD
     epc_latest = dbquery.ca_epc_latest_at_lsoa_level(ca_name='Greater Manchester')
-    epc_latest_df = pd.DataFrame(epc_latest, columns=['lsoa_code', 'imd_rank', 'housing_tenure',
+    epc_latest_df = pd.DataFrame(epc_latest, columns=['lsoa_code', 'housing_tenure',
                                                       'number_of_habitable_rooms', 'energy_rating'])
     epc_latest_df['number_of_habitable_rooms'] = np.where(
         epc_latest_df['number_of_habitable_rooms'] > max_number_of_rooms,
         max_number_of_rooms, epc_latest_df['number_of_habitable_rooms'])
 
+    imd_data = dbquery.imd_rank_in_ca(ca_name='Greater Manchester')
+    imd_data_df = pd.DataFrame(imd_data, columns=['lsoa_code', 'imd_rank'])
+    epc_latest_df = pd.merge(epc_latest_df, imd_data_df, on='lsoa_code', how='left')
+
+    # Get the EPC retrofit data and merge with IMD
+    epc_retrofit = dbquery.ca_epc_retrofit_at_lsoa_level(ca_name='Greater Manchester')
+    epc_retrofit_df = pd.DataFrame(epc_retrofit, columns=['lsoa_code', 'housing_tenure', 'number_of_habitable_rooms',
+                                                          'rating_previous', 'rating_latest', 'environment_impact'])
+    # All number of habitable rooms higher than max_number_of_rooms, make them max_number_of_rooms
+    epc_retrofit_df['number_of_habitable_rooms'] = np.where(epc_retrofit_df['number_of_habitable_rooms'] > max_number_of_rooms,
+                                                            max_number_of_rooms, epc_retrofit_df['number_of_habitable_rooms'])
+    epc_retrofit_df = pd.merge(epc_retrofit_df, imd_data_df, how='left', on='lsoa_code')
+
     # Predict new EPC by using a cumulative logit
-    compute_clogit(epc_latest_df)
+    compute_clogit(epc_latest_df, epc_retrofit_df)
 
-    # load epc_retrofit_ec
-    data_retrofit_ec = dbquery.ca_epc_retrofit_ec_at_lsoa_level(ca_name='Greater Manchester')
-    data_retrofit_ec_df = pd.DataFrame(data_retrofit_ec, columns=['lsoa_code', 'housing_tenure',
-                                                                  'number_of_habitable_rooms', 'environment_impact'])
+    # Predict the environmental impact improvement based on the
+    # number of habitable rooms, IMD and energy rating improvement
+    # Use a Random Forest Regressor model
+    rating_map = {'G': 0, 'F': 1, 'E': 2, 'D': 3, 'C': 4, 'B': 5, 'A': 6}
+    df_model = epc_retrofit_df.copy()
+    df_model['rating_previous_num'] = df_model['rating_previous'].map(rating_map)
+    df_model['rating_latest_num'] = df_model['rating_latest'].map(rating_map)
 
-    # merge data_retrofit_ec_df into epc_latest_df
-    epc_latest_df = pd.merge(epc_latest_df, data_retrofit_ec_df, how='inner',
-                             on=['lsoa_code', 'housing_tenure', 'number_of_habitable_rooms'])
+    X = df_model[['number_of_habitable_rooms', 'rating_previous_num', 'rating_latest_num', 'imd_rank']]
+    y = df_model['environment_impact']
+
+    regr = RandomForestRegressor(n_estimators=100)
+    regr.fit(X, y)
+
+    epc_latest_df['rating_latest_num'] = epc_latest_df['energy_rating_new'].map(rating_map)
+    epc_latest_df['environment_impact'] = regr.predict(epc_latest_df[['number_of_habitable_rooms', 'rating_previous_num',
+                                                                      'rating_latest_num', 'imd_rank']])
 
     # format variables in epc_latest_df
     housing_tenure_reduced_dic = {
